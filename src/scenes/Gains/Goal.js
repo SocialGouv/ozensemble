ab5d732b68ecbc0dc33c0ac44219174e1f7dbec2
Laure STEPHAN <laurestephan5@gmail.com>
2022-05-05 18:25:57 +0200

    Se fixer un objectif

--- src/scenes/Gains/Goal.js
+++ src/scenes/Gains/Goal.js
@@ -1,18 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import H1 from '../../components/H1';
 import styled from 'styled-components';
 import TextStyled from '../../components/TextStyled';
-
+import Calendar from '../../components/Illustrations/Calendar';
+import CocktailGlassTriangle from '../../components/Illustrations/CocktailGlassTriangle';
+import InfoObjectif from '../../components/Illustrations/InfoObjectif';
+import QButton from '../../components/QButton';
+import ButtonPrimary from '../../components/ButtonPrimary';
+import { screenHeight } from '../../styles/theme';
 
 const Goal = () => {
+
+  const [quantity, setQuantity] = useState(1)
+
+  const [Lundi, setLundi] = useState(false)
+  const [Mardi, setMardi] = useState(false)
+  const [Mercredi, setMercredi] = useState(false)
+  const [Jeudi, setJeudi] = useState(false)
+  const [Vendredi, setVendredi] = useState(false)
+  const [Samedi, setSamedi] = useState(false)
+  const [Dimanche, setDimanche] = useState(false)
+
+  const Week = [Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche]
+  const dayNoDrink = Week.filter((e) => e === true).length
+  const NumberByWeek = quantity * (7 - dayNoDrink)
+
+  const navigation = useNavigation();
+
+  const Return = () => {
+    navigation.navigate("GAINS");
+  };
+
+  useEffect(() => {
+    console.log(Week, dayNoDrink)
+  }, [dayNoDrink])
+
   return (
     <ScreenBgStyled>
+      <GoBack onPress={Return}>
+        <TextStyled bold>
+          {"<"} Retour </TextStyled>
+      </GoBack>
       <TopContainer>
         <TopTitle>
           <H1 color="#4030a5">Se fixer un objectif</H1>
         </TopTitle>
       </TopContainer>
+      <Container>
+        <ContainerTime>
+          <TextStyled>La durée de votre objectif est d'<TextStyled bold>un mois</TextStyled></TextStyled>
+        </ContainerTime>
+        <Row>
+          <Calendar size={24} />
+          <TextStyled>Jours où je m'engage à ne pas boire d'alcool</TextStyled>
+        </Row>
+        <DayContainer>
+          <DayButton content="L" active={Lundi} onPress={() => setLundi(!Lundi)} />
+          <DayButton content="M" active={Mardi} onPress={() => setMardi(!Mardi)} />
+          <DayButton content="M" active={Mercredi} onPress={() => setMercredi(!Mercredi)} />
+          <DayButton content="J" active={Jeudi} onPress={() => setJeudi(!Jeudi)} />
+          <DayButton content="V" active={Vendredi} onPress={() => setVendredi(!Vendredi)} />
+          <DayButton content="S" active={Samedi} onPress={() => setSamedi(!Samedi)} />
+          <DayButton content="D" active={Dimanche} onPress={() => setDimanche(!Dimanche)} />
+        </DayContainer>
+        <Row>
+          <CocktailGlassTriangle size={30} />
+          <TextStyled>Nombre de verres par jours que je m'autorise quand je bois de l'alcool</TextStyled>
+        </Row>
+        <Row>
+          <TextStyled>Comment compter un verre sans me tromper </TextStyled>
+          <InfoObjectif size={20} color={"#000000"} />
+        </Row>
+        <QuantityContainer>
+          <QButton content="-" disabled={!quantity} onPress={() => setQuantity(quantity - 1)} />
+          <TextStyled bold color="#4030a5">{quantity}</TextStyled>
+          <QButton content="+" disabled={false} onPress={() => setQuantity(quantity + 1)} />
+        </QuantityContainer>
+        <NumberByWeekContainer>
+          <TextStyled> soit {NumberByWeek} verres par semaine</TextStyled>
+        </NumberByWeekContainer>
+        <CTAButtonContainer>
+          <ButtonPrimary content="Continuer" onPress={() => console.log("continuer")} disabled={dayNoDrink === 0 || quantity === 0} />
+        </CTAButtonContainer>
+      </Container>
     </ScreenBgStyled>
   )
 }
@@ -25,7 +97,7 @@ const ScreenBgStyled = styled.ScrollView`
 `;
 
 const TopContainer = styled.View`
-  padding: 20px 30px 0px;
+  padding: 0px 30px 0px;
 `;
 
 const TopTitle = styled.View`
@@ -36,5 +108,78 @@ const TopTitle = styled.View`
   margin-bottom: 20px;
 `;
 
+const GoBack = styled.TouchableOpacity`
+  padding: 20px 30px 0px;
+`;
+
+const Container = styled.View`
+  padding: 0px 30px 0px;
+`;
+
+const ContainerTime = styled.View`
+  margin-bottom: ${screenHeight * 0.03}
+`;
+
+const Row = styled.View`
+  flex-direction: row;
+  margin-bottom: ${screenHeight * 0.02}
+`;
+
+const QuantityContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+
+`
+const CTAButtonContainer = styled.View`
+  height: ${screenHeight * 0.22}px;
+  align-items: center;
+  background-color: #f9f9f9;
+  flex-shrink: 1;
+  margin-top: ${screenHeight * 0}
+`;
+
+const DayContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: ${screenHeight * 0.08}
+`;
+
+const DayButton = ({ small, content, onPress, active }) => (
+  <QButtonStyled onPress={onPress}>
+    <QButtonContentContainer small={small} backgroundColor={active ? "#4030A5" : "#eeeeee"} >
+      <QButtonContent color={active ? "#eeeeee" : "#000000"}>{content}</QButtonContent>
+    </QButtonContentContainer>
+  </QButtonStyled >
+);
+
+const qButtonSize = 35;
+const QButtonStyled = styled.TouchableOpacity`
+  padding: 1px;
+`;
+
+const QButtonContentContainer = styled.View`
+  height: ${qButtonSize}px;
+  width: ${qButtonSize}px;
+  border-radius: ${qButtonSize}px;
+  border: 1px solid #4030A5;
+  justify-content: center;
+  align-items: center;
+
+`;
+
+const QButtonContent = styled(TextStyled)`
+  font-size: 16px;
+  font-weight: bold;
+  line-height: 25px;
+  justify-content: center;
+  align-items: center;
+  text-align-vertical: center;
+`;
+
+const NumberByWeekContainer = styled.View`
+  align-items: center;
+  margin-bottom: ${screenHeight * 0.1}
+`;
 
 export default Goal
