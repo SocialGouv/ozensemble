eb99b051e1f3ef6a928220bf017d2f5d8c8947e3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-05-10 12:22:29 +0200

    clean again

--- src/reference/constants.js
+++ src/reference/constants.js
@@ -1,23 +1,5 @@
 /* eslint-disable quotes */
 export default {
-  // key storage
-  STORE_KEY_ONBOARDING_DONE: '@OnboardingDoneWithCGU',
-  STORE_KEY_QUIZZ_ONBOARDING_ANSWERS: '@Quizz_answers',
-  STORE_KEY_QUIZZ_ONBOARDING_RESULT: '@Quizz_result',
-  STORE_KEY_QUIZZ_EVALUATE_CONSO_ANSWERS: '@QuizzEvaluateConso_answers',
-  STORE_KEY_QUIZZ_EVALUATE_CONSO_RESULT: '@QuizzEvaluateConso_result',
-  STORE_KEY_QUIZZ_LIFE_QUALITY_ANSWERS: '@QuizzLifeQuality_answers',
-  STORE_KEY_QUIZZ_LIFE_QUALITY_RESULT: '@QuizzLifeQuality_result',
-  STORE_KEY_QUIZZ_MOTIVATIONS_ANSWERS: '@QuizzMotivations_answers',
-  STORE_KEY_QUIZZ_MOTIVATIONS_RESULT: '@QuizzMotivations_result',
-  STORE_KEY_REMINDER: '@Reminder',
-  STORE_KEY_NUMBER_OF_VISITS: '@NumberOfVisits',
-  STORE_KEY_USER_ID: '@UserIdv2',
-  STORE_KEY_DONT_SHOW_SCAN_ALERT: '@ScanAlert',
-  //  notifs
-  NOTIF_REMINDER_TITLE: "C'est l'heure de votre suivi quotidien !",
-  NOTIF_REMINDER_MESSAGE: "N'oubliez pas de remplir votre agenda Oz",
-
   // from
   FROM_MENU: 1,
   FROM_INIT_ROUTE: 2,
@@ -29,20 +11,4 @@ export default {
   FROM_CONSO: 8,
   FROM_REMINDER: 9,
   FROM_RESULT: 10,
-  // questions
-  SCORE: 'score',
-  GENDER: 'gender',
-  MAN: 'man',
-  WOMAN: 'woman',
-
-  // results
-  RESULT_ADDICTED: 'addicted',
-  RESULT_RISK: 'risk',
-  RESULT_GOOD: 'good',
-
-  //results arrow
-  RESULT_ARROW_ADDICTED: 'RESULT_ARROW_ADDICTED',
-  RESULT_ARROW_HARMFUL_USAGE: 'RESULT_ARROW_HARMFUL_USAGE',
-  RESULT_ARROW_SIMPLE_USAGE: 'RESULT_ARROW_SIMPLE_USAGE',
-  RESULT_ARROW_NO_USAGE: 'RESULT_ARROW_NO_USAGE',
 };
