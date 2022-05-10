3ff2fde98bf8364d88ee19b86d29c59c1420f5d1
Laure STEPHAN <laurestephan5@gmail.com>
2022-05-10 14:16:30 +0200

    usestatewithasync get

--- src/hooks/useStateWithAsyncStorage.js
+++ src/hooks/useStateWithAsyncStorage.js
@@ -1,20 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const useStateWithAsyncStorage = (key, initValue) => {
+const useStateWithAsyncStorage = (key, initValue, resetOnInit) => {
 
     const [value, setValue] = useState(initValue);
 
+    const valueType = useMemo(() => typeof initValue, [initValue])
+
     const getInitItemValue = async () => {
+        if (resetOnInit) return AsyncStorage.removeItem(key);
         const foundValue = await AsyncStorage.getItem(key);
         if (!foundValue) return;
-        setValue(JSON.parse(foundValue));
+        if (valueType === 'number') {
+            setValue(Number(foundValue));
+        } else if (valueType === 'boolean') {
+            setValue('true' ? true : false);
+        } else {
+            setValue(JSON.parse(foundValue));
+        }
     }
 
     const setValueInAsyncStorage = (newValue) => {
         setValue(newValue)
-        AsyncStorage.setItem(key, newValue)
+        AsyncStorage.setItem(key, JSON.stringify(newValue))
     }
 
     useEffect(() => {
