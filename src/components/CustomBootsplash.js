eeeef49acf558dd0c003843d3dd29d6057a98147
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-05-18 20:21:05 +0200

    fix bootsplash show

---
+++ src/components/CustomBootsplash.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Modal } from 'react-native';
+import { atom, useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+// waiting for RNBootSplah.show to come back
+export const showBootSplashState = atom({
+  key: 'showBootSplashState',
+  default: false,
+});
+
+const CustomBootsplash = () => {
+  const showBootSplash = useRecoilValue(showBootSplashState);
+
+  return (
+    <Modal visible={showBootSplash} animationType="fade">
+      <FullScreen>
+        <StyledImage source={require('../assets/images/Icon.png')} />
+      </FullScreen>
+    </Modal>
+  );
+};
+
+const FullScreen = styled.View`
+  height: 100%;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  justify-content: center;
+  align-items: center;
+  background-color: #4030a5;
+`;
+
+const StyledImage = styled.Image`
+  width: 250px;
+  height: 250px;
+`;
+
+export default CustomBootsplash;
