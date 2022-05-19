3ab4cf3f8361c22f330489fbd52b074e66b16760
Laure STEPHAN <laurestephan5@gmail.com>
2022-05-19 18:06:51 +0200

    feat: add egality

--- src/components/Illustrations/Equality.js
+++ src/components/Illustrations/Equality.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Svg, { Path, Rect } from 'react-native-svg';
+import Svg, { Rect } from 'react-native-svg';
 import styled from 'styled-components';
 
 const StyledSvg = styled(Svg)``;
