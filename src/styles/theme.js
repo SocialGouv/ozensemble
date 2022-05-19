7311a17567ee67246437c3968fbb9ac11840e7f3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-05-19 08:41:21 +0200

    feat: reminder weekly

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,12 @@
 import { Dimensions } from 'react-native';
+import deviceInfoModule from 'react-native-device-info';
 
 export const buttonHeight = 43;
 export const buttonSmallHeight = 30;
-export const defaultPadding = Math.min(Dimensions.get('window').width * 0.7, 30);
+export const defaultPadding = Math.min(Dimensions.get('window').width * 0.08, 30);
+export const defaultPaddingFontScale = () =>
+  Math.min(16, Dimensions.get('window').width * 0.08) / deviceInfoModule.getFontScaleSync();
+
 export const screenWidth = Dimensions.get('window').width;
 export const screenHeight = Dimensions.get('window').height;
 export const menuHeight = 80;
