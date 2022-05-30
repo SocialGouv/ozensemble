c97bbe44b9b1ad8508107df7ce38016de483bff2
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-05-30 20:36:10 +0200

    fix: test event in api

--- api/src/controllers/event.js
+++ api/src/controllers/event.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { catchErrors } = require("../middlewares/errors");
+const { capture } = require("../third-parties/sentry");
 const router = express.Router();
 
 /*
@@ -29,28 +30,32 @@ router.post(
   catchErrors(async (req, res) => {
     const { body } = req;
 
-    // if (body.event.category === "NAVIGATION" && body.event.action === "HEALTH") {
-    //   return res.status(200).send({
-    //     ok: true,
-    //     sendInApp: [
-    //       "Attention !",
-    //       "Ça ne va pas le faire",
-    //       [
-    //         {
-    //           text: "Pourquoi ?",
-    //           navigate: ["HEALTH"],
-    //         },
-    //         {
-    //           text: "Comment ?",
-    //           navigate: ["CONSO_FOLLOW_UP"],
-    //           style: "destructive",
-    //           event: { category: "IN_APP_CLICK", action: "COMMENT_CLICK" },
-    //         },
-    //       ],
-    //       { cancelable: true },
-    //     ],
-    //   });
-    // }
+    if (body.event.category === "IN_APP_CLICK" && body.event.action === "COMMENT_CLICK") {
+      capture("test ok", { extra: { body } });
+      return res.status(200).send({ ok: true });
+    }
+    if (body.event.category === "NAVIGATION" && body.event.action === "HEALTH") {
+      return res.status(200).send({
+        ok: true,
+        sendInApp: [
+          "Attention !",
+          "Ça ne va pas le faire",
+          [
+            {
+              text: "Pourquoi ?",
+              navigate: ["HEALTH"],
+            },
+            {
+              text: "Comment ?",
+              navigate: ["CONSO_FOLLOW_UP"],
+              style: "destructive",
+              event: { category: "IN_APP_CLICK", action: "COMMENT_CLICK" },
+            },
+          ],
+          { cancelable: true },
+        ],
+      });
+    }
     return res.status(200).send({ ok: true });
   })
 );
