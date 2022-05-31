caffbb4fe1938f34ac4a1b89a51dec5969b2870d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-05-31 11:02:36 +0200

    fix: sentry

--- api/src/config.js
+++ api/src/config.js
@@ -3,7 +3,7 @@ const { version, mobileAppVersion } = require("../package.json");
 const PORT = process.env.PORT || 3000;
 const ENVIRONMENT = process.env.ENVIRONMENT || process.env.NODE_ENV || "development";
 
-const SENTRY_KEY = process.env.SENTRY_KEY;
+const SENTRY_KEY = process.env.SENTRY_KEY || "https://b43d73353b7b48b8857deb69bca98da2@o348403.ingest.sentry.io/2213011";
 
 const VERSION = version;
 const MOBILE_VERSION = mobileAppVersion;
