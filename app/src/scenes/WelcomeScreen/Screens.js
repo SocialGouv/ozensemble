aa2b556e53b71f017f12da838de7a908e0575ec5
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-09 10:31:53 +0200

    fix(app) : WelcomeScreen

--- app/src/scenes/WelcomeScreen/Screens.js
+++ app/src/scenes/WelcomeScreen/Screens.js
@@ -9,10 +9,9 @@ import TextStyled from '../../components/TextStyled';
 import { mediaHeight } from '../../styles/mediaQueries';
 import { screenHeight, screenWidth } from '../../styles/theme';
 import Agreement from './Agreement';
-import { ScreenBgStyled } from '../../components/Styles/SreenByStyled';
 
 export const Screen1 = () => (
-  <ScreenBgStyled WelcomeScreen>
+  <ScreenBgStyled>
     <StyledScreen1 />
     <Title>
       <TextStyled color="#4030a5">Bravo de vouloir réduire votre consommation</TextStyled>
@@ -28,7 +27,7 @@ export const Screen1 = () => (
 );
 
 export const Screen2 = () => (
-  <ScreenBgStyled WelcomeScreen>
+  <ScreenBgStyled>
     <StyledScreen2 />
     <Title>
       <TextStyled color="#4030a5">Comment cela marche ?</TextStyled>
@@ -51,7 +50,7 @@ export const Screen2 = () => (
 );
 
 export const Screen3 = ({ setAgreed, agreed }) => (
-  <ScreenBgStyled WelcomeScreen>
+  <ScreenBgStyled>
     <StyledScreen3 />
     <Title>
       <TextStyled color="#4030a5">Mesurez vos gains</TextStyled>
@@ -64,6 +63,15 @@ export const Screen3 = ({ setAgreed, agreed }) => (
     <Agreement onAgree={() => setAgreed(!agreed)} agreed={agreed} />
   </ScreenBgStyled>
 );
+
+const ScreenBgStyled = styled.View`
+  background-color: #f9f9f9;
+  justify-content: flex-end;
+  align-items: center;
+  flex-shrink: 1;
+  flex-grow: 1;
+  flex-basis: 100%;
+`;
 
 const Title = styled(H1)`
   margin-bottom: ${screenHeight * 0.025}px;
