97ed1e7734f26a4f648d4798c634444d7f10757c
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-09 18:42:58 +0200

    feat(app) : Result page

--- app/src/scenes/Quizzs/QuizzOnboarding/ResultsOnboarding/index.js
+++ app/src/scenes/Quizzs/QuizzOnboarding/ResultsOnboarding/index.js
@@ -1,36 +1,19 @@
 import React from 'react';
 import EmptyView from '../../../../components/EmptyView';
 import Result from './Result';
-import ResultRisk from './ResultRisk';
-import ResultGood from './ResultGood';
-import ResultAddicted from './ResultAddicted';
 
 const Results = ({ navigation, route, resultKey }) => {
   if (!resultKey) return null;
-  // return resultKey ? (
-  //   <Result
-  //     result={resultKey}
-  //     route={route}
-  //     navigation={navigation}
-  //     isInOnboarding={Boolean(route?.params?.onboarding)}
-  //   />
-  // ) : (
-  //   <EmptyView />
-  // );
-
-  switch (resultKey) {
-    default:
-    case null:
-      return <EmptyView />;
-    case 'good':
-      return <ResultGood route={route} navigation={navigation} isInOnboarding={Boolean(route?.params?.onboarding)} />;
-    case 'risk':
-      return <ResultRisk route={route} navigation={navigation} isInOnboarding={Boolean(route?.params?.onboarding)} />;
-    case 'addicted':
-      return (
-        <ResultAddicted route={route} navigation={navigation} isInOnboarding={Boolean(route?.params?.onboarding)} />
-      );
-  }
+  return resultKey ? (
+    <Result
+      result={resultKey}
+      route={route}
+      navigation={navigation}
+      isInOnboarding={Boolean(route?.params?.onboarding)}
+    />
+  ) : (
+    <EmptyView />
+  );
 };
 
 export default Results;
