bf1c62b000e7d2e3d1766fcdf30f6dbe497d2f1c
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-09 17:48:10 +0200

    to rebase

--- app/src/scenes/Quizzs/QuizzOnboarding/ResultsOnboarding/index.js
+++ app/src/scenes/Quizzs/QuizzOnboarding/ResultsOnboarding/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import EmptyView from '../../../../components/EmptyView';
-import ResultAddicted from './ResultAddicted';
-import ResultGood from './ResultGood';
+import Result from './Result';
 import ResultRisk from './ResultRisk';
+import ResultGood from './ResultGood';
+import ResultAddicted from './ResultAddicted';
 
 const Results = ({ navigation, route, resultKey }) => {
   if (!resultKey) return null;
+  // return resultKey ? (
+  //   <Result
+  //     result={resultKey}
+  //     route={route}
+  //     navigation={navigation}
+  //     isInOnboarding={Boolean(route?.params?.onboarding)}
+  //   />
+  // ) : (
+  //   <EmptyView />
+  // );
+
   switch (resultKey) {
     default:
     case null:
