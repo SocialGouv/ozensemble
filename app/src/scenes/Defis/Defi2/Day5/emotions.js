e0ae70446f258232c2e4bafc593584899b3e569c
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-15 15:01:07 +0200

    feat(app) : defi 2 bilan

--- app/src/scenes/Defis/Defi2/Day5/emotions.js
+++ app/src/scenes/Defis/Defi2/Day5/emotions.js
@@ -11,30 +11,35 @@ const emotions = [
     description: 'Me rassurer',
     icon: <NoReassuredSmiley size={56} />,
     iconclicked: <NoReassuredSmiley size={56} color={'#DE285E'} />,
+    iconBilan: <NoReassuredSmiley size={56} color={'#4030A5'} />,
   },
   {
     value: 2,
     description: 'Me faire plaisir',
     icon: <PleasureSmiley size={56} />,
     iconclicked: <PleasureSmiley size={56} color={'#DE285E'} />,
+    iconBilan: <PleasureSmiley size={56} color={'#4030A5'} />,
   },
   {
     value: 3,
     description: 'Me calmer',
     icon: <AngrySmiley size={56} />,
     iconclicked: <AngrySmiley size={56} color={'#DE285E'} />,
+    iconBilan: <AngrySmiley size={56} color={'#4030A5'} />,
   },
   {
     value: 4,
     description: 'Aller moins mal',
     icon: <SadSmiley size={56} />,
     iconclicked: <SadSmiley size={56} color={'#DE285E'} />,
+    iconBilan: <SadSmiley size={56} color={'#4030A5'} />,
   },
   {
     value: 5,
     description: 'Avoir moins mal',
     icon: <IllSmiley size={56} />,
     iconclicked: <IllSmiley size={56} color={'#DE285E'} />,
+    iconBilan: <IllSmiley size={56} color={'#4030A5'} />,
   },
 ];
 
