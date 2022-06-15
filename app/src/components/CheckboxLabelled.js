184f290ddc0f3ada2ce92cf6679169c702f9cdc8
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-06-15 17:05:51 +0200

    fix defi 2 recoile memory

--- app/src/components/CheckboxLabelled.js
+++ app/src/components/CheckboxLabelled.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CheckBox from '@react-native-community/checkbox';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableWithoutFeedback, View } from 'react-native';
+import { View } from 'react-native';
 import styled from 'styled-components';
 import ButtonPrimary from './ButtonPrimary';
 import TextStyled from './TextStyled';
@@ -9,41 +9,43 @@ import TextStyled from './TextStyled';
 const CheckboxLabelled = ({ answerKey, content, alertText, onPress, checked = false, disabled = false }) => {
   const navigation = useNavigation();
   return (
-    <TouchableWithoutFeedback onPress={() => onPress?.(answerKey)}>
-      <View>
-        <ItemContainer>
-          <CheckBoxContainer>
-            <CheckBoxStyled
-              // ios style
-              onCheckColor="#4030a5"
-              onTintColor="#4030a5"
-              onFillColor="#4030a511"
-              animationDuration={0.2}
-              boxType="square"
-              lineWidth={1}
-              //android style
-              tintColors={{ true: '#4030a5', false: '#c4c4c4' }}
-              //common props
-              disabled={disabled}
-              value={checked}
-            />
-          </CheckBoxContainer>
-          <TextStyled>{content}</TextStyled>
-        </ItemContainer>
-        {!!checked && !!alertText && (
-          <AlertContainer>
-            <TextStyled color="#1a1a1a">{alertText}</TextStyled>
-            <ButtonPrimaryStyled
-              small
-              content="Échanger avec un conseiller"
-              onPress={() => navigation.navigate('CONTACT')}
-            />
-          </AlertContainer>
-        )}
-      </View>
-    </TouchableWithoutFeedback>
+    <Container onPress={() => onPress?.(answerKey)} showDisabled={disabled} disabled={disabled}>
+      <ItemContainer showDisabled={disabled}>
+        <CheckBoxContainer>
+          <CheckBoxStyled
+            // ios style
+            onCheckColor="#4030a5"
+            onTintColor="#4030a5"
+            onFillColor="#4030a511"
+            animationDuration={0.2}
+            boxType="square"
+            lineWidth={1}
+            //android style
+            tintColors={{ true: '#4030a5', false: '#c4c4c4' }}
+            //common props
+            disabled={disabled}
+            value={checked}
+          />
+        </CheckBoxContainer>
+        <TextStyled>{content}</TextStyled>
+      </ItemContainer>
+      {!!checked && !!alertText && (
+        <AlertContainer>
+          <TextStyled color="#1a1a1a">{alertText}</TextStyled>
+          <ButtonPrimaryStyled
+            small
+            content="Échanger avec un conseiller"
+            onPress={() => navigation.navigate('CONTACT')}
+          />
+        </AlertContainer>
+      )}
+    </Container>
   );
 };
+
+const Container = styled.TouchableOpacity`
+  ${(props) => props.showDisabled && 'opacity: 0.5;'}
+`;
 
 const CheckBoxContainer = styled.View`
   padding: 2px;
@@ -63,6 +65,7 @@ const ItemContainer = styled.View`
   flex-direction: row;
   margin: 5px;
   width: 87%;
+  ${(props) => props.showDisabled && 'opacity: 0.5;'}
 `;
 
 const AlertContainer = styled.View`
