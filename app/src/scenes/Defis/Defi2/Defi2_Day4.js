da1e1379c880e24ddde9a6d681d9e882945b9daa
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-16 18:15:05 +0200

    feat(app): flatlist + index

--- app/src/scenes/Defis/Defi2/Defi2_Day4.js
+++ app/src/scenes/Defis/Defi2/Defi2_Day4.js
@@ -1,18 +1,15 @@
 import { useIsFocused } from '@react-navigation/native';
-import React, { useCallback, useEffect, useMemo, useRef } from 'react';
-import { NestableDraggableFlatList } from 'react-native-draggable-flatlist';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import H1 from '../../../components/H1';
 import H2 from '../../../components/H2';
 import { defaultPaddingFontScale } from '../../../styles/theme';
 import { setValidatedDays } from '../utils';
-import { ScreenBgStyled } from '../../../components/ScreenBgStyled';
 import BackButton from '../../../components/BackButton';
 import TextStyled from '../../../components/TextStyled';
 import ButtonPrimary from '../../../components/ButtonPrimary';
-import { riskSituationsAnswersKeysSelector, riskSituationsQuizzResultState } from '../../../recoil/quizzs';
 import DraggableFlatListDay4 from './DraggableFlatListDay4';
+import { ScreenBgStyled } from '../../../components/ScreenBgStyled';
 
 const Defi2_Day4 = ({ navigation, route }) => {
   const isFocused = useIsFocused();
@@ -21,11 +18,8 @@ const Defi2_Day4 = ({ navigation, route }) => {
     if (route?.params?.inDefi2) setValidatedDays(route?.params?.day, '@Defi2');
   }, [route?.params, isFocused]);
 
-  const setAnswersRiskSituations = useSetRecoilState(riskSituationsQuizzResultState);
-  const answers = useRecoilValue(riskSituationsAnswersKeysSelector);
-
   return (
-    <>
+    <ScreenBgStyled>
       <TopContainer>
         <BackButton onPress={navigation.goBack} />
         <TopTitle>
@@ -36,17 +30,33 @@ const Defi2_Day4 = ({ navigation, route }) => {
           consommation. Placez les plus motivantes au début et les plus difficiles à la fin de la liste.
         </TextStyled>
         <H2 color="#4030a5">Je suis plus motivé(e) à réduire l'alcool : </H2>
-        <DraggableFlatListDay4 />
-        <ButtonPrimary content="J'ai fini de classer" widthSmall onPress={() => navigation.navigate('DEFI2_MENU')} />
-        <DraggableFlatListDay4 data={answers} setData={setAnswersRiskSituations} />
       </TopContainer>
-    </>
+      <FlatListStyled
+        horizontal
+        alwaysBounceHorizontal={false}
+        noPadding
+        data={[]}
+        ListEmptyComponent={<DraggableFlatListDay4 />}
+      />
+      <ButtonContainer>
+        <ButtonPrimary content="J'ai fini de classer" widthSmall onPress={() => navigation.navigate('DEFI2_MENU')} />
+      </ButtonContainer>
+    </ScreenBgStyled>
   );
 };
 
+const FlatListStyled = styled.FlatList`
+  flex-grow: 1;
+  flex-shrink: 0;
+`;
+
 const TopContainer = styled.View`
   padding: 0px ${defaultPaddingFontScale()}px 0px;
-  margin-bottom: 100px;
+`;
+
+const ButtonContainer = styled.View`
+  margin-top: 20px;
+  margin-bottom: 150px;
 `;
 
 const TopTitle = styled.View`
@@ -55,11 +65,6 @@ const TopTitle = styled.View`
   flex-shrink: 0;
   margin-top: 10px;
   margin-bottom: 20px;
-`;
-
-const ContainerText = styled.View`
-  margin-left: 30px;
-  margin-bottom: 30px;
 `;
 
 export default Defi2_Day4;
--- app/src/scenes/Defis/Defi2/DraggableFlatListDay4.js
+++ app/src/scenes/Defis/Defi2/DraggableFlatListDay4.js
@@ -1,63 +1,79 @@
-import React, { useState } from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import DraggableFlatList from 'react-native-draggable-flatlist';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import DraggableFlatList, { ScaleDecorator } from 'react-native-draggable-flatlist';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import styled from 'styled-components';
+import QButton from '../../../components/QButton';
 import TextStyled from '../../../components/TextStyled';
+import { riskSituationsAnswersKeysSelector, riskSituationsQuizzAnswersState } from '../../../recoil/quizzs';
+import { screenWidth } from '../../../styles/theme';
 
-const DraggableFlatListDay4 = ({ data, setData }) => {
+export default function App() {
+  const setAnswersRiskSituations = useSetRecoilState(riskSituationsQuizzAnswersState);
+  const answers = useRecoilValue(riskSituationsAnswersKeysSelector);
+
   const renderItem = ({ item, drag, isActive }) => {
     return (
-      <TouchableOpacity
-        key={item}
-        onLongPress={drag}
-        disabled={isActive}
-        style={[styles.rowItem, { backgroundColor: isActive ? 'red' : item.backgroundColor }]}>
-        <TextStyled>{item.content}</TextStyled>
-      </TouchableOpacity>
+      <>
+        <ScaleDecorator key={item}>
+          <AnswerContainer onLongPress={drag} disabled={isActive} isActive={isActive}>
+            <TextStyled>{item.content}</TextStyled>
+          </AnswerContainer>
+        </ScaleDecorator>
+      </>
     );
   };
 
-  console.log(data);
-  return null;
-  // <DraggableFlatList
-  //   data={data}
-  //   onDragEnd={({ data }) => setData(data)}
-  //   keyExtractor={(item) => item.answerKey}
-  //   renderItem={renderItem}
-  // />
-};
-
-{
-  /* <DraggableFlatList
-ref={draggableListRef}
-contentContainerStyle={[
-  styles.sectionsList,
-  styles.draggableList,
-  draggableListVisible && styles.draggableListVisible,
-]}
-keyboardShouldPersistTaps="always"
-data={sections}
-renderItem={renderSection}
-keyExtractor={(item) => item.id}
-onDragEnd={onDragEnd}
-ListFooterComponent={
-  <TouchableWithoutFeedback onPress={onSectionsDisactive}>
-    <View style={styles.resetActive} />
-  </TouchableWithoutFeedback>
-}
-scrollEventThrottle={16}
-onScrollEndDrag={(e) => setListScrollOffset(e.nativeEvent.contentOffset.y)}
-onMomentumScrollEnd={(e) => setListScrollOffset(e.nativeEvent.contentOffset.y)}
-/>
- */
+  return (
+    <DraggableFlagListContainer>
+      <QButtons>
+        {answers.map((a, index) => (
+          <QButtonContainer>
+            <QButton
+              key={index}
+              content={index + 1}
+              disabled
+              colorText="#ffffff"
+              colorBorder="#4030A5"
+              colorBackground=" #4030A5"
+            />
+          </QButtonContainer>
+        ))}
+      </QButtons>
+      <DraggableFlatList
+        data={answers}
+        onDragEnd={({ data }) => {
+          setAnswersRiskSituations(data?.map((answer) => answer.answerKey));
+        }}
+        keyExtractor={(item) => item.answerKey}
+        renderItem={renderItem}
+      />
+    </DraggableFlagListContainer>
+  );
 }
 
-export default DraggableFlatListDay4;
+const DraggableFlagListContainer = styled.View`
+  flex-direction: row;
+  margin-left: 20px;
+  align-items: center;
+`;
 
-const styles = StyleSheet.create({
-  rowItem: {
-    height: 100,
-    width: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const QButtons = styled.View`
+  margin-top: 20px;
+`;
+
+const QButtonContainer = styled.View`
+  height: 80px;
+`;
+
+const AnswerContainer = styled(TouchableOpacity)`
+  height: 70px;
+  justify-content: center;
+  padding: 5px;
+  width: ${screenWidth * 0.7}px;
+  border: 1px solid #d3d3e8;
+  border-radius: 3px;
+  margin-vertical: 5px;
+  margin-horizontal: 20px;
+  background-color: ${({ isActive }) => (isActive ? '#4030A540' : '#FFFFFF')};
+`;
