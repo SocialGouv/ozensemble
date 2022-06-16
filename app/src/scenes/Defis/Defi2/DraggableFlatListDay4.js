387cd88b982d2d60be7f5efc84dbcb4ced171b20
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-06-16 12:20:37 +0200

    fix: seems to work

--- app/src/scenes/Defis/Defi2/DraggableFlatListDay4.js
+++ app/src/scenes/Defis/Defi2/DraggableFlatListDay4.js
@@ -1,61 +1,60 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-// import DraggableFlatList from 'react-native-draggable-flatlist';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import DraggableFlatList, { ScaleDecorator } from 'react-native-draggable-flatlist';
+import TextStyled from '../../../components/TextStyled';
 
-const initialData = [
-  {
-    order: 1,
-    label: 'Start Timeular',
-  },
-  {
-    order: 2,
-    label: 'Workout',
-  },
-  {
-    order: 3,
-    label: 'Shower',
-  },
-];
-function MyMorningScreen(props) {
-  const [data, setData] = useState(initialData);
-
-  const renderItem = ({ item, index, drag }) => (
-    <View style={styles.item}>
-      <TouchableOpacity onLongPress={drag}>
-        <Text>{item?.label}</Text>
+const DraggableFlatListDay4 = ({ data, setData }) => {
+  const renderItem = ({ item, drag, isActive }) => {
+    return (
+      <TouchableOpacity
+        onLongPress={drag}
+        disabled={isActive}
+        style={[styles.rowItem, { backgroundColor: isActive ? 'red' : item.backgroundColor }]}>
+        <TextStyled>{item.content}</TextStyled>
       </TouchableOpacity>
-    </View>
-  );
+    );
+  };
 
   return (
-    <View style={styles.screen}>
-      <View style={{ flex: 1 }}>
-        {/* <DraggableFlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={(item, index) => index.toString()}
-          onDragEnd={({ data }) => setData(data)}
-        /> */}
-      </View>
-    </View>
+    <DraggableFlatList
+      data={data}
+      onDragEnd={({ data }) => setData(data)}
+      keyExtractor={(item) => item.answerKey}
+      renderItem={renderItem}
+    />
   );
+};
+{
+  /* <DraggableFlatList
+ref={draggableListRef}
+contentContainerStyle={[
+  styles.sectionsList,
+  styles.draggableList,
+  draggableListVisible && styles.draggableListVisible,
+]}
+keyboardShouldPersistTaps="always"
+data={sections}
+renderItem={renderSection}
+keyExtractor={(item) => item.id}
+onDragEnd={onDragEnd}
+ListFooterComponent={
+  <TouchableWithoutFeedback onPress={onSectionsDisactive}>
+    <View style={styles.resetActive} />
+  </TouchableWithoutFeedback>
 }
+scrollEventThrottle={16}
+onScrollEndDrag={(e) => setListScrollOffset(e.nativeEvent.contentOffset.y)}
+onMomentumScrollEnd={(e) => setListScrollOffset(e.nativeEvent.contentOffset.y)}
+/>
+ */
+}
+export default DraggableFlatListDay4;
 
 const styles = StyleSheet.create({
-  screen: {
-    marginTop: 24,
-    flex: 1,
-    backgroundColor: '#212121',
-  },
-  item: {
-    backgroundColor: 'white',
-    marginTop: 10,
-    padding: 20,
-    marginHorizontal: 10,
-    borderRadius: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  rowItem: {
+    height: 100,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-export default MyMorningScreen;
