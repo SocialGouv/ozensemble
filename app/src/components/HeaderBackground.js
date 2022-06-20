a94c6f27acd7f379f2ac492817dc8ea0fc7055ac
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-20 18:01:21 +0200

    feat(app): Header delete oblique

--- app/src/components/HeaderBackground.js
+++ app/src/components/HeaderBackground.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { screenHeight, screenWidth } from '../styles/theme';
 
 const HeaderBackground = () => (
   <>
     <HeaderBackgroundStyled />
-    <Oblique />
   </>
 );
 
@@ -13,21 +11,6 @@ const HeaderBackgroundStyled = styled.View`
   width: 100%;
   height: 20px;
   background: #39cec0;
-`;
-
-const obliqueHeight = Math.min(screenHeight * 0.1, 10);
-
-const Oblique = styled.View`
-  height: ${obliqueHeight}px;
-  background-color: #f9f9f9;
-  border-left-color: #39cec0;
-  border-left-width: ${screenWidth}px;
-  border-bottom-width: ${obliqueHeight}px;
-  border-bottom-color: transparent;
-  border-top-width: 0px;
-  border-top-color: transparent;
-  border-right-width: ${screenWidth}px;
-  border-right-color: transparent;
 `;
 
 export default HeaderBackground;
