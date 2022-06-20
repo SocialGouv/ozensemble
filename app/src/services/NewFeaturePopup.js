8e5f1b8b17a2f1b77cdc9011049133839593368c
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-06-20 17:57:52 +0200

    feat(api,app): new feature popup handler

--- app/src/services/NewFeaturePopup.js
+++ app/src/services/NewFeaturePopup.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { StyleSheet, View, Text, Platform } from 'react-native';
-import SafeAreaView from 'react-native-safe-area-view';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { StyleSheet, View, Text, Platform, TouchableOpacity } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+import { defaultPaddingFontScale, screenHeight, screenWidth } from '../styles/theme';
+import matomo from './matomo';
 import { storage } from './storage';
 
 /*
@@ -19,55 +21,121 @@ featuresShown = [
       left: 0,
       right: 0,
     },
-    title: 'Regardez ça !'
-    description: 'Eh oui !'
+    title: 'Regardez ça !',
+    description: 'Eh oui !',
     onOkPress: 'EVENT_ON_OK_PRESS',
     onDismissPress: 'EVENT_ON_DISMISS_PRESS',
   }
 ]
  */
 const NewFeaturePopupDisplay = () => {
-  const featuresShownIds = useRef(JSON.parse(storage.getString('@NewFeaturesPopupIdsShown') || JSON.stringify([])));
+  const [featuresShownIds, setFeaturesShownIds] = useState(() => {
+    // storage.delete('@NewFeaturesPopupIdsShown');
+    return JSON.parse(storage.getString('@NewFeaturesPopupIdsShown') || JSON.stringify([]));
+  });
+  const [lastShownId, setLastShownId] = useState(() => {
+    // storage.delete('@NewFeaturesLastShownId');
+    return storage.getString('@NewFeaturesLastShownId') || '';
+  });
   const [featuresToShow, setFeaturesToShow] = useState([]);
 
-  const handleFeaturesShow = async (featuresToShow = []) => {
-    const newFeaturesToShow = featuresToShow.filter((f) => !featuresShownIds.includes(f.id));
-    setFeaturesToShow(newFeaturesToShow);
-  };
+  const handleFeaturesShow = useCallback(
+    async (featuresToShow = []) => {
+      const newFeaturesToShow = featuresToShow.filter((f) => !featuresShownIds.includes(f.id));
+      setFeaturesToShow(newFeaturesToShow);
+    },
+    [featuresShownIds]
+  );
 
   useEffect(() => {
-    NewFeaturePop.featuresShownIds = featuresShownIds.current;
+    NewFeaturePop.lastShownId = lastShownId;
+    storage.set('@NewFeaturesLastShownId', lastShownId || '');
+  }, [lastShownId]);
+
+  useEffect(() => {
+    NewFeaturePop.featuresShownIds = featuresShownIds;
+    storage.set('@NewFeaturesPopupIdsShown', JSON.stringify(featuresShownIds || []));
+  }, [featuresShownIds]);
+
+  useEffect(() => {
     const featurePopupListener = NewFeaturePop.listen(handleFeaturesShow);
     return () => NewFeaturePop.remove(featurePopupListener);
-  }, []);
+  }, [handleFeaturesShow]);
 
   if (!featuresToShow.length) return null;
-  // const featureToShow = featuresToShow[0];
-  // const isLastFeature = featuresToShow.length === 1;
+  const featureToShow = featuresToShow?.[0];
+
+  const onOkPress = () => {
+    if (featureToShow.onOkEvent) matomo.logEvent(featureToShow.onOkEvent);
+    setFeaturesToShow((features) => features.filter((f) => f.id !== featureToShow?.id));
+    setLastShownId(featureToShow?.id);
+    setFeaturesShownIds((featureIds) => [...new Set([...featureIds, featureToShow?.id].filter(Boolean))]);
+  };
+
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.title}>Ceci est une nouvelle fonctionnalité</Text>
-        <Text style={styles.description}>Elle permet d'être trop cool</Text>
+    <>
+      <View style={[styles.container(featureToShow.position), featureToShow.styles?.container || {}]}>
+        {!!featureToShow?.title && (
+          <Text style={[styles.title, featureToShow.styles?.title || {}]}>{featureToShow.title}</Text>
+        )}
+        {!!featureToShow?.description && (
+          <Text style={[styles.description, featureToShow.styles?.description || {}]}>{featureToShow.description}</Text>
+        )}
+        <TouchableOpacity onPress={onOkPress}>
+          <Text style={[styles.okButton, featureToShow.styles?.okButton || {}]}>{featureToShow.okButton}</Text>
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+      {!featureToShow.noPointer && (
+        <Svg
+          viewBox="0 0 14 10"
+          fill="none"
+          style={[styles.pointer(featureToShow.pointerPosition), featureToShow.styles?.pointer || {}]}>
+          <Path d="M6.9282 10L0 0.25L13.8564 0.25L6.9282 10Z" fill="#4030A5" />
+        </Svg>
+      )}
+    </>
   );
 };
 
 export default NewFeaturePopupDisplay;
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 5,
-    backgroundColor: '#f00',
-  },
+  container: ({ top, bottom, left, right }) => ({
+    position: 'absolute',
+    top: top !== undefined ? (top?.includes?.('%') ? top : screenHeight * 0.12 + top) : undefined,
+    bottom: bottom !== undefined ? (bottom?.includes?.('%') ? bottom : screenHeight * 0.12 + bottom) : undefined,
+    left: left !== undefined ? (left?.includes?.('%') ? left : defaultPaddingFontScale() / 2 + left) : undefined,
+    right: right !== undefined ? (right?.includes?.('%') ? right : defaultPaddingFontScale() / 2 + right) : undefined,
+    backgroundColor: '#4030a5',
+    padding: defaultPaddingFontScale() / 3,
+    minWidth: screenWidth / 2,
+    maxWidth: screenWidth * 0.75,
+    borderRadius: 5,
+  }),
   title: {
     fontWeight: Platform.OS === 'android' ? 'bold' : '800',
     color: '#ffffff',
+    marginBottom: 5,
   },
   description: {
     color: '#ffffff',
+    fontWeight: Platform.OS === 'android' ? 'thin' : '300',
   },
+  okButton: {
+    fontWeight: Platform.OS === 'android' ? 'bold' : '800',
+    color: '#ffffff',
+    marginTop: 5,
+    marginLeft: 'auto',
+  },
+  pointer: ({ top, bottom, left, right }) => ({
+    position: 'absolute',
+    top: top !== undefined ? screenHeight * 0.12 + top : undefined,
+    bottom: bottom !== undefined ? screenHeight * 0.12 + bottom : undefined,
+    left,
+    right,
+    height: 20,
+    width: 28,
+  }),
 });
 
 class NewFeaturePopService {
@@ -75,7 +143,7 @@ class NewFeaturePopService {
     this.unsubscribe = null;
   }
 
-  handleShowNewFeaturePopup = (inAppMessage) => {
+  handleShowNewFeaturePopup = (newFeatures) => {
     // two kind of notifications: push notification and in-app message
     // in-app is filled with `data`
     // push notification is filled with `notification`
@@ -93,20 +161,20 @@ class NewFeaturePopService {
     const listenerKeys = Object.keys(this.listeners);
     //  handle initial in-app-message if any, if no listener is mounted yet
     if (!listenerKeys.length) {
-      this.initFeaturePopup = inAppMessage;
+      this.initFeaturePopup = newFeatures;
       return;
     }
     this.initFeaturePopup = null;
 
     //handle normal in-app message
     for (let i = listenerKeys.length - 1; i >= 0; i--) {
-      const inAppMessageHandler = this.listeners[listenerKeys[i]];
-      inAppMessageHandler(inAppMessage);
+      const newFeatureListener = this.listeners[listenerKeys[i]];
+      newFeatureListener(newFeatures);
     }
   };
 
   listeners = {};
-  listen = (fromComponent, callback) => {
+  listen = (callback, fromComponent = 'main') => {
     const listenerKey = `popuplistener_${fromComponent}_${Date.now()}`;
     this.listeners[listenerKey] = callback;
     if (this.initFeaturePopup) this.handleShowNewFeaturePopup(this.initFeaturePopup);
@@ -114,6 +182,7 @@ class NewFeaturePopService {
   };
 
   featuresShownIds = [];
+  lastShownId = '';
 
   remove = (listenerKey) => {
     delete this.listeners[listenerKey];
