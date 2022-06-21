f6740310cf8f678760053b00e4b438e20d53b370
Laure STEPHAN <laurestephan5@gmail.com>
2022-06-21 11:00:17 +0200

    feat(app): defi2

--- app/src/scenes/Defis/Defi2/Defi2_Day7.js
+++ app/src/scenes/Defis/Defi2/Defi2_Day7.js
@@ -2,6 +2,7 @@ import { useIsFocused } from '@react-navigation/native';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useRecoilValue } from 'recoil';
+import { Linking, TouchableOpacity } from 'react-native';
 import H1 from '../../../components/H1';
 import { defaultPaddingFontScale, screenWidth } from '../../../styles/theme';
 import { setValidatedDays } from '../utils';
@@ -15,8 +16,7 @@ import { defi2EmotionState } from '../../../recoil/defis';
 import QButton from '../../../components/QButton';
 import { riskSituationsAnswersKeysSelector } from '../../../recoil/quizzs';
 import emotions from './Day5/emotions';
-import { Bold, P } from '../../../components/Articles';
-import TextStyled from '../../../components/TextStyled';
+import { Bold, P, Underlined } from '../../../components/Articles';
 
 const Defi2_Day7 = ({ navigation, route }) => {
   const isFocused = useIsFocused();
@@ -41,7 +41,7 @@ const Defi2_Day7 = ({ navigation, route }) => {
             <QButton content={1} disabled colorText="#ffffff" colorBorder="#4030A5" colorBackground=" #4030A5" />
           </QButtonCentered>
           <FirstTextContainer>
-            <TextStyled>{answersRiskSituations[0]?.content}</TextStyled>
+            <P>{answersRiskSituations[0]?.content}</P>
           </FirstTextContainer>
           <FirstTextContainer>
             <TextEmotion>Et je bois pour :</TextEmotion>
@@ -66,32 +66,30 @@ const Defi2_Day7 = ({ navigation, route }) => {
                     colorBackground=" #4030A5"
                   />
                   <TextContainer>
-                    <TextStyled>{riskSituation.content}</TextStyled>
+                    <P>{riskSituation.content}</P>
                   </TextContainer>
                 </RiskSituationsContainer>
               );
             })}
         </BackgroundEFEFEF>
-        <TextStyled>
-          Si vous êtes parvenu à <TextStyled bold>gérer la situation</TextStyled> dans le cadre de ce défi, vous êtes{' '}
-          <TextStyled bold>capable de trouver seul(e) des solutions pour faire autrement.</TextStyled>
+        <P>
+          Si vous êtes parvenu à <P bold>gérer la situation</P> dans le cadre de ce défi, vous êtes{' '}
+          <P bold>capable de trouver seul(e) des solutions pour faire autrement.</P>
           {'\n\n'}
-          <TextStyled bold>Sinon, vous avez pu mieux identifier quels sont vos points de blocage.</TextStyled>
+          <P bold>Sinon, vous avez pu mieux identifier quels sont vos points de blocage.</P>
           {'\n\n'}A tout moment: contactez nos équipes pour recourir à une aide complémentaire, gratuite et anonyme.
-        </TextStyled>
-        <ElementDayDefi content={<H2 color="#4030a5">Conclusion de ma semaine</H2>} />
-        <TextStyled bold>
-          Vous n'êtes pas la seule personne à ressentir ces émotions qui impactent sur l'envie de boire !
-        </TextStyled>
+        </P>
         <Spacer />
-        <TextStyled>
-          Chacun réagit différemment à l'alcool. Et les émotions peuvent aussi bien positives que négatives.
-        </TextStyled>
         <Spacer />
-        <TextStyled>
-          L'enquête du <TextStyled bold>British Medical Journal Open</TextStyled> montre que les émotions perçues
-          varient en fonction du type d'alcool consommé.
-        </TextStyled>
+        <ElementDayDefi content={<H2 color="#4030a5">Conclusion de ma semaine</H2>} noMarginBottom />
+        <P bold>Vous n'êtes pas la seule personne à ressentir ces émotions qui impactent sur l'envie de boire !</P>
+        <Spacer />
+        <P>Chacun réagit différemment à l'alcool. Et les émotions peuvent aussi bien positives que négatives.</P>
+        <Spacer />
+        <P>
+          L'enquête du <P bold>British Medical Journal Open</P> montre que les émotions perçues varient en fonction du
+          type d'alcool consommé.
+        </P>
         <Spacer />
         <BackgroundEFEFEF>
           <EmotionsConsommateurs
@@ -108,33 +106,45 @@ const Defi2_Day7 = ({ navigation, route }) => {
             emotion3="17 % tristes"
           />
           <Spacer />
-          <TextStyled>
-            L'<TextStyled bold>intensité des émotions augmente avec la vitesse et la quantité consommée</TextStyled>, et
-            plus particuliérement pour l'agressivité.
-          </TextStyled>
+          <P>
+            L'<P bold>intensité des émotions augmente avec la vitesse et la quantité consommée</P>, et plus
+            particuliérement pour l'agressivité.
+          </P>
           <Spacer />
-          <TextStyled>
+          <P>
             Bien évidemment, ces émotions peuvent être affectées par l'humeur du consommateur avant le premier verre.
-          </TextStyled>
+          </P>
         </BackgroundEFEFEF>
-        <TextStyled>
+        <P>
           Comme les deux faces d'une même pièce, l'alcool comporte des plaisirs mais aussi des effets négatifs. Alors
           quand vous choissiez de consommer :
-          <TextStyled bold>
+          <P bold>
             {'\n    • '}identifiez votre état émotionnel avant le premier verre,{'\n    • '} choisissez le type de
             boisson,
             {'\n    • '} sachez fixer une limite, {'\n    • '}veillez à boire lentement.
-          </TextStyled>
-        </TextStyled>
+          </P>
+        </P>
         <Spacer />
         <Sources
           content={
-            <TextStyled>
-              {'\n    • '}BMJ Open November 22 2017 DOI: 10.1136 bmjopen-2017-016089 {'\n    • '}Le site Améli “L'alcool
-              : définition et repères de consommation”
-              https://www.ameli.fr/assure/sante/themes/alcool-sante/definition-reperes-consommation {'\n    • '}OMS -
-              Organisation mondiale de la Santé
-            </TextStyled>
+            <>
+              <P>
+                {'\n    • '}BMJ Open November 22 2017 DOI: 10.1136 bmjopen-2017-016089 {'\n    • '}Le site Améli
+                “L'alcool : définition et repères de consommation”{'\n'}
+              </P>
+              <TouchableOpacity>
+                <Underlined
+                  color="#4030a5"
+                  onPress={() => {
+                    Linking.openURL(
+                      'https://www.ameli.fr/assure/sante/themes/alcool-sante/definition-reperes-consommation'
+                    );
+                  }}>
+                  https://www.ameli.fr/assure/sante/themes/alcool-sante/definition-reperes-consommation
+                </Underlined>
+              </TouchableOpacity>
+              <P>{'\n    • '}OMS - Organisation mondiale de la Santé</P>
+            </>
           }
         />
       </TopContainer>
@@ -161,8 +171,9 @@ const Spacer = styled.View`
 
 const BackgroundEFEFEF = styled.View`
   background-color: #efefef;
-  margin-horizontal: -20px;
-  padding: 20px;
+  margin-horizontal: -50px;
+  padding-horizontal: 50px;
+  padding-vertical: 20px;
   margin-bottom: 20px;
 `;
 
@@ -207,8 +218,6 @@ const RiskSituationsContainer = styled.View`
 const TextEmotion = styled(P)`
   text-align: center;
 `;
-
-const UnderlinedButton = styled.TouchableOpacity``;
 
 const EmotionsConsommateurs = ({ drinkCategorie, emotion1, emotion2, emotion3 }) => (
   <EmotionsContainer>
