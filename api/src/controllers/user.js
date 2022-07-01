29d41cdf4562a5f26b8299f361115283a778ccd2
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-07-01 04:14:21 +0200

    feat: activate push token sending to server

---
+++ api/src/controllers/user.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { catchErrors } = require("../middlewares/errors");
+const { capture } = require("../third-parties/sentry");
+const router = express.Router();
+const inappMessages = require("../in-app-messages");
+const newFeatures = require("../new-features");
+
+router.put(
+  "/",
+  catchErrors(async (req, res) => {
+    const { body } = req;
+    if (body.pushToken) capture("push token coming", { extra: body });
+    return res.status(200).send({ ok: true });
+  })
+);
+
+module.exports = router;
