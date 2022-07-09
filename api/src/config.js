7960b39a6aceee8d2499606f621bdd29d48233e8
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-07-09 10:25:26 +0200

    feat: setup prisma

--- api/src/config.js
+++ api/src/config.js
@@ -3,6 +3,12 @@ const { version, mobileAppVersion } = require("../package.json");
 const PORT = process.env.PORT || 3000;
 const ENVIRONMENT = process.env.ENVIRONMENT || process.env.NODE_ENV || "development";
 
+const PGHOST = process.env.PGHOST;
+const PGPORT = process.env.PGPORT;
+const PGUSER = process.env.PGUSER;
+const PGPASSWORD = process.env.PGPASSWORD || null;
+const PGDATABASE = process.env.PGDATABASE;
+
 const SENTRY_KEY = process.env.SENTRY_KEY || "https://b43d73353b7b48b8857deb69bca98da2@o348403.ingest.sentry.io/2213011";
 
 const VERSION = version;
@@ -10,6 +16,11 @@ const MOBILE_VERSION = mobileAppVersion;
 
 module.exports = {
   PORT,
+  PGHOST,
+  PGPORT,
+  PGUSER,
+  PGPASSWORD,
+  PGDATABASE,
   ENVIRONMENT,
   SENTRY_KEY,
   VERSION,
--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const inappMessages = require("../in-app-messages");
 const newFeatures = require("../new-features");
 const { sendPushNotification } = require("../services/push-notifications");
+const prisma = require("../prisma");
 
 router.put(
   "/",
@@ -15,6 +16,18 @@ router.put(
     // console.log({ ok: true, data: result });
     // return res.status(200).send({ ok: true, data: JSON.stringify(result, null, 2) });
     return res.status(200).send({ ok: true });
+  })
+);
+
+router.post(
+  "/",
+  catchErrors(async (req, res) => {
+    const newUser = await prisma.user.create({
+      data: {
+        matomo_id: "12345",
+      },
+    });
+    return res.status(200).send({ ok: true, data: newUser });
   })
 );
 
