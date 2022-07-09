311187f00bc32183ee829466b2c0baf832f09de4
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-07-09 12:03:46 +0200

    fix: log config

--- api/src/prisma.js
+++ api/src/prisma.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 module.exports = prisma;
