897fdeee7d51c9e7f84e512c49e26ed579a80757
Marin Bouanchaud <40020721+Marin2705@users.noreply.github.com>
2022-10-20 12:19:04 +0200

    Feat: défi 3 (frise + explications J1) (#108)
    * add defi 3 on Mes Defis
    
    * frise défi 3 & début J1
    
    * update picture Defi3
    
    * explications J1

---
+++ app/src/scenes/Defis/Defi3/defi3_Data.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import TextStyled from '../../../components/TextStyled';
+
+export const defi3_Data = [
+  {
+    actionType: 'content',
+    title: 'J1',
+    tagLine: "L'alcool en quelques chiffres",
+    description: (
+      <>
+        Comprendre le rôle et la place de l’alcool dans mon environnement, c’est aussi{' '}
+        <TextStyled bold>connaître les derniers chiffres.</TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        Comprendre le rôle et la place de l’alcool dans mon environnement, c’est aussi{' '}
+        <TextStyled bold>connaître les derniers chiffres.</TextStyled>
+      </>
+    ),
+    textCTA: 'Je commence',
+    screenCTA: 'DEFI3_DAY_1',
+  },
+  {
+    actionType: 'quizz',
+    title: 'J2',
+    tagLine: 'Comprendre les normes',
+    description: (
+      <>
+        <TextStyled bold>Renforcez</TextStyled> vos connaissances sur les “normes recommandées” avant de pousser plus
+        loin votre réflexion.
+      </>
+    ),
+    descriptionDone: (
+      <>
+        <TextStyled bold>Renforcez</TextStyled> vos connaissances sur les “normes recommandées” avant de pousser plus
+        loin votre réflexion.
+      </>
+    ),
+    textCTA: "Je m'informe",
+    screenCTA: 'DEFI2_DAY_2',
+  },
+  {
+    actionType: 'content',
+    title: 'J3',
+    tagLine: 'Identifier mes situations à risques',
+    description: (
+      <>
+        Identifiez vos “situations à risque” afin de réfléchir aux
+        <TextStyled bold> besoins que remplit l'alcool</TextStyled> dans ces occasions. C'est la première étapes pour
+        <TextStyled bold> trouver des solutions pour remplir ce besoin autrement.</TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        Identifiez vos “situations à risque” afin de réfléchir aux
+        <TextStyled bold> besoins que remplit l'alcool</TextStyled> dans ces occasions. C'est la première étapes pour
+        <TextStyled bold> trouver des solutions pour remplir ce besoin autrement.</TextStyled>
+      </>
+    ),
+    textCTA: 'Je reconnais mes situations',
+    screenCTA: 'DEFI2_DAY_3',
+  },
+  {
+    actionType: 'quizz',
+    title: 'J4',
+    tagLine: 'Hiérarchiser mes situations à risques',
+    description: (
+      <>
+        Toutes les situations à risque ne sont pas égales :
+        <TextStyled bold> classez-les par ordre de motivation à réduire votre consommation.</TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        <TextStyled bold>Remplissez vos consommations</TextStyled> tout au long de la journée et revenez
+        <TextStyled bold> demain</TextStyled> pour la suite de ce point de 7 jours.
+      </>
+    ),
+    textCTA: 'Je classe',
+    screenCTA: 'DEFI2_DAY_4',
+  },
+  {
+    actionType: 'content',
+    title: 'J5',
+    tagLine: 'Affronter une situation à risque',
+    description: (
+      <>
+        En plus de vous aider à <TextStyled bold>identifier vos forces</TextStyled> (les situations que vous contrôlez)
+        et vos faiblesses (celles à haut risque) il est important de vous aider à réflechir au
+        <TextStyled bold> pourquoi vous buvez dans cette situation ?</TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        En plus de vous aider à <TextStyled bold>identifier vos forces</TextStyled> (les situations que vous contrôlez)
+        et vos faiblesses (celles à haut risque) il est important de vous aider à réflechir au
+        <TextStyled bold> pourquoi vous buvez dans cette situation ?</TextStyled>
+      </>
+    ),
+    textCTA: 'Je suis prêt ',
+    screenCTA: 'DEFI2_DAY_5',
+  },
+  {
+    actionType: 'content',
+    title: 'J6',
+    tagLine: 'Savoir dire Non',
+    description: (
+      <>
+        <TextStyled bold>Préparez votre discours</TextStyled> pour dire “non” en situation : refuser un verre sans que
+        cela soit un refus social, du groupe. Sachez faire passer le message que vous
+        <TextStyled bold> refusez l'alcool et pas la personne !</TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        <TextStyled bold>Préparez votre discours</TextStyled> pour dire “non” en situation : refuser un verre sans que
+        cela soit un refus social, du groupe. Sachez faire passer le message que vous
+        <TextStyled bold> refusez l'alcool et pas la personne !</TextStyled>
+      </>
+    ),
+    textCTA: 'Je peux dire Non',
+    screenCTA: 'DEFI2_DAY_6',
+  },
+  {
+    actionType: 'content',
+    title: 'J7',
+    tagLine: 'Bilan de ma semaine ',
+    description: (
+      <>
+        Bravo, vous avez fait de nouveau un pas vers une meilleure connaissance du rôle addictif de l'alcool.{' '}
+        <TextStyled bold>
+          Vous n'êtes pas la seule personne à ressentir ses émotions qui impactent sur l'envie de boire !
+        </TextStyled>
+      </>
+    ),
+    descriptionDone: (
+      <>
+        Bravo, vous avez fait de nouveau un pas vers une meilleure connaissance du rôle addictif de l'alcool.{' '}
+        <TextStyled bold>
+          Vous n'êtes pas la seule personne à ressentir ses émotions qui impactent sur l'envie de boire !
+        </TextStyled>
+      </>
+    ),
+    textCTA: 'Je consulte mon bilan',
+    screenCTA: 'DEFI2_DAY_7',
+  },
+];
