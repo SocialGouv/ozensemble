0ec72041556ab45d6cef005a011b9c6b102f9b26
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2022-10-26 12:20:14 +0200

    J6

--- app/src/scenes/Defis/Defi3/Day6/index.js
+++ app/src/scenes/Defis/Defi3/Day6/index.js
@@ -1,25 +1,14 @@
 import { useIsFocused } from '@react-navigation/native';
 import React, { useEffect } from 'react';
 import { setValidatedDays } from '../../utils';
-import TextStyled from '../../../../components/TextStyled';
-import ButtonPrimary from '../../../../components/ButtonPrimary';
-import Element from '../../../../components/ElementDayDefi';
-import WrapperContainer from '../../../../components/WrapperContainer';
-import styled from 'styled-components';
 import { createStackNavigator } from '@react-navigation/stack';
-import { screenWidth } from '../../../../styles/theme';
-import { defaultPaddingFontScale } from '../../../../styles/theme';
-import H2 from '../../../../components/H2';
-import { P } from '../../../../components/Articles';
 import { riskSituationsAnswersKeysSelector } from '../../../../recoil/quizzs';
 import { defi2EmotionState } from '../../../../recoil/defis';
 import emotions from '../../../../components/emotions';
 import { useRecoilValue } from 'recoil';
-import { Spacer } from '../../../../components/Articles';
-import ArrowEmotion from '../../../../components/ArrowEmotion';
-import { View, TouchableOpacity } from 'react-native';
 import Doctolib from '../../../Health/Doctolib';
 import ContactForm from '../../../Health/ContactForm';
+import { Conseils, Explications } from './Screens';
 
 const Day6Stack = createStackNavigator();
 
@@ -30,7 +19,10 @@ const Defi3_Day6 = ({ navigation, route }) => {
   const emotion = emotions.find((e) => e.value === emotionValue);
   const answerSection = answersRiskSituations[0]?.answerKey.split('.')[0];
   const situationText = answerSection == 1 ? 'C’est une situation extérieure' : 'C’est une situation intérieure';
-
+  const conseil =
+    emotion?.value === 2
+      ? answersRiskSituations[0]?.consommationReasonPositiveEmotion
+      : answersRiskSituations[0]?.consommationReasonNegativeEmotion;
   useEffect(() => {
     if (route?.params?.inDefi3) setValidatedDays(route?.params?.day, '@Defi3');
   }, [route?.params, isFocused]);
@@ -42,157 +34,19 @@ const Defi3_Day6 = ({ navigation, route }) => {
       initialRouteName={route?.params?.initialRouteName}>
       <Day6Stack.Screen name="EXPLICATIONS">
         {({ navigation }) => (
-          <WrapperContainer
-            onPressBackButton={navigation.goBack}
-            noPaddingHorizontal
-            title="Aller plus loin pour maintenir ma motivation ">
-            <PaddingContainer>
-              <Element
-                content={
-                  <>
-                    <TextStyled>
-                      Nous allons nous pencher sur les raisons qui vous poussent à consommer. {'\n\n'}
-                      <TextStyled bold>C’est humain de perdre en motivation</TextStyled>, et nous allons vous donner des{' '}
-                      <TextStyled bold>conseils</TextStyled> pour continuer votre démarche de réduction. {'\n\n'}
-                      Pour personnaliser cet exercice, nous reprenons{' '}
-                      <TextStyled bold>votre situation du défi 2</TextStyled> :
-                    </TextStyled>
-                  </>
-                }
-              />
-            </PaddingContainer>
-
-            <SituationContainer>
-              <H2 color="#4030a5">Ma situation la plus facile à changer</H2>
-              <Spacer size={10} />
-              <FirstSituationContainer>
-                <FirstTextContainer>
-                  <P noMarginBottom>{answersRiskSituations[0]?.content}</P>
-                </FirstTextContainer>
-                <FirstTextContainer>
-                  <TextEmotion noMarginBottom>Et je bois pour :</TextEmotion>
-                  <Spacer size={10} />
-                  {emotion?.iconBilan}
-                  <Spacer size={10} />
-                  <TextEmotion noMarginBottom color="#4030A5">
-                    {emotion?.description}
-                  </TextEmotion>
-                </FirstTextContainer>
-              </FirstSituationContainer>
-            </SituationContainer>
-
-            <PaddingContainer>
-              <Element
-                content={
-                  <>
-                    <TextStyled>
-                      Les chercheurs ont étudié les raisons de consommer et ont avancé l’hypothèse que{' '}
-                      <TextStyled bold>nous buvons de l’alcool pour obtenir des effets spécifiques</TextStyled>, qui
-                      peuvent se classer selon deux dimensions : {'\n\n'}
-                    </TextStyled>
-                    <BulletPoint>
-                      <TextStyled>
-                        {'\u2022'} <TextStyled bold>l’état émotionnel</TextStyled> (positif ou négatif)
-                      </TextStyled>
-                    </BulletPoint>
-                    <BulletPoint>
-                      <TextStyled>
-                        {'\u2022'} <TextStyled bold>la situation</TextStyled> (intérieure ou extérieure)
-                      </TextStyled>
-                    </BulletPoint>
-                  </>
-                }
-              />
-              <ButtonPrimaryStyled
-                content="Je découvre les conseils"
-                widthSmall
-                onPress={() => navigation.navigate('CONSEILS')}
-              />
-            </PaddingContainer>
-          </WrapperContainer>
+          <Explications answersRiskSituations={answersRiskSituations} emotion={emotion} navigation={navigation} />
         )}
       </Day6Stack.Screen>
 
-      <Day6Stack.Screen name="CONSEILS">
+      <Day6Stack.Screen name="CONSEILS" emotion={emotion} situationText={situationText}>
         {({ navigation }) => (
-          <WrapperContainer
-            onPressBackButton={navigation.goBack}
-            noPaddingHorizontal
-            title="Aller plus loin pour maintenir ma motivation ">
-            <SituationContainer>
-              <H2 color="#4030a5">Ma situation la plus facile à changer</H2>
-              <Spacer size={10} />
-              <FirstSituationContainer>
-                <FirstTextContainer>
-                  <P noMarginBottom>{answersRiskSituations[0]?.content}</P>
-                </FirstTextContainer>
-                <Spacer size={10} />
-                <FirstTextContainer>
-                  <TextEmotion noMarginBottom>Et je bois pour :</TextEmotion>
-                  <Spacer size={10} />
-                  {emotion?.iconBilan}
-                  <Spacer size={10} />
-                  <TextEmotion noMarginBottom color="#4030A5">
-                    {emotion?.description}
-                  </TextEmotion>
-                </FirstTextContainer>
-              </FirstSituationContainer>
-
-              <ArrowContainer>
-                <ArrowEmotion />
-                <ArrowEmotion />
-              </ArrowContainer>
-
-              <FirstSituationContainer>
-                <FirstTextContainer>
-                  <Spacer size={10} />
-                  <TextStyled>{situationText}</TextStyled>
-                  <Spacer size={10} />
-                </FirstTextContainer>
-                <Spacer size={10} />
-                <FirstTextContainer>
-                  <Spacer size={10} />
-                  <TextStyled>
-                    {emotion?.value === 2
-                      ? 'C’est une émotion positive (traduisant de la joie)'
-                      : 'C’est une émotion négative (traduisant de la peur)'}
-                  </TextStyled>
-                  <Spacer size={10} />
-                </FirstTextContainer>
-              </FirstSituationContainer>
-            </SituationContainer>
-
-            <PaddingContainer>
-              <TextStyled bold color="#4030A5">
-                Ma raison de consommer{'\n'}
-              </TextStyled>
-              <TextStyled>conseil & motif ?? {'\n\n'}</TextStyled>
-              <TextStyled bold color="#DE285E">
-                Le conseil d’Oz Ensemble{'\n'}
-              </TextStyled>
-              <TextStyled>
-                Si l’alcool peut aider à gérer la tristesse ou la colère, il désinhibe et exacerbe également ces
-                émotions lorsque l’on devient ivre. Rappelez-vous que le verre qui vous aide à tenir au départ peut vous
-                pousser aussi à dire des choses à votre entourage que vous regretterez par la suite. Voire même ne pas
-                vous en souvenir ! {'\n\n\n'}
-                Si votre entourage vous fait du mal, dites-le leur simplement et calmez-vous autrement en vous isolant
-                un temps ou en parlant à une personne de confiance. Prenez le temps de parler des sujets qui fâchent
-                quand vous êtes apaisé et sobre.{'\n\n'}
-              </TextStyled>
-
-              <TouchableOpacity onPress={() => navigation.navigate('CONTACT')}>
-                <TextStyled bold>
-                  Si ces sujets sont trop difficiles à aborder seul, les conseils d’un{' '}
-                  <TextStyled bold underline color="#4030A5">
-                    psychologue
-                  </TextStyled>{' '}
-                  pourraient vous aider à gérer ces problèmes relationnels mieux que l’alcool.{'\n\n\n'}
-                </TextStyled>
-              </TouchableOpacity>
-
-              <ButtonPrimaryStyled content="J’ai compris" widthSmall onPress={() => navigation.navigate('CONSEILS')} />
-            </PaddingContainer>
-          </WrapperContainer>
+          <Conseils
+            answersRiskSituations={answersRiskSituations}
+            emotion={emotion}
+            situationText={situationText}
+            conseil={conseil}
+            navigation={navigation}
+          />
         )}
       </Day6Stack.Screen>
 
@@ -201,64 +55,5 @@ const Defi3_Day6 = ({ navigation, route }) => {
     </Day6Stack.Navigator>
   );
 };
-
-const ArrowContainer = styled.View`
-  flex-direction: row;
-  width: 45%;
-  justify-content: space-between;
-`;
-
-const PaddingContainer = styled.View`
-  padding-horizontal: ${defaultPaddingFontScale()}px;
-`;
-
-const SituationContainer = styled.View`
-  width: 100%;
-  heigth: 100px;
-  background-color: #efefef;
-  padding: 10px;
-  padding-bottom: 25px;
-  padding-top: 25px;
-  align-items: center;
-  margin-bottom: 30px;
-`;
-
-const BulletPoint = styled.View`
-  margin: 10px;
-`;
-
-const ButtonPrimaryStyled = styled(ButtonPrimary)`
-  margin-bottom: 50px;
-`;
-
-const FirstSituationContainer = styled.View`
-  flex-direction: row;
-  align-items: stretch;
-`;
-
-const FirstTextContainer = styled.View`
-  background-color: #ffffff;
-  border: 1px solid #d3d3e8;
-  border-radius: 3px;
-  width: ${screenWidth * 0.4 - 20}px;
-  padding: 7px;
-  align-items: center;
-  justify-content: center;
-  /* height: 100%; */
-`;
-
-const TextContainer = styled.View`
-  background-color: #ffffff;
-  border: 1px solid #d3d3e8;
-  border-radius: 3px;
-  margin-left: 10px;
-  margin-vertical: 10px;
-  padding: 10px;
-  width: ${screenWidth * 0.77 - 20}px;
-`;
-
-const TextEmotion = styled(P)`
-  text-align: center;
-`;
 
 export default Defi3_Day6;
--- app/src/scenes/Quizzs/QuizzRiskSituations/riskSituations.js
+++ app/src/scenes/Quizzs/QuizzRiskSituations/riskSituations.js
@@ -1,37 +1,288 @@
+import React from 'react';
+import TextStyled from '../../../components/TextStyled';
+
 const riskSituations = [
   {
     sectionTitle: 'extérieure',
     answers: [
       {
+        answerKey: '1.1',
+        content: "Quand je suis frustré(e) ou en colère contre quelqu'un",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche d’apaisement afin de rester en
+            relation avec votre groupe social. Vous cherchez à ne pas en être exclu(e) du fait de votre tempérament (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de transcender votre émotion
+            négative de colère en faisant la fête par exemple. Vous cherchez à vous abrutir, à oublier (
+            <TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
         answerKey: '1.2',
         content: "Quand ma relation avec quelqu'un de mon entourage m'inquiète ou me rend anxieux(se)",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de vous conformer au groupe
+            social. Vous cherchez à ne pas vous en sentir exclu(e) ou à de gérer avec l’alcool les émotions négatives
+            que vous renvoie votre entourage (<TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de vous sentir mieux face à
+            une relation à l’autre anxiogène (<TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
       },
-      { answerKey: '1.1', content: "Quand je suis frustré(e) ou en colère contre quelqu'un" },
-      { answerKey: '1.3', content: 'Quand ma journée se passe mal : stress, gestion du quotidien, frustration...' },
-      { answerKey: '1.4', content: "Quand j'ai envie de dire à quelqu'un ce que je pense vraiment" },
+      {
+        answerKey: '1.3',
+        content: 'Quand ma journée se passe mal : stress, gestion du quotidien, frustration...',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à la recherche de gestion de votre stress grâce
+            à l’alcool. Vous cherchez à pouvoir “tenir” ou ne pas “exploser” (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre tentative de contenir votre émotion
+            négative d’anxiété. Vous cherchez à vous abrutir, à oublier (<TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '1.4',
+        content: "Quand j'ai envie de dire à quelqu'un ce que je pense vraiment",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de vous conformer au groupe
+            social. Vous cherchez à ne pas vous en sentir exclu(e) et à ne pas faire du mal à l’autre (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de désinhibition, de sortir de
+            vous même pour mieux vous mettre en relation. Par exemple, pour aborder l’autre (
+            <TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
       {
         answerKey: '1.5',
         content: "Quand je suis avec certaines personnes et qu'on veut fêter, s'amuser ou s'évader ensemble",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer de l’alcool serait liée à votre recherche de vous conformer
+            au groupe social. Vous cherchez à ne pas vous en sentir exclu(e) (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à la recherche du plaisir et des situations
+            festives (<TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
       },
-      { answerKey: '1.6', content: "Quand j'ai besoin de courage d'affronter une situation que je redoute" },
-      { answerKey: '1.7', content: "Quand on m'annonce une mauvaise nouvelle ou une contrariété" },
-      { answerKey: '1.8', content: 'Quand on me propose un verre' },
+      {
+        answerKey: '1.6',
+        content: "Quand j'ai besoin de courage d'affronter une situation que je redoute",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de courage et d’apaisement de
+            la tension interne que vous ressentez (<TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre tentative de contenir votre émotion
+            négative d’anxiété. Vous cherchez à vous donner du courage (<TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '1.7',
+        content: "Quand on m'annonce une mauvaise nouvelle ou une contrariété",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de gestion par l’alcool des
+            émotions négatives liées à cette mauvaise nouvelle, afin d’aller moins mal ou d’anesthésier la douleur (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche du plaisir malgré les
+            obstacles qui se dressent devant vous. Vous cherchez à vous dépasser (
+            <TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '1.8',
+        content: 'Quand on me propose un verre',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche de vous conformer au groupe
+            social tout en apaisant votre tension interne. Il peut aussi parfois vous sembler plus facile de “vider
+            votre sac” ou de parler de vos difficultés si vous êtes alcoolisé (
+            <TextStyled bold>motif de conformité</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait liée à votre recherche du plaisir et des situations
+            festives. Vous ne souhaitez pas passer pour le rabat joie dans les fêtes (
+            <TextStyled bold>motif social</TextStyled>).
+          </TextStyled>
+        ),
+      },
     ],
   },
   {
     sectionTitle: 'intérieure',
     answers: [
-      { answerKey: '2.1', content: 'Quand je suis fâché(e) ou frustré(e) contre moi-même' },
-      { answerKey: '2.2', content: 'Quand je suis triste ou déprimé(e)' },
-      { answerKey: '2.3', content: 'Quand je suis stressé(e) ou anxieux(se)' },
-      { answerKey: '2.4', content: 'Quand je ne me sens pas bien physiquement' },
+      {
+        answerKey: '2.1',
+        content: 'Quand je suis fâché(e) ou frustré(e) contre moi-même',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt d’atténuer vos soucis et de mieux gérer vos
+            sentiments négatifs (<TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une recherche de sensation interne et de lâcher prise (
+            <TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '2.2',
+        content: 'Quand je suis triste ou déprimé(e)',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt d’atténuer vos soucis et de mieux gérer vos
+            sentiments négatifs (<TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une recherche de sensation interne et d’oubli (
+            <TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '2.3',
+        content: 'Quand je suis stressé(e) ou anxieux(se)',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt l’atténuation de vos soucis et la recherche
+            d’une gestion de vos sentiments négatifs (
+            <TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une recherche de sensation interne et de lâcher prise (
+            <TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '2.4',
+        content: 'Quand je ne me sens pas bien physiquement',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt l’atténuation de vos soucis et la recherche
+            d’une anesthésie de vos douleurs physiques (
+            <TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une recherche de sensation interne et d’anesthésie de vos douleurs
+            physiques (<TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
       {
         answerKey: '2.5',
         content: "Quand j'ai de la difficulté à m'endormir",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt l’atténuation de vos soucis et la recherche
+            d’une gestion vos sentiments négatifs (
+            <TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt de fuir le sommeil en utilisant l’alcool comme
+            un boost ou un remontant (<TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
       },
-      { answerKey: '2.6', content: 'Quand je veux me récompenser' },
-      { answerKey: '2.7', content: "Quand j'ai des pensées négatives vis à vis de moi ou de ma situation" },
-      { answerKey: '2.8', content: 'Quand je déguste une boisson alcoolisée dont le goût me plaît particulièrement' },
+      {
+        answerKey: '2.6',
+        content: 'Quand je veux me récompenser',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt de vous offrir une récompense alors que vous
+            avez le sentiment que votre situation est difficile (
+            <TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt la recherche de sensation interne et d’amusement (
+            <TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '2.7',
+        content: "Quand j'ai des pensées négatives vis-à-vis de moi ou de ma situation",
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt d’atténuer vos soucis et de mieux gérer vos
+            sentiments négatifs (<TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une tentative de gérer vos émotions négatives en les remplaçant par
+            l’oubli et l’ivresse (<TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
+      {
+        answerKey: '2.8',
+        content: 'Quand je déguste une boisson alcoolisée dont le goût me plaît particulièrement',
+        consommationReasonNegativeEmotion: (
+          <TextStyled>
+            Dans cette situation, votre raison de consommer serait plutôt initialement pour atténuer vos soucis mais
+            elle appelle secondairement à de la culpabilité et des envies de consommer irrépressibles (
+            <TextStyled bold>motif de coping / pour oublier ses soucis</TextStyled>).
+          </TextStyled>
+        ),
+        consommationReasonPositiveEmotion: (
+          <TextStyled>
+            Votre raison de consommer serait plutôt une recherche de sensation interne et d’amusement (
+            <TextStyled bold>motif de renforcement</TextStyled>).
+          </TextStyled>
+        ),
+      },
     ],
   },
 ];
