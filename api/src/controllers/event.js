342332186cfa3de2f7233a64dec6d626bf9fc39a
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-10-28 07:28:03 +0200

    fix(api): warn if try to send email withn old app

--- api/src/controllers/event.js
+++ api/src/controllers/event.js
@@ -16,6 +16,28 @@ router.post(
       return res.status(200).send({ ok: true, newFeatures: [newFeatures["new-defis"], newFeatures["new-articles"]] });
     }
 
+    const sendNPSEvent = body.event?.category === "NPS";
+    const exportDataEvent = body.event?.category === "EXPORT";
+    if (appversion < 99 && (exportDataEvent || sendNPSEvent)) {
+      return res.status(200).send({
+        ok: true,
+        sendInApp: [
+          "L'envoi d'email n'est plus disponible sur cette version d'application",
+          "Mettez à jour votre application !",
+          [
+            {
+              text: "Mettre à jour",
+              link:
+                appdevice === "ios"
+                  ? "https://apps.apple.com/us/app/oz-ensemble/id1498190343?ls=1"
+                  : "https://play.google.com/store/apps/details?id=com.addicto",
+            },
+          ],
+          { cancelable: true },
+        ],
+      });
+    }
+
     return res.status(200).send({ ok: true });
   })
 );
