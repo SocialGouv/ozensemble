e2f81d58d26bb11162acf496622586e949979ffe
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-10-28 15:14:30 +0200

    fix(api): warn if try to send email withn old app (#115)
    * fix(api): warn if try to send email withn old app
    
    * fix: req.headers
    
    * fix req.headers.appdevice
    
    * fix event Export
    
    * update package api
    
    Co-authored-by: Marin2705 <40020721+Marin2705@users.noreply.github.com>

--- api/src/controllers/event.js
+++ api/src/controllers/event.js
@@ -16,6 +16,29 @@ router.post(
       return res.status(200).send({ ok: true, newFeatures: [newFeatures["new-defis"], newFeatures["new-articles"]] });
     }
 
+    const sendNPSEvent = body.event?.category === "NPS";
+    const exportDataEvent = body.event?.action === "EXPORT";
+    if (req.headers.appversion < 99 && (exportDataEvent || sendNPSEvent)) {
+      return res.status(200).send({
+        ok: true,
+        sendInApp: [
+          "L'envoi d'email n'est plus disponible sur cette version d'application",
+          "Mettez à jour votre application !",
+          [
+            {
+              text: "Mettre à jour",
+              link:
+                req.headers.appdevice === "ios"
+                  ? "https://apps.apple.com/us/app/oz-ensemble/id1498190343?ls=1"
+                  : "https://play.google.com/store/apps/details?id=com.addicto",
+            },
+            { text: "Plus tard", style: "cancel" },
+          ],
+          { cancelable: true },
+        ],
+      });
+    }
+
     return res.status(200).send({ ok: true });
   })
 );
