a95558ac45f743a894f443cc12dd0ed0da3ec9fd
vi-dot <Vi-dot@users.noreply.github.com>
2022-10-29 13:05:35 +0200

    Update OMSIllustationsManAndWoman.js

--- app/src/components/OMSIllustationsManAndWoman.js
+++ app/src/components/OMSIllustationsManAndWoman.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { screenWidth } from '../styles/theme';
 import TextStyled from './TextStyled';
-import ManAndWoman from './illustrations/ManAndWoman';
+import ManAndWoman from '../components/illustrations/ManAndWoman';
 
 const OMSIllustationsManAndWoman = ({}) => {
   return (
