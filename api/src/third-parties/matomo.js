6adf232a017426948409baf30e2723f3770eb71e
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2022-11-08 10:24:28 +0100

    fix: matmom in backend (#125)

--- api/src/third-parties/matomo.js
+++ api/src/third-parties/matomo.js
@@ -4,12 +4,11 @@ const { MATOMO_URL, MATOMO_IDSITE_1 } = require("../config");
 
 const __DEV__ = process.env.NODE_ENV === "development";
 
-class Api {
-  init({ baseUrl, idsite, userId, _idvc }) {
+class _MatomoBackend {
+  init({ baseUrl, idsite, _idvc }) {
     this.baseUrl = baseUrl;
     this.idsite = idsite;
-    this.userId = userId;
-    this._idvc = _idvc;
+    // this._idvc = _idvc;
     this.initDone = true;
   }
 
@@ -19,8 +18,8 @@ class Api {
   }
 
   makeid(length = 16) {
-    var result = '';
-    var characters = '01234567890abcdefABCDEF';
+    var result = "";
+    var characters = "01234567890abcdefABCDEF";
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -47,11 +46,9 @@ class Api {
   computeParams(params, idsite) {
     params = {
       idsite,
-      _id: this.userId,
-      uid: this.userId,
       rec: 1,
       rand: Date.now(),
-      _idvc: this._idvc,
+      // _idvc: this._idvc,
       ...params,
       //...this.computeCustomDimensions(this.dimensions),
     };
@@ -59,10 +56,10 @@ class Api {
       const computedParam = `${key}=${params[key]}`;
       if (index === 0) return computedParam;
       return `${paramString}&${computedParam}`;
-    }, '');
+    }, "");
   }
 
-  async logEvent({ category, action, name = '', value = null }) {
+  async logEvent({ userId, category, action, name = "", value = null }) {
     // e_c — The event category. Must not be empty. (eg. Videos, Music, Games...)
     // e_a — The event action. Must not be empty. (eg. Play, Pause, Duration, Add Playlist, Downloaded, Clicked...)
     // e_n — The event name. (eg. a Movie name, or Song name, or File name...)
@@ -70,15 +67,17 @@ class Api {
     const params = {
       e_c: category,
       e_a: action,
+      uid: userId,
+      _id: userId,
     };
-    if (name !== '') params.e_n = name;
+    if (name !== "") params.e_n = name;
     if (value !== null && !isNaN(Number(value))) params.e_v = Number(value);
     await this.execute(params);
   }
 
   async execute(params) {
     try {
-      if (!this.initDone) throw new Error('matomo not initialized yet');
+      if (!this.initDone) throw new Error("matomo not initialized yet");
       const url = `${this.baseUrl}?${this.computeParams(params, this.idsite)}`;
       if (__DEV__) {
         // console.log(params, this.dimensions);
@@ -88,7 +87,7 @@ class Api {
 
       if (__DEV__ && res.status !== 200) {
         console.log(res);
-        throw new Error('error fetching matomo');
+        throw new Error("error fetching matomo");
       }
 
       const url2 = `${this.baseUrl2}?${this.computeParams(params, this.idsite2)}`;
@@ -99,21 +98,20 @@ class Api {
       // }
       if (__DEV__ && res2.status !== 200) {
         console.log(res);
-        throw new Error('error fetching matomo');
+        throw new Error("error fetching matomo");
       }
     } catch (e) {
       if (__DEV__) {
-        console.log('matomo error', e);
+        console.log("matomo error", e);
       }
     }
   }
 }
 
-const matomo = new Api();
+const matomo = new _MatomoBackend();
 matomo.init({
   baseUrl: MATOMO_URL,
   idsite: MATOMO_IDSITE_1,
-  userId: "api",
 });
 
 module.exports = matomo;
