d21455a8c259e39bf4181922b8c00a73670bbfc2
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2022-11-08 10:40:56 +0100

    timestamp in route params

--- app/src/scenes/AddDrink/AddDrinkNavigator.js
+++ app/src/scenes/AddDrink/AddDrinkNavigator.js
@@ -4,10 +4,10 @@ import ConsosList from './ConsosList';
 import ChoiceDrinkOrNoDrink from './ChoiceDrinkOrNoDrink';
 
 const AddDrinkStack = createStackNavigator();
-const AddDrinkNavigator = () => {
+const AddDrinkNavigator = ({ route }) => {
   return (
     <AddDrinkStack.Navigator headerMode="none" mode="modal" initialRouteName="CHOICE_DRINK_OR_NO_DRINK">
-      <AddDrinkStack.Screen name="CHOICE_DRINK_OR_NO_DRINK">
+      <AddDrinkStack.Screen name="CHOICE_DRINK_OR_NO_DRINK" initialParams={{ timestamp: route.params.timestamp }}>
         {(props) => <ChoiceDrinkOrNoDrink {...props} />}
       </AddDrinkStack.Screen>
       <AddDrinkStack.Screen name="CONSOS_LIST">{(props) => <ConsosList {...props} />}</AddDrinkStack.Screen>
