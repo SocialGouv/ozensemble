8b4add5a75ad95f02f41c0e96145c0c2f0282e40
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-12-06 08:09:19 +0100

    fix: error in notifications

--- api/src/services/push-notifications.js
+++ api/src/services/push-notifications.js
@@ -44,14 +44,13 @@ const sendPushNotification = async ({ matomoId, pushNotifToken, title, body, lin
           userId: matomoId,
         });
       } else if (results[0]?.failure) {
-        capture(`push notification sent failure: ${results[0].message?.[0]?.errorMsg}`, { extra: { results } });
+        capture(`push notification sent failure: ${results[0].message?.[0]?.errorMsg}`, { extra: { results, data, pushNotifToken } });
         await matomo.logEvent({
           category: "PUSH_NOTIFICATION_SEND",
           action: "FAILED",
           name: results[0].message?.[0]?.errorMsg,
           userId: matomoId,
         });
-        capture(e, { extra: { users, payload } });
         return { ok: false, results };
       }
     }
