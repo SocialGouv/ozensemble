8bd5cc6bc9c2bdcd5a67ef60a65af6774eb8cdb0
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2022-12-20 16:11:38 +0100

    redirect qr codes

---
+++ api/src/controllers/qr.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { catchErrors } = require("../middlewares/errors");
+const router = express.Router();
+const matomo = require("../third-parties/matomo");
+
+router.get(
+  "/playstore",
+  catchErrors(async (req, res) => {
+    await matomo.logEvent({
+      category: "QR",
+      action: "PLAYSTORE",
+    });
+    return res
+      .writeHead(307, {
+        Location: `https://play.google.com/store/apps/details?id=com.addicto`,
+      })
+      .end();
+  })
+);
+
+router.get(
+  "/appstore",
+  catchErrors(async (req, res) => {
+    await matomo.logEvent({
+      category: "QR",
+      action: "APPSTORE",
+    });
+    return res
+      .writeHead(307, {
+        Location: `https://apps.apple.com/fr/app/oz-ensemble/id1498190343`,
+      })
+      .end();
+  })
+);
+
+module.exports = router;
