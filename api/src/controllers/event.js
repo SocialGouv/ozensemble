9ab3eff9995661c5f51cff7e1cbdc9283ed81314
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-01-02 11:31:22 +0100

    add old versions

--- api/src/controllers/event.js
+++ api/src/controllers/event.js
@@ -16,6 +16,30 @@ router.post(
       return res.status(200).send({ ok: true, newFeatures: [newFeatures["new-defis"], newFeatures["new-articles"]] });
     }
 
+    // old versions
+    const sendNPSEvent = body.event?.category === "NPS";
+    const exportDataEvent = body.event?.action === "EXPORT";
+    if (req.headers.appversion < 99 && (exportDataEvent || sendNPSEvent)) {
+      return res.status(200).send({
+        ok: true,
+        sendInApp: [
+          "L'envoi d'email n'est plus disponible sur cette version d'application",
+          "Mettez à jour votre application !",
+          [
+            {
+              text: "Mettre à jour",
+              link:
+                req.headers.appdevice === "ios"
+                  ? "https://apps.apple.com/us/app/oz-ensemble/id1498190343?ls=1"
+                  : "https://play.google.com/store/apps/details?id=com.addicto",
+            },
+            { text: "Plus tard", style: "cancel" },
+          ],
+          { cancelable: true },
+        ],
+      });
+    }
+
     if (body.event.category === "NAVIGATION" && body.event.action === "GAINS_MAIN_VIEW")
       return res.status(200).send({ ok: true, newFeatures: [newFeatures["new-gains"]] });
     if (body.event.category === "NAVIGATION" && body.event.action === "DEFIS_MENU")
@@ -27,35 +51,6 @@ router.post(
 
     // default : show new-gains (default page on startup)
     return res.status(200).send({ ok: true, newFeatures: [newFeatures["new-gains"]] });
-
-    // if (body.event.category === "APP" && body.event.action === "APP_OPEN") {
-    //   await new Promise((res) => setTimeout(res, 1000)); // maybe better for showing up on time
-    // }
-
-    // const sendNPSEvent = body.event?.category === "NPS";
-    // const exportDataEvent = body.event?.action === "EXPORT";
-    // if (req.headers.appversion < 99 && (exportDataEvent || sendNPSEvent)) {
-    //   return res.status(200).send({
-    //     ok: true,
-    //     sendInApp: [
-    //       "L'envoi d'email n'est plus disponible sur cette version d'application",
-    //       "Mettez à jour votre application !",
-    //       [
-    //         {
-    //           text: "Mettre à jour",
-    //           link:
-    //             req.headers.appdevice === "ios"
-    //               ? "https://apps.apple.com/us/app/oz-ensemble/id1498190343?ls=1"
-    //               : "https://play.google.com/store/apps/details?id=com.addicto",
-    //         },
-    //         { text: "Plus tard", style: "cancel" },
-    //       ],
-    //       { cancelable: true },
-    //     ],
-    //   });
-    // }
-
-    // return res.status(200).send({ ok: true });
   })
 );
 
