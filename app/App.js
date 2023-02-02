d031bc524e127504ec77b4760721cd58b5fa602f
Marin Bouanchaud <40020721+Marin2705@users.noreply.github.com>
2023-02-02 16:27:14 +0100

    fix: sentry context device name (#146)
    * fix: sentry context device name
    
    * fix contexts path in event object

--- app/App.js
+++ app/App.js
@@ -33,7 +33,16 @@ dayjs.extend(isSameOrAfter);
 dayjs.extend(weekday);
 
 if (!__DEV__) {
-  Sentry.init({ dsn: SENTRY_XXX });
+  Sentry.init({
+    dsn: SENTRY_XXX,
+    beforeSend(event) {
+      if (event.contexts?.device?.name) {
+        // Don't send user's device name
+        delete event.contexts.device.name;
+      }
+      return event;
+    },
+  });
 }
 
 const App = () => {
