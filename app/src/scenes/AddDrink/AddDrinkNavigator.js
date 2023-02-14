84a6387c98da65dfac7af477fd6471a530d6e827
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-02-14 16:50:28 +0100

    fix deeplink ADD_DRINK

--- app/src/scenes/AddDrink/AddDrinkNavigator.js
+++ app/src/scenes/AddDrink/AddDrinkNavigator.js
@@ -6,7 +6,7 @@ const AddDrinkStack = createStackNavigator();
 const AddDrinkNavigator = ({ route }) => {
   return (
     <AddDrinkStack.Navigator headerMode="none" mode="modal" initialRouteName="CONSOS_LIST">
-      <AddDrinkStack.Screen initialParams={{ timestamp: route.params.timestamp }} name="CONSOS_LIST">
+      <AddDrinkStack.Screen initialParams={{ timestamp: route?.params?.timestamp }} name="CONSOS_LIST">
         {(props) => <ConsosList {...props} />}
       </AddDrinkStack.Screen>
     </AddDrinkStack.Navigator>
