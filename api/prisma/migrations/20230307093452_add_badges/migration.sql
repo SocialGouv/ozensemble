0982c2aed6ec2e2fa04907e08e5d4bbda86bfa8b
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-03-07 10:43:25 +0100

    fix: prisma migration

---
+++ api/prisma/migrations/20230307093452_add_badges/migration.sql
@@ -0,0 +1,38 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "announcementModals" TEXT[] DEFAULT ARRAY[]::TEXT[];
+
+-- CreateTable
+CREATE TABLE "Badge" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "stars" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Badge_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Consommation" (
+    "id" TEXT NOT NULL,
+    "drinkKey" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT '',
+    "quantity" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "doses" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "kcal" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "price" DOUBLE PRECISION NOT NULL DEFAULT 1,
+    "volume" TEXT NOT NULL DEFAULT '',
+    "userId" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Consommation_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Badge" ADD CONSTRAINT "Badge_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Consommation" ADD CONSTRAINT "Consommation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
