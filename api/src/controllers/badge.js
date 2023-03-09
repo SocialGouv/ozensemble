f66f5405101eef164e80826eac2db5ecdf0f30e1
Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>
2023-03-09 12:38:07 +0100

    no cta for new user

--- api/src/controllers/badge.js
+++ api/src/controllers/badge.js
@@ -17,17 +17,28 @@ router.get(
   "/:matomoId",
   catchErrors(async (req, res) => {
     const { matomoId } = req.params;
-
     if (!matomoId) return res.status(200).send({ ok: true, data: [] });
-
+    let user = await prisma.user.findUnique({ where: { matomo_id: matomoId } });
+    console.log("user", user);
+    let CTAContent;
+    if (user) {
+      CTAContent = "Voir mes badges";
+    } else {
+      CTAContent = null;
+      user = await prisma.user.create({
+        data: {
+          matomo_id: matomoId,
+        },
+      });
+    }
     // find badges of matomoId
-    const user = await prisma.user.upsert({
-      where: { matomo_id: matomoId },
-      create: {
-        matomo_id: matomoId,
-      },
-      update: {},
-    });
+    // const user = await prisma.user.upsert({
+    //   where: { matomo_id: matomoId },
+    //   create: {
+    //     matomo_id: matomoId,
+    //   },
+    //   update: {},
+    // });
 
     const badges = await prisma.badge.findMany({
       where: {
@@ -36,22 +47,22 @@ router.get(
     });
 
     let announcementModal = null;
-
     if (!user.announcementModals.includes("@NewBadgesAnnouncementFeatures3")) {
+      console.log(user);
       announcementModal = {
         id: "@NewBadgesAnnouncementFeatures3",
         badgesCategories: ["drinks", "goals"], // "defis", "articles"
         title: "Nouveau\u00A0: les badges arrivent dans l'application\u00A0!",
         description:
           "Gagnez des badges symboliques en ajoutant vos consommations tous les jours ou en atteignant votre objectif de la semaine\u00A0!",
-        CTAButton: "Voir mes badges",
+        CTAButton: CTAContent,
         CTANavigation: ["BADGES_LIST"],
       };
       await prisma.user.update({
         where: { id: user.id },
         data: {
           announcementModals: {
-            set: [...user.announcementModals, "@NewBadgesAnnouncementFeatures"],
+            set: [...user.announcementModals, "@NewBadgesAnnouncementFeatures3"],
           },
         },
       });
