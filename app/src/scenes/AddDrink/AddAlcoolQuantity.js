94c4e75d6a7ec1806a7730e18baec1a10ee80e94
Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>
2023-04-06 16:36:52 +0200

    fix: same display as perviuous page

--- app/src/scenes/AddDrink/AddAlcoolQuantity.js
+++ app/src/scenes/AddDrink/AddAlcoolQuantity.js
@@ -13,34 +13,38 @@ import API from '../../services/api';
 import { alcoolQuantityCatalog } from './alcoolQuantityCatalog';
 const AddAlcoolQuantity = ({ navigation, handleQuantitySelection }) => {
   return (
-    <View className="h-full bg-white pt-5">
-      <ScrollView className="pb-5" style={{ padding: defaultPaddingFontScale() }}>
-        <BackButton content="Retour" bold onPress={() => navigation.goBack()} />
-        <H2 color="#4030a5" className="mt-5 mb-4">
-          Sélectionnez une quantité d'alcool
-        </H2>
-        <TextStyled italic className="text-xs mb-4">
-          Cliquez sur un contenant pour compléter le champ
-        </TextStyled>
-        {alcoolQuantityCatalog.map((quantity) => {
-          return (
-            <TouchableOpacity
-              key={quantity.name}
-              className="flex flex-row bg-[#F3F3F6] h-12 mb-3 rounded-lg border border-[#DBDBE8] items-center px-2"
-              onPress={() => {
-                handleQuantitySelection(quantity.volume);
-              }}>
-              <quantity.icon size={30} />
-              <View className="flex flex-row flex-wrap ml-2 w-10/12">
-                <TextStyled bold className="">
-                  {quantity.name} :{' '}
-                </TextStyled>
-                <Text>{quantity.volume}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
-      </ScrollView>
+    <View className="h-full bg-white py-10">
+      <View className="bg-white rounded-xl mt-auto absolute bottom-0 w-full h-full shadow-xl shadow-[#5E5E5E]">
+        <ScrollView style={{ padding: defaultPaddingFontScale() }}>
+          <BackButton content="Retour" bold onPress={() => navigation.goBack()} />
+          <H2 color="#4030a5" className="mt-5 mb-4">
+            Sélectionnez une quantité d'alcool
+          </H2>
+          <TextStyled italic className="text-xs mb-4">
+            Cliquez sur un contenant pour compléter le champ
+          </TextStyled>
+          <View className="mb-10">
+            {alcoolQuantityCatalog.map((quantity) => {
+              return (
+                <TouchableOpacity
+                  key={quantity.name}
+                  className="flex flex-row bg-[#F3F3F6] h-12 mb-3 rounded-lg border border-[#DBDBE8] items-center px-2"
+                  onPress={() => {
+                    handleQuantitySelection(quantity.name, quantity.vol);
+                  }}>
+                  <quantity.icon size={30} />
+                  <View className="flex flex-row flex-wrap ml-2 w-10/12">
+                    <TextStyled bold className="">
+                      {quantity.name} :{' '}
+                    </TextStyled>
+                    <Text>{quantity.volume}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 };
