909db37046dd8c0217b4b91411cf483a316d4de4
Axel <100227882+axelgcd@users.noreply.github.com>
2023-04-11 16:31:13 +0200

    fix: link add quantity (#243)
    Co-authored-by: Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>
    Co-authored-by: Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>

--- app/src/components/DrinkPersonalisation.js
+++ app/src/components/DrinkPersonalisation.js
@@ -5,12 +5,16 @@ import { TextInput } from 'react-native-gesture-handler';
 import TextStyled from './TextStyled';
 import ArrowDown from './ArrowDown';
 import { QuantitySetter } from './DrinkQuantitySetter';
+import ButtonPrimary from './ButtonPrimary';
 
-const DrinkPersonalisation = () => {
-  const [quantity, setQuantity] = useState(0);
+const DrinkPersonalisation = ({ navigation, quantitySelected, setQuantitySelected }) => {
   const onSetQuantity = (q) => {
     setQuantity(q);
   };
+  const [drinkName, setDrinkName] = useState('');
+  const [drinkPrice, setDrinkPrice] = useState(0);
+  const [drinkAlcoolPercentage, setDrinkAlcoolPercentage] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   return (
     <View>
       <View>
@@ -19,15 +23,27 @@ const DrinkPersonalisation = () => {
           <TextInput
             className="bg-[#F3F3F6] h-14 rounded-lg border border-[#DBDBE9] text-[#4030A5] px-4 my-2"
             placeholder="Bière forte, verre de vin au bar..."
+            value={drinkName}
+            onChangeText={(value) => setDrinkName(value)}
           />
           <Text className="text-xs">(30 caractères max)</Text>
         </View>
         <View className="mb-4">
-          <TextStyled bold>Quantité d’alcool servie (cl)</TextStyled>
-          <TouchableOpacity className="bg-[#f3f3f6] h-14 rounded-lg border border-[#dbdbe9] px-4 my-2 flex flex-row justify-between items-center">
-            <Text className="text-[#CACACD] flex">Sélectionnez une quantité</Text>
-            <ArrowDown color="#000000" size={30} strokeWidth={2} />
-          </TouchableOpacity>
+          <TextStyled bold>Quantité d'alcool servie (cl)</TextStyled>
+          {!quantitySelected?.volume ? (
+            <TouchableOpacity
+              className="bg-[#f3f3f6] h-14 rounded-lg border border-[#dbdbe9] px-4 my-2 flex flex-row justify-between items-center"
+              onPress={() => navigation.navigate('ADD_QUANTITY')}>
+              <Text className="text-[#CACACD] flex">Sélectionnez une quantité</Text>
+              <ArrowDown color="#000000" size={30} strokeWidth={2} />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              className="bg-[#f3f3f6] h-14 rounded-lg border border-[#dbdbe9] px-4 my-2 flex flex-row justify-between items-center"
+              onPress={() => navigation.navigate('ADD_QUANTITY')}>
+              <Text className="text-[#4030A5] flex">{quantitySelected?.volume?.split(' ')[0]}</Text>
+            </TouchableOpacity>
+          )}
         </View>
         <View className="flex flex-row justify-between mb-8">
           <View className="basis-1/2 pr-2">
@@ -35,6 +51,8 @@ const DrinkPersonalisation = () => {
             <TextInput
               className="bg-[#F3F3F6] h-14 rounded-lg border border-[#DBDBE9] text-[#4030A5] px-4 my-2"
               placeholder="Degrés"
+              value={drinkAlcoolPercentage}
+              onChangeText={(value) => setDrinkAlcoolPercentage(value)}
             />
           </View>
           <View className="basis-1/2 pl-2">
@@ -42,12 +60,25 @@ const DrinkPersonalisation = () => {
             <TextInput
               className="bg-[#F3F3F6] h-14 rounded-lg border border-[#DBDBE9] text-[#4030A5] px-4 my-2"
               placeholder="Euros"
+              value={drinkPrice}
+              onChangeText={(value) => setDrinkPrice(value)}
             />
           </View>
         </View>
         <View className="flex flex-row justify-between items-center flex-wrap gap-y-6">
           <TextStyled bold>Quantité bue aujourd'hui</TextStyled>
           <QuantitySetter quantity={quantity} onSetQuantity={onSetQuantity} />
+        </View>
+        <View className="flex flex-row justify-center mt-14 mb-10 ">
+          <ButtonPrimary
+            content="Créer ma boisson"
+            onPress={() => {
+              setQuantitySelected(null);
+            }}
+            disabled={
+              drinkPrice === '' || drinkAlcoolPercentage === '' || drinkName === '' || !quantitySelected?.volume
+            }
+          />
         </View>
       </View>
     </View>
--- app/src/scenes/AddDrink/AddOwnDrink.js
+++ app/src/scenes/AddDrink/AddOwnDrink.js
@@ -7,18 +7,24 @@ import H1 from '../../components/H1';
 import SwitchButtons from '../../components/SwitchButtons';
 import DrinkPersonalisation from '../../components/DrinkPersonalisation';
 import CocktailPersonalisation from '../../components/CocktailPersonalisation';
-import ButtonPrimary from '../../components/ButtonPrimary';
 import { ScrollView } from 'react-native-gesture-handler';
 
-const AddOwnDrink = ({ navigation }) => {
+const AddOwnDrink = ({ navigation, quantitySelected, setQuantitySelected }) => {
   const [switchPosition, setSwitchPosition] = useState(0);
+
   if (switchPosition === 0) {
     return (
       <View className="h-full bg-white py-10">
         <View className="bg-white rounded-xl mt-auto absolute bottom-0 w-full h-full shadow-xl shadow-[#5E5E5E]">
           <ScrollView>
             <View style={{ paddingHorizontal: defaultPaddingFontScale(), paddingTop: defaultPaddingFontScale() }}>
-              <BackButton content="Retour" bold onPress={() => navigation.goBack()} />
+              <BackButton
+                content="Retour"
+                bold
+                onPress={() => {
+                  navigation.goBack();
+                }}
+              />
               <H1 className="mt-5 mb-8">Créez une nouvelle boisson</H1>
               <View className="mb-8 flex flex-row justify-between items-center">
                 <Text className="font-bold text-lg">{'Est-ce un cocktail\u00A0?'}</Text>
@@ -29,14 +35,10 @@ const AddOwnDrink = ({ navigation }) => {
                   handleSwitchChange={setSwitchPosition}
                 />
               </View>
-              <DrinkPersonalisation />
-            </View>
-            <View className="flex flex-row justify-center mt-14 mb-10 ">
-              <ButtonPrimary
-                content="Créer ma boisson"
-                onPress={() => {
-                  // Todo: add drink to database
-                }}
+              <DrinkPersonalisation
+                navigation={navigation}
+                quantitySelected={quantitySelected}
+                setQuantitySelected={setQuantitySelected}
               />
             </View>
           </ScrollView>
@@ -56,14 +58,6 @@ const AddOwnDrink = ({ navigation }) => {
               <SwitchButtons handleSwitchChange={setSwitchPosition} />
             </View>
             <CocktailPersonalisation navigation={navigation} />
-          </View>
-          <View className="flex flex-row justify-center mt-14 mb-10">
-            <ButtonPrimary
-              content="Créer mon cocktail"
-              onPress={() => {
-                // Todo: add drink to database
-              }}
-            />
           </View>
         </ScrollView>
       </View>
