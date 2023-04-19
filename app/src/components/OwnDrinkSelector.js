781f300fb4e3545765307c4d237e08fb751acfab
Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>
2023-04-19 14:06:09 +0200

    fix: automation migration

--- app/src/components/OwnDrinkSelector.js
+++ app/src/components/OwnDrinkSelector.js
@@ -4,14 +4,14 @@ import { QuantitySetter } from './DrinkQuantitySetter';
 import TextStyled from './TextStyled';
 
 const OwnDrinkSelector = ({ drink, quantity, setDrinkQuantityRequest, onUpdateDrink }) => {
-  const { drinkKey, volume, doses, alcoolPercentage, categoryKey } = drink;
+  const { drinkKey, volume, doses, alcoolPercentage, categoryKey, displayDrinkModal } = drink;
 
   return (
     <View className="flex flex-row justify-center mb-2 px-4">
       <View className="w-full">
         <View className="bg-white rounded-md border border-[#DFDFEB] flex flex-row flex-wrap justify-between px-3 py-1 ">
           <TouchableOpacity className="mr-2 my-2" onPress={onUpdateDrink}>
-            <TextStyled bold>{drinkKey}</TextStyled>
+            <TextStyled bold>{displayDrinkModal}</TextStyled>
             {categoryKey === 'ownCocktail' ? (
               <Text className="text-sm">
                 {doses} {doses <= 1 ? 'unité' : 'unités'}
