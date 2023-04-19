e97db34f80a69cfee5077a5b3c93d11bc9ecf1f6
Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>
2023-04-19 10:38:36 +0200

    fix: add back button to CGU and InfosLegales

--- app/src/scenes/Infos/CGUs.js
+++ app/src/scenes/Infos/CGUs.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import { StyleSheet, Dimensions, View } from 'react-native';
 import Pdf from 'react-native-pdf';
+import BackButton from '../../components/BackButton';
+import { defaultPaddingFontScale } from '../../styles/theme';
 
 const pdfUrl = 'https://ozensemble.fr/files/25012023-Oz_Ensemble-CGU_app_V4.pdf';
 
 const CGUs = ({ onClose }) => (
-  <View style={styles.container}>
+  <View className="flex-1 justify-start items-center mt-6">
+    <View className="flex flex-row w-full mb-4" style={{ paddingHorizontal: defaultPaddingFontScale() }}>
+      <BackButton onPress={onClose} />
+    </View>
     <Pdf
       trustAllCerts={false}
       source={{
         uri: pdfUrl,
         cache: true,
       }}
-      style={styles.pdf}
+      className="flex-1 w-screen h-screen"
     />
   </View>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
 
 export default CGUs;
