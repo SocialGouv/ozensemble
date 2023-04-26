d24618cdb678b820de8b9965159fe8a60698e067
Axel <100227882+axelgcd@users.noreply.github.com>
2023-04-26 16:45:43 +0200

    feat:  compute calendar color drinks (#302)
    Co-authored-by: Axel Guichard <axelguichard@MacBook-Air-de-Axel.local>

--- app/src/components/Calendar.js
+++ app/src/components/Calendar.js
@@ -1,8 +1,13 @@
 import dayjs from 'dayjs';
 import React, { useMemo, useState } from 'react';
 import { Text, View, TouchableOpacity, Dimensions, PixelRatio } from 'react-native';
+import { useRecoilValue } from 'recoil';
+import { dailyDosesSelector } from '../recoil/consos';
+import { totalDrinksByDrinkingDaySelector, daysWithGoalNoDrinkState } from '../recoil/gains';
 import ArrowLeft from './ArrowLeft';
 import ArrowRight from './ArrowRight';
+import LegendStar from './illustrations/icons/LegendStar';
+
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 const Calendar = () => {
@@ -11,8 +16,66 @@ const Calendar = () => {
   const firstDayOfMonth = selectedMonth.startOf('month');
   const lastDayOfMonth = selectedMonth.endOf('month');
   const firstDayOfCalendar = firstDayOfMonth.startOf('week');
+  const dailyDoses = useRecoilValue(dailyDosesSelector());
+  const maxDosesByDrinkingDay = useRecoilValue(totalDrinksByDrinkingDaySelector);
+  const nbDayWithNoDrinkGoal = useRecoilValue(daysWithGoalNoDrinkState).length;
+  const computeStyleWithDrinks = (day) => {
+    const formatedDay = day.format('YYYY-MM-DD');
+    const doses = dailyDoses[formatedDay];
+    if (doses >= 0) {
+      if (nbDayWithNoDrinkGoal !== 0) {
+        if (doses > maxDosesByDrinkingDay) {
+          return {
+            borderStyle: 'solid',
+            borderColor: '#FC8383',
+            backgroundColor: '#FC8383',
+            textColor: '#fff',
+            isStar: false,
+          };
+        } else if (doses > 0) {
+          return {
+            borderStyle: 'solid',
+            borderColor: '#34D39A',
+            backgroundColor: '#34D39A',
+            textColor: '#fff',
+            isStar: false,
+          };
+        } else {
+          return {
+            borderStyle: 'solid',
+            borderColor: 'transparent',
+            backgroundColor: 'transparent',
+            textColor: '#fff',
+            isStar: true,
+          };
+        }
+      } else {
+        if (doses > 0) {
+          return {
+            borderStyle: 'solid',
+            borderColor: '#FC8383',
+            backgroundColor: '#FC8383',
+            textColor: '#fff',
+            isStar: false,
+          };
+        } else {
+          return {
+            borderStyle: 'solid',
+            borderColor: 'transparent',
+            backgroundColor: 'transparent',
+            textColor: '#fff',
+            isStar: true,
+          };
+        }
+      }
+    } else {
+      return { borderStyle: 'dotted', borderColor: '#4030A5', backgroundColor: 'white', textColor: '#4030A5' };
+    }
+  };
+
   const calendarDayByWeek = useMemo(() => {
-    let days = [firstDayOfCalendar];
+    const firstDayStyles = computeStyleWithDrinks(firstDayOfCalendar);
+    let days = [{ day: firstDayOfCalendar, styles: firstDayStyles }];
     let previousDay = firstDayOfCalendar;
     let res = [];
     const nbDays = previousDay.add(35, 'days').diff(lastDayOfMonth) > 0 ? 35 : 42;
@@ -22,16 +85,20 @@ const Calendar = () => {
         days = [];
       }
       const day = previousDay.add(1, 'day');
-      days = [...days, day];
+      const styles = computeStyleWithDrinks(day);
+      days = [...days, { day: day, styles: styles }];
       previousDay = day;
     }
     return res;
-  }, [firstDayOfCalendar]);
+  }, [firstDayOfCalendar, dailyDoses]);
+
+  // arbitrary choice of a medium screen size for 414. If smaller screen -> smaller font size else bigger font size
   const widthBaseScale = SCREEN_WIDTH / 414;
   const fontSize = useMemo(() => {
     const newSize = 15 * widthBaseScale;
     return Math.round(PixelRatio.roundToNearestPixel(newSize));
   }, [SCREEN_WIDTH]);
+
   return (
     <>
       <View className="flex flex-row w-full justify-between px-5 items-center">
@@ -51,8 +118,8 @@ const Calendar = () => {
       </View>
       <View className="flex flex-row justify-between mt-3">
         {cols.map((col) => (
-          <View className="flex flex-row grow justify-center basis-4">
-            <Text key={col} className="text-[#B6C1CD]" style={{ fontSize: fontSize }}>
+          <View key={col} className="flex flex-row grow justify-center basis-4">
+            <Text className="text-[#B6C1CD]" style={{ fontSize: fontSize }}>
               {col}
             </Text>
           </View>
@@ -60,34 +127,55 @@ const Calendar = () => {
       </View>
       <View>
         {calendarDayByWeek.map((calendarWeek) => {
-          const bgColor = dayjs().startOf('day').diff(calendarWeek[0]) > 0 ? '#F5F6FA' : 'none';
+          const bgColor = dayjs().startOf('day').diff(calendarWeek[0].day) > 0 ? '#F5F6FA' : 'none';
           return (
             <View
               className="flex flex-row justify-between mt-2 rounded-lg"
-              key={calendarWeek[0]}
+              key={calendarWeek[0].day + 'week'}
               style={{ backgroundColor: bgColor }}>
               {calendarWeek.map((calendarDay) => {
-                return calendarDay.diff(dayjs().startOf('day'), 'day') > 0 ? (
+                return calendarDay.day.diff(dayjs().startOf('day'), 'day') > 0 ? (
                   <View
-                    key={calendarDay}
+                    key={calendarDay.day}
                     className="flex flex-row grow items-center basis-4 justify-center rounded-md aspect-square m-1">
-                    <Text className="text-[#DCE3E9] font-semibold" style={{ fontSize: fontSize }}>
-                      {calendarDay.date()}
+                    <Text
+                      className="text-[#DCE3E9] font-semibold"
+                      style={{
+                        fontSize: fontSize,
+                      }}>
+                      {calendarDay.day.date()}
                     </Text>
                   </View>
                 ) : (
                   <TouchableOpacity
-                    key={calendarDay + 'blue'}
-                    className="flex flex-row grow basis-4 items-center justify-center border border-dotted bg-white border-[#4030A5] rounded-md aspect-square m-1">
-                    <Text className="text-[#4030A5] font-semibold" style={{ fontSize: fontSize }}>
-                      {calendarDay.date()}
+                    key={calendarDay.day + 'blue'}
+                    className="flex flex-row grow basis-4 items-center justify-center rounded-md aspect-square m-1"
+                    style={{
+                      borderStyle: calendarDay.styles.borderStyle,
+                      backgroundColor: calendarDay.styles.backgroundColor,
+                      borderColor: calendarDay.styles.borderColor,
+                      borderWidth: 1,
+                    }}>
+                    {Boolean(calendarDay.styles.isStar) && (
+                      <View className="absolute">
+                        <LegendStar size={fontSize * 2.5} />
+                      </View>
+                    )}
+
+                    <Text
+                      className="font-semibold absolute"
+                      style={{
+                        fontSize: fontSize,
+                        color: calendarDay.styles.textColor,
+                      }}>
+                      {calendarDay.day.date()}
                     </Text>
                   </TouchableOpacity>
                 );
               })}
               <View
                 className="flex flex-row grow basis-4  items-center justify-center rounded-md aspect-square m-1"
-                key={calendarWeek[0] + 'goal'}>
+                key={calendarWeek[0].day + 'goal'}>
                 <Text style={{ fontSize: fontSize }}>X</Text>
               </View>
             </View>
