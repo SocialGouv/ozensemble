f33c08121179d222dc6b17db18b964920478d017
Axel Guichard <axelguichard.45@gmail.com>
2023-05-15 10:21:29 +0200

    fix: clean svg

--- app/src/components/illustrations/icons/OnGoingGoal.js
+++ app/src/components/illustrations/icons/OnGoingGoal.js
@@ -14,5 +14,3 @@ function OnGoingGoal({ size = 20, ...props }) {
 }
 
 export default OnGoingGoal;
-
-<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>;
