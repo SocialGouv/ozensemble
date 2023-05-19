1ce9fc317eb4229d9142fd42ab355cd3e552255f
Axel Guichard <axelguichard.45@gmail.com>
2023-05-19 10:14:37 +0200

    fix: legal mentions to pdf

--- app/src/scenes/Infos/MentionsLegales.js
+++ app/src/scenes/Infos/MentionsLegales.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Linking, Text } from 'react-native';
+import { View } from 'react-native';
 import styled from 'styled-components';
-import H2 from '../../components/H2';
-import WrapperContainer from '../../components/WrapperContainer';
 import TextStyled from '../../components/TextStyled';
+import BackButton from '../../components/BackButton';
+import Pdf from 'react-native-pdf';
+import { defaultPaddingFontScale } from '../../styles/theme';
+
+const pdfUrl = 'https://ozensemble.fr/files/12052023-Mentionslegales_App-OzEnsemble.pdf';
 
 const MentionsLegales = ({ onClose }) => (
-  <WrapperContainer title="Mentions Légales - Application Oz Ensemble" onPressBackButton={onClose}>
-    <Spacer size={50} />
-    <H2>Editeur de la plateforme</H2>
-    <Spacer size={30} />
-    <P>
-      La Plateforme est éditée par l’Agence régionale de santé Île-de-France située : {'\n\n'}
-      Immeuble Le Curve{'\n'}
-      13 Rue du Landy{'\n'}
-      93200 Saint-Denis{'\n'}
-      Téléphone : 01 44 02 00 00{'\n'}
-    </P>
-    <P>Association CaPASSCité</P>
-    <P>Madame Géraldine TALBOT, Directrice</P>
-    <P>70, rue Douy Delcupe</P>
-    <P>93100 Montreuil</P>
-  </WrapperContainer>
+  <View className="flex-1 justify-start items-center mt-6">
+    <View className="flex flex-row w-full mb-4" style={{ paddingHorizontal: defaultPaddingFontScale() }}>
+      <BackButton onPress={onClose} />
+    </View>
+    <Pdf
+      trustAllCerts={false}
+      source={{
+        uri: pdfUrl,
+        cache: true,
+      }}
+      className="flex-1 w-screen h-screen"
+    />
+  </View>
 );
 
 const P = styled(TextStyled)`
