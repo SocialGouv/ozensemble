a69e52faaccf82bd9b26611feed3e53414a58193
Axel Guichard <axelguichard.45@gmail.com>
2023-05-31 15:02:11 +0200

    fix: color, UI, round values

--- app/src/components/ModalGainDetails.js
+++ app/src/components/ModalGainDetails.js
@@ -3,17 +3,14 @@ import Svg, { Path } from 'react-native-svg';
 import { View, TouchableOpacity, Text } from 'react-native';
 import Modal from './Modal';
 import { hitSlop } from '../styles/theme';
-import CrossDefisFailed from './illustrations/icons/CrossDefisFailed';
-import CheckDefisValidated from './illustrations/icons/CheckDefisValidated';
-import TextStyled from './TextStyled';
-import InterogationMark from './illustrations/icons/InterogationMark';
 import Confetti from './Confettis';
-import OnGoingGoal from './illustrations/icons/OnGoingGoal';
 
-const ModalGainDetails = ({ weekExpenses, weekExpenses, isConso, firstDay, lastDay, onClose }) => {
+const ModalGainDetails = ({ content, onClose, visible }) => {
   const firstDayMonth = content?.firstDay?.split(' ')[1];
   const lastDayMonth = content?.lastDay?.split(' ')[1];
   const firstDayDisplay = firstDayMonth === lastDayMonth ? content?.firstDay?.split(' ')[0] : content?.firstDay;
+  const caloriesTitle = content?.weekKcal <= content?.estimationKcal ? 'KCalories évitées' : 'Calories en plus';
+  const eurosTitle = content?.weekExpenses <= content?.estimationExpenses ? 'Euros épargnés' : 'Euros non-épargnés';
   return (
     <Modal visible={!!content} animationType="fade" withBackground hideOnTouch>
       <View className="bg-white rounded-xl">
@@ -38,83 +35,72 @@ const ModalGainDetails = ({ weekExpenses, weekExpenses, isConso, firstDay, lastD
           <Text className="text-xs text-[#939EA6] text-center">
             semaine du {firstDayDisplay} au {content?.lastDay}
           </Text>
-          <View className="flex flex-row justify-around mt-8">
-            <View className="rounded-md bg-[#F5F6FA]">
-              <Text className="text-[#4030A5] font-semibold text-center text-xs">Estimation initiale</Text>
-              <View className={'flex flex-row justify-center'}>
-                <Text className="text-center font-bold mt-1 text-xl">{Math.round(content?.estimationExpenses)}</Text>
-                <Text className="text-center font-bold mt-1 text-lg">
-                  {' '}
-                  {Math.round(content?.consosWeek) > 1 ? 'unités' : 'unité'}
+          <View className="flex flex-row mt-8 space-x-3">
+            <View className="py-2 px-1 bg-[#F5F6FA] rounded-md grow basis-28">
+              <Text className="text-[#939EA6] text-center text-xs">Estimation initiale</Text>
+              <View className="flex flex-row grow justify-center">
+                <Text className="text-center font-bold mt-1 text-xl">
+                  {Math.round(content?.estimationExpenses * 10) / 10}
                 </Text>
+                <Text className="text-center font-bold mt-1 text-lg">€</Text>
               </View>
             </View>
-            <View>
-              <Text className="text-[#4030A5] font-semibold text-center text-xs">Objectif max</Text>
-              {content?.consosWeekGoal ? (
-                <View className={'flex flex-row justify-center'}>
-                  <Text className="text-center font-bold mt-1 text-xl">{Math.round(content?.consosWeekGoal)}</Text>
-                  <Text className="text-center font-bold mt-1 text-lg">
-                    {' '}
-                    {Math.round(content?.consosWeekGoal) > 1 ? 'unités' : 'unité'}
-                  </Text>
-                </View>
-              ) : (
-                <Text className="text-center font-bold mt-1 text-xl">?</Text>
-              )}
+            <View className="py-2 px-1 bg-[#F5F6FA] rounded-md grow basis-28">
+              <Text className="text-[#939EA6] text-center text-xs">Consommation</Text>
+              <View className="flex flex-row justify-center">
+                <Text className="text-center font-bold mt-1 text-xl">
+                  {Math.round(content?.weekExpenses * 10) / 10}
+                </Text>
+                <Text className="text-center font-bold mt-1 text-lg">€</Text>
+              </View>
             </View>
           </View>
-          {content?.consommationContent && (
-            <Text className="text-center mt-4">
-              {content?.consommationContent?.split('__')?.map((string, index) => {
-                return (
-                  <React.Fragment key={string}>
-                    <TextStyled bold={index % 2}>{string}</TextStyled>
-                  </React.Fragment>
-                );
-              })}
-            </Text>
-          )}
-
-          {content?.consosWeekGoal && (
-            <View>
-              <View className="flex flex-row justify-center">
-                <View className="w-3/4 border border-[#DDDDDD] my-8"></View>
-              </View>
-
-              <View className="flex flex-row justify-around">
-                <View>
-                  <Text className="text-[#4030A5] text-center font-semibold text-xs">Jours où j'ai bu</Text>
-                  <View className={'flex flex-row justify-center'}>
-                    <Text className="text-center font-bold mt-1 text-xl ">{content?.drinkingDays}</Text>
-                    <Text className="text-center font-bold mt-1 text-lg">
-                      {' '}
-                      {content?.drinkingDays > 1 ? 'jours' : 'jour'}
-                    </Text>
-                  </View>
-                </View>
-                <View>
-                  <Text className="text-[#4030A5] text-center font-semibold text-xs">Objectif max</Text>
-                  <View className={'flex flex-row justify-center'}>
-                    <Text className="text-center font-bold mt-1 text-xl">{content?.drinkingDaysGoal}</Text>
-                    <Text className="text-center font-bold mt-1 text-lg">
-                      {' '}
-                      {content?.drinkingDaysGoal > 1 ? 'jours' : 'jour'}
-                    </Text>
-                  </View>
-                </View>
-              </View>
-              <Text className="text-center mt-4">
-                {content?.drinkingDaysContent?.split('__')?.map((string, index) => {
-                  return (
-                    <React.Fragment key={string}>
-                      <TextStyled bold={index % 2}>{string}</TextStyled>
-                    </React.Fragment>
-                  );
-                })}
+          <View className="py-2 mt-3 bg-[#F5F6FA] rounded-md">
+            <Text className="text-center text-[#939EA6] text-xs">{eurosTitle}</Text>
+            <View
+              className={[
+                'mx-auto px-2 py-1 rounded-md mt-2',
+                caloriesTitle === 'Calories en plus' ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
+              ].join(' ')}>
+              <Text className="text-center text-white font-bold text-xl">
+                {Math.round(Math.abs(content?.weekExpenses - content?.estimationExpenses) * 10) / 10}€
               </Text>
             </View>
-          )}
+          </View>
+          <View className="flex flex-row justify-center">
+            <View className="w-3/4 border border-[#DDDDDD] my-8"></View>
+          </View>
+          <View className="flex flex-row space-x-3">
+            <View className="py-2 bg-[#F5F6FA] rounded-md grow basis-24">
+              <Text className="text-[#939EA6] text-center text-xs">Estimation initiale</Text>
+              <View className="flex flex-row justify-center items-baseline">
+                <Text className="text-center font-bold mt-1 text-xl ">
+                  {Math.round(content?.estimationKcal * 10) / 10}
+                </Text>
+                <Text className="text-center font-bold mt-1 text-base"> KCAL</Text>
+              </View>
+            </View>
+            <View className="py-2 bg-[#F5F6FA] rounded-md grow basis-24">
+              <Text className="text-[#939EA6] text-center text-xs">Consommation</Text>
+              <View className="flex flex-row justify-center items-baseline">
+                <Text className="text-center font-bold mt-1 text-xl">{Math.round(content?.weekKcal * 10) / 10}</Text>
+                <Text className="text-center font-bold mt-1 text-base"> KCAL</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View className="py-2 bg-[#F5F6FA] rounded-md">
+          <Text className="text-center text-[#939EA6] text-xs">{caloriesTitle}</Text>
+          <View
+            className={[
+              'flex flex-row justify-center mx-auto px-2 py-1 rounded-md mt-2 items-baseline',
+              caloriesTitle === 'Calories en plus' ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
+            ].join(' ')}>
+            <Text className="text-center font-bold text-xl text-white">
+              {Math.round(Math.abs(content?.weekKcal - content?.estimationKcal) * 10) / 10}
+            </Text>
+            <Text className="text-center font-bold text-base text-white"> KCAL</Text>
+          </View>
         </View>
       </View>
       {content?.status === 'Success' && <Confetti run={true} />}
