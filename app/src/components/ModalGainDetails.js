07943c1b44adb1751d9348dd2cce4f0291936392
Axel <100227882+axelgcd@users.noreply.github.com>
2023-05-31 15:20:33 +0200

    feat:  modal gain details (#395)
    Co-authored-by: Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
    Co-authored-by: Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>

---
+++ app/src/components/ModalGainDetails.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+import { View, TouchableOpacity, Text } from 'react-native';
+import Modal from './Modal';
+import { hitSlop } from '../styles/theme';
+import Confetti from './Confettis';
+
+const ModalGainDetails = ({ content, onClose, visible }) => {
+  const firstDayMonth = content?.firstDay?.split(' ')[1];
+  const lastDayMonth = content?.lastDay?.split(' ')[1];
+  const firstDayDisplay = firstDayMonth === lastDayMonth ? content?.firstDay?.split(' ')[0] : content?.firstDay;
+  const caloriesTitle = content?.weekKcal <= content?.estimationKcal ? 'KCalories évitées' : 'KCalories en plus';
+  const eurosTitle = content?.weekExpenses <= content?.estimationExpenses ? 'Euros épargnés' : 'Euros non-épargnés';
+  return (
+    <Modal visible={!!content} animationType="fade" withBackground hideOnTouch>
+      <View className="bg-white rounded-xl">
+        <View className="h-5 flex flex-row  justify-end">
+          <TouchableOpacity onPress={onClose} hitSlop={hitSlop(15)}>
+            <Svg fill="none" viewBox="0 0 24 24" className="h-5 w-5">
+              <Path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={3}
+                stroke="black"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </Svg>
+          </TouchableOpacity>
+        </View>
+
+        <View className=" p-2">
+          <Text color="#000" className="text-center mb-1 text-xl font-extrabold">
+            Résultats semaine
+          </Text>
+          <Text className="text-xs text-[#939EA6] text-center">
+            semaine du {firstDayDisplay} au {content?.lastDay}
+          </Text>
+          <View className="flex flex-row mt-8 space-x-3">
+            <View className="py-2 px-1 bg-[#F5F6FA] rounded-md grow basis-28">
+              <Text className="text-[#939EA6] text-center text-xs">Estimation initiale</Text>
+              <View className="flex flex-row grow justify-center">
+                <Text className="text-center font-bold mt-1 text-xl">
+                  {Math.round(content?.estimationExpenses * 10) / 10}
+                </Text>
+                <Text className="text-center font-bold mt-1 text-lg">€</Text>
+              </View>
+            </View>
+            <View className="py-2 px-1 bg-[#F5F6FA] rounded-md grow basis-28">
+              <Text className="text-[#939EA6] text-center text-xs">Consommation</Text>
+              <View className="flex flex-row justify-center">
+                <Text className="text-center font-bold mt-1 text-xl">
+                  {Math.round(content?.weekExpenses * 10) / 10}
+                </Text>
+                <Text className="text-center font-bold mt-1 text-lg">€</Text>
+              </View>
+            </View>
+          </View>
+          <View className="py-2 mt-3 bg-[#F5F6FA] rounded-md">
+            <Text className="text-center text-[#939EA6] text-xs">{eurosTitle}</Text>
+            <View
+              className={[
+                'mx-auto px-2 py-1 rounded-md mt-2',
+                eurosTitle === 'Euros non-épargnés' ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
+              ].join(' ')}>
+              <Text className="text-center text-white font-bold text-xl">
+                {Math.round(Math.abs(content?.weekExpenses - content?.estimationExpenses) * 10) / 10}€
+              </Text>
+            </View>
+          </View>
+          <View className="flex flex-row justify-center">
+            <View className="w-3/4 border border-[#DDDDDD] my-8"></View>
+          </View>
+          <View className="flex flex-row space-x-3">
+            <View className="py-2 bg-[#F5F6FA] rounded-md grow basis-24">
+              <Text className="text-[#939EA6] text-center text-xs">Estimation initiale</Text>
+              <View className="flex flex-row justify-center items-baseline">
+                <Text className="text-center font-bold mt-1 text-xl ">
+                  {Math.round(content?.estimationKcal * 10) / 10}
+                </Text>
+                <Text className="text-center font-bold mt-1 text-base"> KCAL</Text>
+              </View>
+            </View>
+            <View className="py-2 bg-[#F5F6FA] rounded-md grow basis-24">
+              <Text className="text-[#939EA6] text-center text-xs">Consommation</Text>
+              <View className="flex flex-row justify-center items-baseline">
+                <Text className="text-center font-bold mt-1 text-xl">{Math.round(content?.weekKcal * 10) / 10}</Text>
+                <Text className="text-center font-bold mt-1 text-base"> KCAL</Text>
+              </View>
+            </View>
+          </View>
+          <View className="py-2 bg-[#F5F6FA] rounded-md mt-3">
+            <Text className="text-center text-[#939EA6] text-xs">{caloriesTitle}</Text>
+            <View
+              className={[
+                'flex flex-row justify-center mx-auto px-2 py-1 rounded-md mt-2 items-baseline',
+                caloriesTitle === 'KCalories en plus' ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
+              ].join(' ')}>
+              <Text className="text-center font-bold text-xl text-white">
+                {Math.round(Math.abs(content?.weekKcal - content?.estimationKcal) * 10) / 10}
+              </Text>
+              <Text className="text-center font-bold text-base text-white"> KCAL</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      {content?.status === 'Success' && <Confetti run={true} />}
+    </Modal>
+  );
+};
+
+export default ModalGainDetails;
--- app/src/components/WeeklyGains.js
+++ app/src/components/WeeklyGains.js
@@ -1,20 +1,29 @@
-import React, { useMemo } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import { Dimensions, Text, TouchableOpacity, View, PixelRatio } from 'react-native';
 
 import dayjs from 'dayjs';
 import { useRecoilValue } from 'recoil';
 import { consolidatedCatalogSelector, drinksState } from '../recoil/consos';
 import { previousDrinksPerWeekState } from '../recoil/gains';
 import { defaultPaddingFontScale } from '../styles/theme';
+import ModalGainDetails from './ModalGainDetails';
 
 const WeeklyGains = ({ selectedMonth }) => {
+  const { width: SCREEN_WIDTH } = Dimensions.get('window');
   const firstDayOfMonth = selectedMonth.startOf('month');
   const lastDayOfMonth = selectedMonth.endOf('month');
   const firstDayOfCalendar = firstDayOfMonth.startOf('week');
   const nbDays = firstDayOfCalendar.add(35, 'days').diff(lastDayOfMonth) > 0 ? 35 : 42; // 35 days if the month run on 5 weeks, 42 if it run on 6 weeks
   const catalog = useRecoilValue(consolidatedCatalogSelector);
   const drinks = useRecoilValue(drinksState);
+  const [modalContent, setModalContent] = useState(null);
   const previousDrinksPerWeek = useRecoilValue(previousDrinksPerWeekState);
+  // arbitrary choice of a medium screen size for 414. If smaller screen -> smaller font size else bigger font size
+  const widthBaseScale = SCREEN_WIDTH / 414;
+  const fontSize = useMemo(() => {
+    const newSize = 15 * widthBaseScale;
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  }, [SCREEN_WIDTH]);
   const myWeeklyKcalBeforeObjective = useMemo(
     () =>
       previousDrinksPerWeek.reduce(
@@ -37,8 +46,8 @@ const WeeklyGains = ({ selectedMonth }) => {
     const _weekInfos = [];
     const nbWeeks = nbDays / 7;
     for (let i = 0; i < nbWeeks; i++) {
-      const startDay = firstDayOfCalendar.add(i * 7, 'days').format('DD');
-      const endDay = firstDayOfCalendar.add(i * 7 + 6, 'days').format('DD');
+      const startDay = firstDayOfCalendar.add(i * 7, 'days');
+      const endDay = firstDayOfCalendar.add(i * 7 + 6, 'days');
       _weekInfos[i] = {
         startDay: startDay,
         endDay: endDay,
@@ -66,24 +75,32 @@ const WeeklyGains = ({ selectedMonth }) => {
     <View style={{ paddingHorizontal: defaultPaddingFontScale() }}>
       <View className="flex flex-row mt-4 mb-2">
         <View className=" flex flex-grow">
-          <Text className="text-center text-sm text-[#B6C1CD]">Semaine</Text>
+          <Text className="text-center text-[#B6C1CD]" style={{ fontSize: fontSize }}>
+            Semaine
+          </Text>
         </View>
         <View className=" flex flex-grow ">
-          <Text className="flex flex-grow text-center text-sm text-[#B6C1CD]">€ épargnés</Text>
+          <Text className="flex flex-grow text-center text-[#B6C1CD]" style={{ fontSize: fontSize }}>
+            € épargnés
+          </Text>
         </View>
         <View className=" flex flex-grow ">
-          <Text className="flex flex-grow text-center text-sm text-[#B6C1CD]">KCAL évitées</Text>
+          <Text className="flex flex-grow text-center text-[#B6C1CD]" style={{ fontSize: fontSize }}>
+            KCAL évitées
+          </Text>
         </View>
         <View className=" flex flex-grow mr-1">
-          <Text className="flex flex-grow text-center text-sm text-[#B6C1CD]">Détails</Text>
+          <Text className="flex flex-grow text-center text-[#B6C1CD]" style={{ fontSize: fontSize }}>
+            Détails
+          </Text>
         </View>
       </View>
       {weekInfos.map((week) => {
         return (
-          <View className="flex flex-row mb-2 bg-[#F5F6FA] rounded-lg py-1">
-            <View className="flex flex-row grow items-center justify-center basis-16">
-              <Text className="text-sm text-[#939EA6] font-semibold">
-                {week.startDay} au {week.endDay}
+          <View className="flex flex-row mb-2 bg-[#F5F6FA] rounded-lg py-1" key={week.startDay}>
+            <View className="flex flex-row grow items-center justify-center basis-14">
+              <Text className=" text-[#939EA6] font-semibold" style={{ fontSize: fontSize }}>
+                {week.startDay.format('DD')} au {week.endDay.format('DD')}
               </Text>
             </View>
             <View className="flex flex-row grow justify-center items-center basis-20">
@@ -92,8 +109,8 @@ const WeeklyGains = ({ selectedMonth }) => {
                   'justify-center rounded-md flex flex-row my-1 py-1 mx-4 grow',
                   week.euros > myWeeklyExpensesBeforeObjective ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
                 ].join(' ')}>
-                <Text className="text-sm text-white font-semibold">
-                  {Math.abs(week.euros - myWeeklyExpensesBeforeObjective)}€
+                <Text className=" text-white font-semibold" style={{ fontSize: fontSize }}>
+                  {Math.round(Math.abs(week.euros - myWeeklyExpensesBeforeObjective) * 10) / 10}€
                 </Text>
               </View>
             </View>
@@ -103,19 +120,33 @@ const WeeklyGains = ({ selectedMonth }) => {
                   'justify-center rounded-md flex flex-row my-1 py-1 mx-1 grow',
                   week.kcal > myWeeklyKcalBeforeObjective ? 'bg-[#FF7979]' : 'bg-[#3AD39D] ',
                 ].join(' ')}>
-                <Text className="text-sm text-white font-semibold">
-                  {Math.abs(week.kcal - myWeeklyKcalBeforeObjective)} KCAL
+                <Text className="text-white font-semibold" style={{ fontSize: fontSize }}>
+                  {Math.round(Math.abs(week.kcal - myWeeklyKcalBeforeObjective))} KCAL
                 </Text>
               </View>
             </View>
             <View className="flex flex-row grow justify-center items-center">
-              <TouchableOpacity className="rounded-xl bg-[#4030A5] py-1 px-2">
-                <Text className="text-white text-xs font-semibold">Détails</Text>
+              <TouchableOpacity
+                className="bg-[#4030A5] rounded-full py-1 px-2"
+                onPress={() => {
+                  setModalContent({
+                    weekKcal: week.kcal,
+                    estimationKcal: myWeeklyKcalBeforeObjective,
+                    weekExpenses: week.euros,
+                    estimationExpenses: myWeeklyExpensesBeforeObjective,
+                    firstDay: week.startDay.format('DD MMMM'),
+                    lastDay: week.endDay.format('DD MMMM'),
+                  });
+                }}>
+                <Text className="text-white font-semibold" style={{ fontSize: fontSize }}>
+                  Détails
+                </Text>
               </TouchableOpacity>
             </View>
           </View>
         );
       })}
+      <ModalGainDetails content={modalContent} onClose={() => setModalContent(null)} />
     </View>
   );
 };
