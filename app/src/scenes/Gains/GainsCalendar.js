141e1f1a86652ea51a0770da27c908c7f12e6d71
Axel Guichard <axelguichard.45@gmail.com>
2023-06-01 15:52:15 +0200

    fix: merge

--- app/src/scenes/Gains/GainsCalendar.js
+++ app/src/scenes/Gains/GainsCalendar.js
@@ -3,7 +3,7 @@ import React, { useMemo, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useRecoilValue } from 'recoil';
 import H1 from '../../components/H1';
-import { dailyDosesSelector } from '../../recoil/consos';
+import { dailyDosesSelector, drinksState, feedDaysSelector } from '../../recoil/consos';
 import Calendar from '../../components/Calendar';
 import { logEvent } from '../../services/logEventsWithMatomo';
 import { Text, TouchableOpacity, View } from 'react-native';
@@ -19,6 +19,13 @@ import LegendStar from '../../components/illustrations/icons/LegendStar';
 import LegendInfos from '../../components/illustrations/icons/LegendInfos';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import OnGoingGoal from '../../components/illustrations/icons/OnGoingGoal';
+import GainsIcon from '../../components/illustrations/icons/GainsIcon';
+import H2 from '../../components/H2';
+import styled from 'styled-components';
+import InfoRoundIcon from '../../components/illustrations/icons/InfoRoundIcon';
+import { previousDrinksPerWeekState } from '../../recoil/gains';
+import EuroIcon from '../../components/illustrations/icons/EuroIcon';
+import KcalIcon from '../../components/illustrations/icons/KcalIcon';
 
 import TargetGoal from '../../components/illustrations/icons/TargetGoal';
 
@@ -96,6 +103,62 @@ const GainsCalendar = ({ isOnboarded, setShowOnboardingGainModal, setDateToScrol
   }, [dailyDoses, currentMonth]);
   const [window, setWindow] = useState('calendar');
   const [selectedMonth, setSelectedMonth] = useState(dayjs());
+  const days = useRecoilValue(feedDaysSelector);
+  const beginDateOfOz = useMemo(() => {
+    if (!days.length) return null;
+    return dayjs(days[days.length - 1]);
+  }, [days]);
+  const previousDrinksPerWeek = useRecoilValue(previousDrinksPerWeekState);
+  const drinks = useRecoilValue(drinksState);
+
+  const myWeeklyExpensesBeforeObjective = useMemo(
+    () =>
+      previousDrinksPerWeek.reduce(
+        (sum, drink) =>
+          sum +
+          drink.quantity * (drinksCatalog.find((drinkCatalog) => drinkCatalog.drinkKey === drink.drinkKey)?.price || 0),
+        0
+      ),
+    [previousDrinksPerWeek]
+  );
+
+  const myWeeklyKcalBeforeObjective = useMemo(
+    () =>
+      previousDrinksPerWeek.reduce(
+        (sum, drink) =>
+          sum +
+          drink.quantity * (drinksCatalog.find((drinkCatalog) => drinkCatalog.drinkKey === drink.drinkKey)?.kcal || 0),
+        0
+      ),
+    [previousDrinksPerWeek]
+  );
+  const mySavingsSinceBeginning = useMemo(() => {
+    if (!days.length) return null;
+    const myExpensesSinceBegnining = drinks.reduce(
+      (sum, drink) =>
+        sum +
+        drink.quantity * (drinksCatalog.find((drinkCatalog) => drinkCatalog.drinkKey === drink.drinkKey)?.price || 0),
+      0
+    );
+    const numberOfDaysSinceBeginning = Math.abs(dayjs(beginDateOfOz).diff(dayjs(), 'days'));
+    const averageDailyExpenses = myExpensesSinceBegnining / numberOfDaysSinceBeginning;
+    const averageDailyExpensesBeforeObjective = myWeeklyExpensesBeforeObjective / 7;
+    return Math.ceil(averageDailyExpensesBeforeObjective - averageDailyExpenses) * numberOfDaysSinceBeginning;
+  }, [drinks, days, myWeeklyExpensesBeforeObjective, beginDateOfOz]);
+
+  const myKcalSavingsSinceBeginning = useMemo(() => {
+    if (!days.length) return null;
+    const myKcalSinceBegnining = drinks.reduce(
+      (sum, drink) =>
+        sum +
+        drink.quantity * (drinksCatalog.find((drinkCatalog) => drinkCatalog.drinkKey === drink.drinkKey)?.kcal || 0),
+      0
+    );
+    const numberOfDaysSinceBeginning = Math.abs(dayjs(beginDateOfOz).diff(dayjs(), 'days'));
+    const averageDailyKcal = myKcalSinceBegnining / numberOfDaysSinceBeginning;
+    const averageDailyKcalBeforeObjective = myWeeklyKcalBeforeObjective / 7;
+    return Math.ceil(averageDailyKcalBeforeObjective - averageDailyKcal) * numberOfDaysSinceBeginning;
+  }, [drinks, days, myWeeklyKcalBeforeObjective, beginDateOfOz]);
 
   return (
     <View className="py-5">
@@ -245,11 +308,78 @@ const GainsCalendar = ({ isOnboarded, setShowOnboardingGainModal, setDateToScrol
                 </TouchableOpacity>
               </View>
             )}
+            <WeeklyGains selectedMonth={selectedMonth} />
+            <View className="pt-5" style={{ paddingHorizontal: defaultPaddingFontScale() }}>
+              <View className="flex flex-row space-x-1 items-center mb-3 mt-6">
+                <GainsIcon size={25} />
+                <H2 color="#4030a5">Mes gains depuis le début</H2>
+                <GainsFromStartInfoButton onPress={() => navigation.push('GAINS_FROM_START_MODALE')}>
+                  <InfoRoundIcon size={25} />
+                </GainsFromStartInfoButton>
+              </View>
+              <CategoriesContainer>
+                <Categorie>
+                  <View>
+                    <EuroIcon size={25} />
+                  </View>
+                  <Spacer size={5} />
+                  <TextStyled bold size={35}>
+                    {mySavingsSinceBeginning > 0 ? mySavingsSinceBeginning : 0}
+                  </TextStyled>
+                  <Text>Euros épargnés</Text>
+                </Categorie>
+                <Spacer size={20} />
+                <Categorie>
+                  <View>
+                    <KcalIcon size={25} />
+                  </View>
+                  <Spacer size={5} />
+                  <TextStyled bold size={35}>
+                    {myKcalSavingsSinceBeginning > 0 ? myKcalSavingsSinceBeginning : 0}
+                  </TextStyled>
+                  <CategorieText>KCalories évitées</CategorieText>
+                </Categorie>
+              </CategoriesContainer>
+            </View>
           </>
         )}
       </View>
     </View>
   );
 };
+
+const Spacer = styled.View`
+  height: ${({ size }) => size || 20}px;
+  width: ${({ size }) => size || 20}px;
+`;
+
+const GainsFromStartInfoButton = styled.TouchableOpacity`
+  justify-content: center;
+`;
+
+const CategorieText = styled(TextStyled)`
+  text-align: center;
+`;
+
+const CategoriesContainer = styled.View`
+  justify-content: space-between;
+  flex-direction: row;
+  margin-bottom: 15px;
+`;
+
+const Categorie = styled.View`
+  border: 1px solid #dddddd;
+  border-radius: 5px;
+  flex: 1;
+  align-items: center;
+  justify-content: flex-end;
+  overflow: hidden;
+  padding: 10px 4px 10px 4px;
+  min-height: 90px;
+`;
+
+const Container = styled.View`
+  padding-top: 20px;
+`;
 
 export default GainsCalendar;
