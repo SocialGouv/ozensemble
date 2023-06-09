9fa60364cdfe88f7d6b90983e17fe802f618eb10
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-06-09 11:34:25 +0200

    fix: require cycle (#421)

--- app/src/scenes/ConsoFollowUp/Diagram.js
+++ app/src/scenes/ConsoFollowUp/Diagram.js
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { selectorFamily, useRecoilValue } from 'recoil';
 import styled, { css } from 'styled-components';
-import { Text, TouchableOpacity } from 'react-native';
+import { Modal, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import dayjs from 'dayjs';
 import { screenHeight } from '../../styles/theme';
@@ -15,7 +15,10 @@ import Equality from '../../components/illustrations/Equality';
 import H3 from '../../components/H3';
 import PeriodSelector from '../../components/PeriodSelector';
 import PeriodSwitchToggle from '../../components/PeriodSwitchToggle';
-import DiagramHelpModal from './DiagramHelpModal';
+import Stars from '../../components/illustrations/Stars';
+import OneDoseAlcoolExplanation from '../../components/OneDoseAlcoolExplanation';
+import WrapperContainer from '../../components/WrapperContainer';
+import { defaultPaddingFontScale } from '../../styles/theme';
 
 const maxDosesOnScreen = 999;
 const minBarHeight = 1;
@@ -68,7 +71,7 @@ const diffWithPreviousWeekSelector = selectorFamily({
     },
 });
 
-const Diagram = ({ inModalHelp = false }) => {
+export default function Diagram({ inModalHelp = false }) {
   let now = Date.now();
 
   const navigation = useNavigation();
@@ -342,7 +345,7 @@ const Diagram = ({ inModalHelp = false }) => {
       )}
     </>
   );
-};
+}
 
 const LegendsContainer = styled.View`
   width: 100%;
@@ -498,4 +501,91 @@ const Line = styled.View`
   border-color: #39cec0;
 `;
 
-export default Diagram;
+const Elem = ({ content, lineHeight = 20 }) => (
+  <ElemContainer>
+    <Stars color="#4030a5" style={{ marginRight: 10 }} size={20} />
+    <TextStyled style={{ flex: 1, lineHeight, fontSize: 18 }}>{content}</TextStyled>
+  </ElemContainer>
+);
+
+const DiagramHelpModal = ({ visible, onCloseHelp }) => {
+  const totalDrinksByDrinkingDay = useRecoilValue(totalDrinksByDrinkingDaySelector);
+  return (
+    <Modal visible={visible} animationType="slide" presentationStyle="formSheet" onRequestClose={onCloseHelp}>
+      <WrapperContainer onPressBackButton={onCloseHelp}>
+        <Diagram inModalHelp={true} />
+        <Paragraph>
+          <Elem
+            content={
+              <TextStyled>
+                Le graphique représente{' '}
+                <TextStyled bold color="#4030a5">
+                  les unités d'alcool
+                </TextStyled>{' '}
+                consommées sur une journée.
+              </TextStyled>
+            }
+          />
+          <Elem
+            content={
+              <TextStyled>
+                La ligne verte représente{' '}
+                <TextStyled bold color="#4030a5">
+                  le seuil de votre objectif
+                </TextStyled>{' '}
+                {totalDrinksByDrinkingDay === 0
+                  ? "(2 unités représentant pour l'instant le seuil fixé par l'OMS)"
+                  : `(${totalDrinksByDrinkingDay} unité${totalDrinksByDrinkingDay > 1 ? 's' : ''} par jour)`}
+              </TextStyled>
+            }
+          />
+        </Paragraph>
+        <Paragraph>
+          <Elem
+            content={
+              <TextStyled>
+                Une unité d'alcool correspond à environ 10 grammes d'alcool pur, soit environ un verre de vin de 13cl à
+                12°c ou un demi de bière à 4°c par exemple.
+              </TextStyled>
+            }
+          />
+        </Paragraph>
+        <OneDoseAlcoolExplanation marginOffset={defaultPaddingFontScale()} noMinHeight />
+        <Paragraph>
+          <Elem
+            content={
+              <TextStyled>
+                Lorsque vous saisisez une consommation, l'application{' '}
+                <TextStyled color="#4030a5">convertit automatiquement</TextStyled> en unité d'alcool.
+              </TextStyled>
+            }
+          />
+        </Paragraph>
+        <Paragraph>
+          <Elem
+            content={
+              <TextStyled>
+                <TextStyled color="#4030a5">Compter ses consommations</TextStyled> est un pas essentiel pour prendre
+                conscience de ce que l'on consomme.
+              </TextStyled>
+            }
+          />
+        </Paragraph>
+      </WrapperContainer>
+    </Modal>
+  );
+};
+
+export const TopContainer = styled.View`
+  padding-horizontal: 20px;
+`;
+
+const Paragraph = styled.View`
+  margin-bottom: 25px;
+`;
+
+const ElemContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  margin: 10px 0;
+`;
