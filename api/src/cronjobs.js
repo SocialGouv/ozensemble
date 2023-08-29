11ea59f34781d70197bf2335dca757941b0424b4
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-08-29 11:49:04 +0200

    fix(api): split cronjobs (#452)

--- api/src/cronjobs.js
+++ api/src/cronjobs.js
@@ -1,6 +1,7 @@
+require("dotenv").config({ path: "./.env" });
+
 const cron = require("node-cron");
 const { capture } = require("./third-parties/sentry");
-const { CRONJOBS_ENABLED } = require("./config");
 
 const { notificationsCronJob, scheduleNotificationsInactivity5DaysCronJob } = require("./notifications");
 const { reminderCronJob } = require("./controllers/reminder");
@@ -11,17 +12,19 @@ cron.schedule("* * * * *", async () => {
   launchCronJob("reminder", reminderCronJob);
 });
 
+cron.schedule("* * * * *", async () => {
+  // every minute
+  await launchCronJob("test", async () => {
+    console.log("test cron job " + new Date());
+  });
+});
+
 cron.schedule("0 0 4 * * * *", async () => {
   // every day at 4 am, find users with lastConsoAdded = 4 days ago and create a notification for tomorrow (6th day)
   launchCronJob("schedule notifications 5 days inactivity", scheduleNotificationsInactivity5DaysCronJob);
 });
 
 const launchCronJob = async (name, job) => {
-  if (CRONJOBS_ENABLED === false) {
-    console.log(`should start cronjob ${name} but CRONJOBS_ENABLED=false`);
-    return;
-  }
-
   try {
     job();
   } catch (e) {
