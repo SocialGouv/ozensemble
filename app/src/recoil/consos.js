8db7cda4ce715aa513e20f0b9c8c56044fac1ed3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-09-27 16:44:01 +0200

    clean

--- app/src/recoil/consos.js
+++ app/src/recoil/consos.js
@@ -133,22 +133,17 @@ export const derivedDataFromDrinksState = selector({
     let i = 0;
     for (const drink of drinks) {
       i++;
-      console.log(`DRINK ${i}`, drink);
       const day = dayjs(drink.timestamp).format('YYYY-MM-DD');
-      console.log('DAY IS', day);
       // init startOfWeek
       if (!startOfWeek) {
         startOfWeek = dayjs(day).startOf('week').format('YYYY-MM-DD');
-        console.log('INIT START OF WEEK', startOfWeek);
       }
 
       // init goalStartOfWeek
       if (!goalStartOfWeek) {
         goalStartOfWeek = goalsByWeek[startOfWeek];
-        console.log('INIT GOAL START OF WEEK', goalStartOfWeek);
         if (!goalStartOfWeek) {
           goalStartOfWeek = goals.at(-1);
-          console.log('NO GOAL START OF WEEK, INIT AGAIN', goalStartOfWeek);
         }
         // save the first goal of the week
         calendarGoalsStartOfWeek[startOfWeek] = goalStartOfWeek;
@@ -156,59 +151,45 @@ export const derivedDataFromDrinksState = selector({
       if (startOfWeek > day) {
         // update startOfWeek for weekly doses and goals
         startOfWeek = dayjs(day).startOf('week').format('YYYY-MM-DD');
-        console.log('RESET START OF WEEK', startOfWeek);
         currentWeek = {};
         dayWithDosesNotRespectedExists = false;
         // update goalStartOfWeek
         const nextGoal = goalsByWeek[startOfWeek];
         if (nextGoal) {
           goalStartOfWeek = nextGoal;
-          console.log('NEXT GOAL', goalStartOfWeek);
         }
         calendarGoalsStartOfWeek[startOfWeek] = goalStartOfWeek;
       }
       // daily drinks
       if (!drinksByDay[day]) {
         drinksByDay[day] = [];
-        console.log('INIT DRINKS BY DAY');
       }
       drinksByDay[day].push(drink);
 
       // daily doses
       const dose = mapDrinkToDose(drink, consolidatedCatalogObject);
-      console.log('DOSE', dose);
       if (!dailyDoses[day]) {
-        console.log('INIT DAILY DOSES');
         dailyDoses[day] = 0;
       }
       dailyDoses[day] = dailyDoses[day] + dose;
-      console.log('DAILY DOSE IS', dailyDoses[day], 'FOR DAY', day);
       // weekly doses
       if (!weeklyDoses[startOfWeek]) {
-        console.log('INIT WEEKLY DOSES');
         weeklyDoses[startOfWeek] = 0;
       }
       weeklyDoses[startOfWeek] = weeklyDoses[startOfWeek] + dose;
-      console.log('WEEKLY DOSE IS', weeklyDoses[startOfWeek], 'FOR START OF WEEK', startOfWeek);
       // monthly doses
       const month = day.slice(0, 'YYYY-MM'.length);
-      console.log('MONTH IS', month);
       if (!monthlyDoses[month]) {
-        console.log('INIT MONTHLY DOSES');
         monthlyDoses[month] = 0;
       }
       monthlyDoses[month] = monthlyDoses[month] + dose;
-      console.log('MONTHLY DOSE IS', monthlyDoses[month], 'FOR MONTH', month);
 
-      console.log('CALENDAR DAYS FIRST');
       // calendar days
       if (!goalStartOfWeek) {
         calendarDays[day] = dailyDoses[day] > 0 ? 'noGoalAndDoses' : 'noGoalAndNoDoses';
-        console.log('NO GOAL YET', calendarDays[day]);
       } else {
         if (dailyDoses[day] === 0) {
           calendarDays[day] = 'goalExistsAndNoDoses';
-          console.log('GOAL AND NO DOSES', calendarDays[day]);
         } else if (dailyDoses[day] > goalStartOfWeek.dosesByDrinkingDay) {
           // WARNING: the `dosesByDrinkingDay` is just an average of total drinks / authorized days
           // example: 4 units per week that I can drink in 7 days: average of 0.57 units per day
@@ -216,28 +197,17 @@ export const derivedDataFromDrinksState = selector({
           // although I respected my goal of 4 units per week
           calendarDays[day] = 'goalExistsButNotRespected';
           dayWithDosesNotRespectedExists = true; // for optimization purpose
-          console.log(
-            'DOSES EXCEED',
-            calendarDays[day],
-            'daily doses',
-            dailyDoses[day],
-            'goal dose by day',
-            goalStartOfWeek.dosesByDrinkingDay
-          );
         } else {
           calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-          console.log('DOSES WITHIN GOAL', calendarDays[day], dailyDoses[day], goalStartOfWeek.dosesByDrinkingDay);
         }
       }
 
       // calendar goals
       // count days in current week
-      console.log('CALENDAR GOALS');
       currentWeek[day] = dailyDoses[day];
       const daysFilled = Object.keys(currentWeek).length;
       const daysWithDrinks = Object.values(currentWeek).filter((dose) => dose > 0).length;
       const daysWithNoDrink = 7 - daysWithDrinks;
-      console.log({ daysFilled, daysWithDrinks, daysWithNoDrink });
       if (!goalStartOfWeek) {
         calendarGoalsStartOfWeek[startOfWeek] = {
           status: 'NoGoal',
@@ -245,7 +215,6 @@ export const derivedDataFromDrinksState = selector({
             'Fixez vous un objectif maximum de consommations pour analyser vos résultats chaque semaine',
           consosWeek: weeklyDoses[startOfWeek],
         };
-        console.log('NO GOAL YES FOR OBJECTIVE');
       } else if (daysFilled < 7) {
         calendarGoalsStartOfWeek[startOfWeek] = {
           status: 'InProgress',
@@ -256,29 +225,21 @@ export const derivedDataFromDrinksState = selector({
           drinkingDaysGoal: 7 - goalStartOfWeek.daysWithGoalNoDrink.length,
           drinkingDays: daysWithDrinks,
         };
-        console.log('DAYS FILLED < 7', daysFilled);
-        console.log('DAY WITH DOSES NOT RESPECTED EXISTS', dayWithDosesNotRespectedExists);
         if (!!dayWithDosesNotRespectedExists) {
           const dosesAreGood = weeklyDoses[startOfWeek] <= goalStartOfWeek.dosesPerWeek;
-          console.log('DOSES ARE GOOD WHILE DAYS STILL UNDER 7', dosesAreGood);
           if (dosesAreGood) {
             for (let i = 0; i < 7; i++) {
               const day = dayjs(startOfWeek).add(i, 'day').format('YYYY-MM-DD');
-              console.log('CHANGING CALENDAR DAY ?', calendarDays[day]);
               if (calendarDays[day] === 'goalExistsButNotRespected') {
                 calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-                console.log('CHANGING CALENDAR DAY !!!', calendarDays[day]);
               }
             }
           }
         }
       } else {
-        console.log('DAYS FILLED IS 7', daysFilled);
         const dosesAreGood = weeklyDoses[startOfWeek] <= goalStartOfWeek.dosesPerWeek;
         const drinkDaysAreGood = daysWithNoDrink >= goalStartOfWeek.daysWithGoalNoDrink.length;
-        console.log({ dosesAreGood, drinkDaysAreGood });
         if (dosesAreGood && drinkDaysAreGood) {
-          console.log('DOSES ARE GOOD AND DRINK DAYS TOO');
           calendarGoalsStartOfWeek[startOfWeek] = {
             status: 'Success',
             consommationMessage: 'Vos consommations de cette semaine sont __dans__ votre objectif fixé.',
@@ -288,20 +249,16 @@ export const derivedDataFromDrinksState = selector({
             drinkingDaysGoal: 7 - goalStartOfWeek.daysWithGoalNoDrink.length,
             drinkingDays: daysWithDrinks,
           };
-          console.log('DAY WITH DOSES NOT RESPECTED EXISTS ?', dayWithDosesNotRespectedExists);
           if (dosesAreGood && !!dayWithDosesNotRespectedExists) {
             for (let i = 0; i < 7; i++) {
               const day = dayjs(startOfWeek).add(i, 'day').format('YYYY-MM-DD');
-              console.log('CHANGING CALENDAR DAY ?', calendarDays[day]);
               if (calendarDays[day] === 'goalExistsButNotRespected') {
                 calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-                console.log('CHANGING CALENDAR DAY !!!', calendarDays[day]);
               }
             }
           }
         }
         if (!dosesAreGood && drinkDaysAreGood) {
-          console.log('DOSES ARE NO GOOD AND DRINK DAYS ARE GOOD');
           calendarGoalsStartOfWeek[startOfWeek] = {
             status: 'Fail',
             consommationMessage: 'Vos consommations de cette semaine sont __supérieures__ à votre objectif fixé.',
@@ -316,18 +273,15 @@ export const derivedDataFromDrinksState = selector({
             if (dailyDoses[day] > goalStartOfWeek.dosesByDrinkingDay) {
               if (calendarDays[day] === 'goalExistsAndDosesWithinGoal') {
                 calendarDays[day] = 'goalExistsButNotRespected';
-                console.log('FIXING CALENDAR DAY', day, calendarDays[day]);
               }
             } else {
               if (calendarDays[day] === 'goalExistsButNotRespected') {
                 calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-                console.log('FIXING CALENDAR DAY BUT SHOULDNT', day, calendarDays[day]);
               }
             }
           }
         }
         if (dosesAreGood && !drinkDaysAreGood) {
-          console.log('DOSES ARE GOOD AND DRINK DAYS ARE NO GOOD');
           calendarGoalsStartOfWeek[startOfWeek] = {
             status: 'Fail',
             consommationMessage: 'Vos consommations de cette semaine sont __dans__ votre objectif fixé.',
@@ -337,20 +291,16 @@ export const derivedDataFromDrinksState = selector({
             drinkingDaysGoal: 7 - goalStartOfWeek.daysWithGoalNoDrink.length,
             drinkingDays: daysWithDrinks,
           };
-          console.log('DAY WITH DOSES NOT RESPECTED EXISTS ?', dayWithDosesNotRespectedExists);
           if (!!dayWithDosesNotRespectedExists) {
             for (let i = 7; i > 0; i--) {
               const day = dayjs(startOfWeek).add(i, 'day').format('YYYY-MM-DD');
-              console.log('CHANGING CALENDAR DAY ?', calendarDays[day]);
               if (calendarDays[day] === 'goalExistsButNotRespected') {
                 calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-                console.log('CHANGING CALENDAR DAY !!!', calendarDays[day]);
               }
             }
           }
         }
         if (!dosesAreGood && !drinkDaysAreGood) {
-          console.log('DOSES ARE NO GOOD AND DRINK DAYS ARE NO GOOD');
           calendarGoalsStartOfWeek[startOfWeek] = {
             status: 'Fail',
             consommationMessage: 'Vos consommations de cette semaine sont __supérieures__ à votre objectif fixé.',
@@ -365,12 +315,10 @@ export const derivedDataFromDrinksState = selector({
             if (dailyDoses[day] > goalStartOfWeek.dosesByDrinkingDay) {
               if (calendarDays[day] === 'goalExistsAndDosesWithinGoal') {
                 calendarDays[day] = 'goalExistsButNotRespected';
-                console.log('FIXING CALENDAR DAY', day, calendarDays[day]);
               }
             } else {
               if (calendarDays[day] === 'goalExistsButNotRespected') {
                 calendarDays[day] = 'goalExistsAndDosesWithinGoal';
-                console.log('FIXING CALENDAR DAY BUT SHOULDNT', day, calendarDays[day]);
               }
             }
           }
@@ -386,11 +334,6 @@ export const derivedDataFromDrinksState = selector({
       const expenses = mapDrinkToPrice(drink, consolidatedCatalogObject);
       if (!weeklyExpenses[startOfWeek]) weeklyExpenses[startOfWeek] = 0;
       weeklyExpenses[startOfWeek] = weeklyExpenses[startOfWeek] + expenses;
-      console.log('FINITO FOR THIS DAY');
-      console.log('----------------------');
-      console.log('----------------------');
-      console.log('----------------------');
-      console.log('----------------------');
     }
 
     return {
