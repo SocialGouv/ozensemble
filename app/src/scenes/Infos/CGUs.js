90ca4aaaca60c327641818a5c676b8b7beb48583
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-11-16 16:32:33 +0100

    feat: v207

--- app/src/scenes/Infos/CGUs.js
+++ app/src/scenes/Infos/CGUs.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
-import Pdf from 'react-native-pdf';
+import { Dimensions, View } from 'react-native';
+// import Pdf from 'react-native-pdf';
 import BackButton from '../../components/BackButton';
 import { defaultPaddingFontScale } from '../../styles/theme';
+import styled from 'styled-components';
+import WebView from 'react-native-webview';
 
-const pdfUrl = 'https://ozensemble.fr/files/CGU_app.pdf';
+const htmlUrl = 'https://ozensemble.fabique.social.gouv.fr/files/CGU_app.html';
 
 const CGUs = ({ onClose }) => (
   <View className="flex-1 justify-start items-center mt-6">
     <View className="flex flex-row w-full mb-4" style={{ paddingHorizontal: defaultPaddingFontScale() }}>
       <BackButton onPress={onClose} />
     </View>
-    <Pdf
+    {/* <Pdf
       trustAllCerts={false}
       source={{
-        uri: pdfUrl,
-        cache: true,
+        uri: htmlUrl,
+        // cache: true,
       }}
       className="flex-1 w-screen h-screen"
-    />
+    /> */}
+
+    <WebViewContainer>
+      <WebView
+        source={{
+          uri: htmlUrl,
+        }}
+      />
+    </WebViewContainer>
   </View>
 );
 
 export default CGUs;
+
+const WebViewContainer = styled.View`
+  flex-grow: 1;
+  flex-shrink: 2;
+  height: 100%;
+  width: ${Dimensions.get('window').width}px;
+`;
