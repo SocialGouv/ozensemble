f33a08d7409f1262421c42fa05788b26982f40a8
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-11-16 16:34:25 +0100

    feat: v207

--- app/src/scenes/Infos/PrivacyPolicy.js
+++ app/src/scenes/Infos/PrivacyPolicy.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import { View } from 'react-native';
-import Pdf from 'react-native-pdf';
+import { Dimensions, View } from 'react-native';
+// import Pdf from 'react-native-pdf';
 import BackButton from '../../components/BackButton';
 import { defaultPaddingFontScale } from '../../styles/theme';
+import { WebView } from 'react-native-webview';
+import styled from 'styled-components';
 
-const pdfUrl = 'https://ozensemble.fr/files/privacy_policy_app.pdf';
+const htmlUrl = 'https://ozensemble.fabique.social.gouv.fr/files/privacy_policy_app.html';
 
 const PrivacyPolicy = ({ onClose }) => (
   <View className="flex-1 justify-start items-center mt-6">
     <View className="flex flex-row w-full mb-4" style={{ paddingHorizontal: defaultPaddingFontScale() }}>
       <BackButton onPress={onClose} />
     </View>
-    <Pdf
+    {/* <Pdf
       trustAllCerts={false}
       source={{
         uri: pdfUrl,
-        cache: true,
+        // cache: true,
+      }}
+      onLoadComplete={(numberOfPages, filePath) => {
+        console.log(`Number of pages: ${numberOfPages}`);
+      }}
+      onPageChanged={(page, numberOfPages) => {
+        console.log(`Current page: ${page}`);
+      }}
+      onError={(error) => {
+        console.log(error);
       }}
       className="flex-1 w-screen h-screen"
-    />
+    /> */}
+
+    <WebViewContainer>
+      <WebView
+        source={{
+          uri: htmlUrl,
+        }}
+      />
+    </WebViewContainer>
   </View>
 );
 
 export default PrivacyPolicy;
+
+const WebViewContainer = styled.View`
+  flex-grow: 1;
+  flex-shrink: 2;
+  height: 100%;
+  width: ${Dimensions.get('window').width}px;
+`;
