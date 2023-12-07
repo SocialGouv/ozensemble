0b34d5720799f01efc3bdc47bb11b762ae7a61f0
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-07 11:49:35 +0100

    fix: date instead of item

--- app/src/scenes/AddDrink/AddDrinkNavigator.js
+++ app/src/scenes/AddDrink/AddDrinkNavigator.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ConsosList from './ConsosList';
 import EmotionsList from '../AddEmotion/EmotionsList';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 import ContextSuggestion from '../AddEmotion/ContextSuggestion';
 
 const AddDrinkStack = createStackNavigator();
@@ -12,26 +12,25 @@ const AddDrinkNavigator = ({ route }) => {
   const [addDrinkModalTimestamp, setDrinkModalTimestamp] = useState(() => route?.params?.timestamp);
 
   return (
-    <AddDrinkStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="CONSOS_LIST">
-      <AddDrinkStack.Screen initialParams={{ parent: route?.params?.parent }} name="CONSOS_LIST">
-        {(props) => (
-          <ConsosList
-            {...props}
-            setDrinkModalTimestamp={setDrinkModalTimestamp}
-            addDrinkModalTimestamp={addDrinkModalTimestamp}
-          />
-        )}
-      </AddDrinkStack.Screen>
-      <AddDrinkStack.Screen initialParams={{ parent: route?.params?.parent }} name="EMOTIONS_LIST">
-        {(props) => (
-          <EmotionsList {...props} addDrinkModalTimestamp={addDrinkModalTimestamp} key={addDrinkModalTimestamp} />
-        )}
-      </AddDrinkStack.Screen>
-
-      <AddDrinkStack.Screen initialParams={{ parent: route?.params?.parent }} name="CONTEXT_SUGGESTION_SCREEN">
-        {(props) => <ContextSuggestion {...props} />}
-      </AddDrinkStack.Screen>
-    </AddDrinkStack.Navigator>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <AddDrinkStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="CONSOS_LIST">
+        <AddDrinkStack.Screen initialParams={{ parent: route?.params?.parent }} name="CONSOS_LIST">
+          {(props) => (
+            <ConsosList
+              {...props}
+              setDrinkModalTimestamp={setDrinkModalTimestamp}
+              addDrinkModalTimestamp={addDrinkModalTimestamp}
+            />
+          )}
+        </AddDrinkStack.Screen>
+        <AddDrinkStack.Screen initialParams={{ parent: route?.params?.parent }} name="EMOTIONS_LIST">
+          {(props) => (
+            <EmotionsList {...props} addDrinkModalTimestamp={addDrinkModalTimestamp} key={addDrinkModalTimestamp} />
+          )}
+        </AddDrinkStack.Screen>
+      </AddDrinkStack.Navigator>
+    </>
   );
 };
 
