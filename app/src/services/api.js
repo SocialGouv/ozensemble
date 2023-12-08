be2e4ef41364d7e6872bcd75cd1917531e55d0b0
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2023-12-08 13:58:29 +0100

    fix: api

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -17,8 +17,8 @@ export const checkNetwork = async (test = false) => {
 };
 
 class ApiService {
-  host = 'localhost:3615';
-  scheme = 'http';
+  host = 'API_HOST';
+  scheme = 'SCHEME';
   getUrl = (path, query) => {
     return new URI().host(this.host).scheme(this.scheme).path(path).setSearch(query).toString();
   };
