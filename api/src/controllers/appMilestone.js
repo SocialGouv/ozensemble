40a2e14f0912027a83c7a146d3a39f96f8222e1e
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-12 09:30:37 +0100

    fix: oups modal

--- api/src/controllers/appMilestone.js
+++ api/src/controllers/appMilestone.js
@@ -53,7 +53,7 @@ router.post(
 
     // USER SURVEY:
     if (req.headers.appversion < 205) return res.status(200).send({ ok: true });
-    if (req.headers.appversion >= 211) {
+    if (req.headers.appversion >= 214) {
       const newUserContextFeature = await prisma.appMilestone.findUnique({
         where: { id: `${user.id}_@NewUserContextFeature` },
       });
