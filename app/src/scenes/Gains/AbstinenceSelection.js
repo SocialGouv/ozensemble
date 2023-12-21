c396385c121006d0c850cb7433591c8cd9268d4e
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2023-12-21 15:54:19 +0100

    fix: buttons color place text from info

--- app/src/scenes/Gains/AbstinenceSelection.js
+++ app/src/scenes/Gains/AbstinenceSelection.js
@@ -3,7 +3,8 @@ import { View, Text, SafeAreaView, TouchableOpacity, Dimensions } from 'react-na
 
 import { storage } from '../../services/storage';
 
-const AbstinenceSelection = ({ navigation }) => {
+const AbstinenceSelection = ({ navigation, route }) => {
+  const isFromInfo = route?.name.includes('ABSTINENCE_SELECTION');
   const onClose = () => {
     navigation.goBack();
   };
@@ -17,16 +18,24 @@ const AbstinenceSelection = ({ navigation }) => {
       <View className="bg-white rounded-xl mx-5">
         <View className=" flex w-full mb-2 px-2">
           <Text className="text-center mb-3 text-xl mt-8 px-2 font-extrabold">Compteur d'abstinence</Text>
-          <Text className="line text-lg text-justify  color-black mb-5 mx-6 mt-2">
-            Il sera affiché sous votre calendrier et vous permettra de suivre vos jours consécutifs sans consommation
-            d'alcool si vous avez fait le choix d'être abstinent.
-          </Text>
+          {isFromInfo && (
+            <Text className="line text-lg text-justify  color-black mb-5 mx-6 mt-2">
+              Il sera affiché sous votre calendrier et vous permettra de suivre vos jours consécutifs sans consommation
+              d'alcool si vous avez fait le choix d'être abstinent.
+            </Text>
+          )}
+          {!isFromInfo && (
+            <Text className="line text-lg text-justify  color-black mb-5 mx-6 mt-2">
+              Il vous permettra de suivre vos jours consécutifs sans consommation d’alcool si vous avez fait le choix
+              d’être abstinent. Vous pourrez toujours modifier ce choix dans l’onglet Infos.
+            </Text>
+          )}
           <Text className=" text-center mb-1 text-xl px-2 font-bold">
             Voulez-vous activer le compteur d'abstinence ?
           </Text>
           <View className="flex flex-row justify-center p-2 mb-6 mt-2">
             <TouchableOpacity
-              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#DE285E] "
+              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#4030A5] "
               onPress={() => {
                 onClose();
                 storage.set('@isAbstinent', true);
@@ -34,7 +43,7 @@ const AbstinenceSelection = ({ navigation }) => {
               <Text className="text-xl color-white font-extrabold">Oui</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#4030A5] ml-7"
+              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#DE285E] ml-7"
               onPress={() => {
                 onClose();
                 storage.set('@isAbstinent', false);
