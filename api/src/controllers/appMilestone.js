b397bf807f910ae4882aa732b2bc074e200266f0
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2023-12-21 09:15:56 +0100

    fix: version

--- api/src/controllers/appMilestone.js
+++ api/src/controllers/appMilestone.js
@@ -53,7 +53,7 @@ router.post(
 
     // USER SURVEY:
     if (req.headers.appversion < 205) return res.status(200).send({ ok: true });
-    if (req.headers.appversion >= 225) {
+    if (req.headers.appversion >= 226) {
       const newUserAbstinenceFeature = await prisma.appMilestone.findUnique({
         where: { id: `${user.id}_@NewUserAbstinenceFeature` },
       });
