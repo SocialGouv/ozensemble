84752db82953699a67d4fda17ca5c53cdd6a0ce7
YoanRos <98326747+YoanRos@users.noreply.github.com>
2023-12-21 09:40:41 +0100

    feat: abstinence (#508)

--- app/src/recoil/consos.js
+++ app/src/recoil/consos.js
@@ -123,12 +123,14 @@ export const derivedDataFromDrinksState = selector({
     const calendarGoalsStartOfWeek = {};
     const weeklyKcals = {};
     const weeklyExpenses = {};
+    let abstinenceDays = 0;
+    let abstinenceSerieBroken = false;
+    let doneDay = null;
 
     // temp variables
     let startOfWeek = null;
     let goalStartOfWeek = null;
     let currentWeek = {};
-
     for (const drink of drinks) {
       const day = dayjs(drink.timestamp).format('YYYY-MM-DD');
       // init startOfWeek
@@ -168,6 +170,16 @@ export const derivedDataFromDrinksState = selector({
         dailyDoses[day] = 0;
       }
       dailyDoses[day] = dailyDoses[day] + dose;
+      // abstinence days
+      if (!abstinenceSerieBroken && doneDay !== day) {
+        if (dose === 0 && (!doneDay || doneDay === dayjs(day).add(1, 'day').format('YYYY-MM-DD'))) {
+          abstinenceDays++;
+        } else {
+          abstinenceSerieBroken = true;
+        }
+      }
+      doneDay = day;
+
       // weekly doses
       if (!weeklyDoses[startOfWeek]) {
         weeklyDoses[startOfWeek] = 0;
@@ -295,6 +307,7 @@ export const derivedDataFromDrinksState = selector({
       calendarGoalsStartOfWeek,
       weeklyKcals,
       weeklyExpenses,
+      abstinenceDays,
     };
   },
 });
---
+++ app/src/scenes/Gains/AbstinenceSelection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
+
+import { storage } from '../../services/storage';
+
+const AbstinenceSelection = ({ navigation }) => {
+  const onClose = () => {
+    navigation.goBack();
+  };
+  return (
+    <SafeAreaView
+      style={{
+        justifyContent: 'start',
+        marginTop: Dimensions.get('window').height * 0.3,
+      }}
+      className=" rounded-t-xl">
+      <View className="bg-white rounded-xl mx-5">
+        <View className=" flex w-full mb-2 px-2">
+          <Text className="text-center mb-3 text-xl mt-8 px-2 font-extrabold">Compteur d'abstinence</Text>
+          <Text className="line text-lg text-justify  color-black mb-5 mx-6 mt-2">
+            Il sera affiché sous votre calendrier et vous permettra de suivre vos jours consécutifs sans consommation
+            d'alcool si vous avez fait le choix d'être abstinent.
+          </Text>
+          <Text className=" text-center mb-1 text-xl px-2 font-bold">
+            Voulez-vous activer le compteur d'abstinence ?
+          </Text>
+          <View className="flex flex-row justify-center p-2 mb-6 mt-2">
+            <TouchableOpacity
+              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#DE285E] mr-6"
+              onPress={() => {
+                onClose();
+                storage.set('@isAbstinent', true);
+              }}>
+              <Text className="text-xl color-white font-extrabold">Oui</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="justify-center  items-center rounded-3xl h-12 w-24 bg-[#4030A5] mr-6"
+              onPress={() => {
+                onClose();
+                storage.set('@isAbstinent', false);
+              }}>
+              <Text className="text-xl color-white font-extrabold">Non</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default AbstinenceSelection;
