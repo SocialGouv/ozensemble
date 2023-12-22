122cdebda590ee5d42527d7f5c5195796bf2a82d
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2023-12-22 11:17:50 +0100

    fix: route undefined

--- app/src/scenes/Gains/AbstinenceSelection.js
+++ app/src/scenes/Gains/AbstinenceSelection.js
@@ -4,7 +4,7 @@ import { logEvent } from '../../services/logEventsWithMatomo';
 import { storage } from '../../services/storage';
 
 const AbstinenceSelection = ({ navigation, route }) => {
-  const isFromInfo = route?.params?.parent === 'Infos';
+  const isFromInfo = route?.params ? route?.params?.parent === 'Infos' : false;
   const onClose = () => {
     navigation.goBack();
   };
@@ -18,7 +18,7 @@ const AbstinenceSelection = ({ navigation, route }) => {
       <View className="bg-white rounded-xl mx-5">
         <View className=" flex w-full mb-2 px-2">
           <Text className="text-center mb-3 text-xl mt-8 px-2 font-extrabold">Compteur d'abstinence</Text>
-          {isFromInfo && (
+          {!!isFromInfo && (
             <Text className="line text-lg text-justify  color-black mb-5 mx-6 mt-2">
               Il sera affiché sous votre calendrier et vous permettra de suivre vos jours consécutifs sans consommation
               d'alcool si vous avez fait le choix d'être abstinent.
