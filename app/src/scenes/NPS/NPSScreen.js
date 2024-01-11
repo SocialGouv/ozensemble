37a81d4ac7691a6e9d6573ad628eb25b9211c1f1
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-01-11 14:07:56 +0100

    fix: useless css

--- app/src/scenes/NPS/NPSScreen.js
+++ app/src/scenes/NPS/NPSScreen.js
@@ -1,21 +1,16 @@
 import React, { useRef, useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
-import { Dimensions, Platform, View, Text, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
+import { Platform, View, Text, TextInput, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import pck from '../../../package.json';
 import Background from '../../components/Background';
 import ButtonPrimary from '../../components/ButtonPrimary';
-import TextStyled from '../../components/TextStyled';
-import TextInputStyled from '../../components/TextInputStyled';
+
 import { logEvent } from '../../services/logEventsWithMatomo';
 import NotificationService from '../../services/notifications';
 import Mark from './Mark';
 import { storage } from '../../services/storage';
-import { ScreenBgStyled } from '../../components/ScreenBgStyled';
-import H2 from '../../components/H2';
-import H3 from '../../components/H3';
-import { defaultPaddingFontScale, screenWidth } from '../../styles/theme';
+
 import BackButton from '../../components/BackButton';
 import { sendMail } from '../../services/mail';
 import useAppState from '../../services/useAppState';
@@ -266,68 +261,5 @@ const NPSScreen = ({ navigation, route }) => {
     </SafeAreaProvider>
   );
 };
-
-const Container = styled.View`
-  height: 100%;
-  width: ${Dimensions.get('window').width}px;
-`;
-
-const KeyboardAvoidingViewStyled = styled.KeyboardAvoidingView`
-  flex: 1;
-  min-height: 100%;
-`;
-
-const commonCss = css`
-  width: 95%;
-  flex-shrink: 0;
-`;
-
-const TopTitle = styled(H2)`
-  ${commonCss}
-  margin-top: 10px;
-`;
-
-const TopSubTitle = styled(H3)`
-  ${commonCss}
-  margin-top: 35px;
-`;
-
-const FeedBackStyled = styled(TextInputStyled)`
-  width: 100%;
-  height: 100px;
-  border-radius: 3px;
-  border: 1px solid #dbdbe9;
-  background-color: #f3f3f6;
-  border-radius: 7px;
-  padding: 15px;
-  margin-top: 15px;
-  justify-content: flex-start;
-  align-items: flex-start;
-  color: #4030a5;
-`;
-
-const ButtonContainer = styled.View`
-  margin-vertical: 20px;
-  align-items: flex-start;
-  flex-grow: 0;
-  flex-direction: row;
-  justify-content: space-around;
-  margin-left: -${defaultPaddingFontScale()}px;
-  width: ${screenWidth}px;
-  margin-bottom: 150px;
-`;
-
-const ContactTextInput = styled(TextInputStyled)`
-  width: 100%;
-  height: 50px;
-  background-color: #f3f3f6;
-  border: 1px solid #dbdbe9;
-  color: #4030a5;
-  border-radius: 7px;
-  padding-left: 15px;
-  justify-content: center;
-  margin-bottom: 10px;
-  margin-top: 15px;
-`;
 
 export default NPSScreen;
