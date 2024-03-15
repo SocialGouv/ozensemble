1689a94a8cd7b47a39b70cb10a0467e8d22f8bfc
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-03-15 09:43:12 +0100

    fix: was always true

--- api/src/controllers/goal.js
+++ api/src/controllers/goal.js
@@ -30,8 +30,7 @@ router.post(
 
     // we want to know if the goal of this week is already proceded. If it is, we want to create/modify the one for next week
     // if it is not, we want to update the current one with the new values
-    if (thisWeekGoal?.status !== "InProgress") date = dayjs(date).add(1, "week").startOf("week").format("YYYY-MM-DD");
-
+    if (thisWeekGoal && thisWeekGoal.status !== "InProgress") date = dayjs(date).add(1, "week").startOf("week").format("YYYY-MM-DD");
     await prisma.goal.upsert({
       where: { id: `${user.id}_${date}` },
       create: {
