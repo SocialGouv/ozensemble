d18574365337d6e6c3e0761e8a7677fe6c73fdeb
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-04-11 09:30:39 +0200

    Update api/src/middlewares/versionCheck.js

--- api/src/middlewares/versionCheck.js
+++ api/src/middlewares/versionCheck.js
@@ -3,7 +3,7 @@ const { he } = require("date-fns/locale");
 const MINIMUM_MOBILE_APP_VERSION = 75;
 
 module.exports = ({ headers: { appversion, appdevice } }, res, next) => {
-  if (appdevice && !appversion) return res.status(403).send({ ok: false, sendInApp: [appdevice] });
+  if (appdevice && !appversion) return res.status(403).send({ ok: false, sendInApp: ["Veuillez mettre à jour votre application!"] });
   if (appdevice && Number(appversion) < MINIMUM_MOBILE_APP_VERSION)
     return res.status(403).send({
       ok: false,
