3ac9e5367c00db500465b77667f555a709fc69f1
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-04-16 11:24:12 +0200

    feat: own cl lack icon

--- app/src/scenes/AddDrink/AddAlcoolQuantity.js
+++ app/src/scenes/AddDrink/AddAlcoolQuantity.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { View, Text, TouchableOpacity, ScrollView, Modal } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView, Modal, Touchable } from 'react-native';
 import BackButton from '../../components/BackButton';
 import { defaultPaddingFontScale } from '../../styles/theme';
 import TextStyled from '../../components/TextStyled';
 import H2 from '../../components/H2';
 import { alcoolQuantityCatalog } from './alcoolQuantityCatalog';
 import { getIcon } from '../ConsoFollowUp/drinksCatalog';
+import { TextInput } from 'react-native-gesture-handler';
+import CheckDefisValidated from '../../components/illustrations/icons/CheckDefisValidated';
 
 const AddAlcoolQuantity = ({ visible, hide, setQuantitySelected }) => {
+  const [value, setValue] = useState('');
+  const [selected, setSelected] = useState(false);
   return (
     <Modal visible={visible} hide={hide} animationType="sheet" presentationStyle="formSheet">
       <View className="h-full w-full bg-white">
@@ -19,9 +23,28 @@ const AddAlcoolQuantity = ({ visible, hide, setQuantitySelected }) => {
               Sélectionnez une quantité d'alcool
             </H2>
             <TextStyled italic className="text-xs mb-4">
-              Cliquez sur un contenant pour compléter le champ
+              Saisissez une valeur dans le premier champ ou cliquez sur un contenant pour compléter le champ
             </TextStyled>
             <View className="mb-10">
+              <View className="flex flex-row border border-[#DBDBE8] bg-[#F3F3F6] items-center justify-between rounded-lg h-14 px-2 mb-5">
+                <TextInput
+                  className=""
+                  placeholder="Saisissez une valeur en cl"
+                  keyboardType="numeric"
+                  value={value}
+                  onChangeText={(value) => {
+                    setValue(value), setQuantitySelected({ volume: value, name: 'Autre', icon: 'CocktailGlass' });
+                  }}></TextInput>
+                <TouchableOpacity
+                  className=""
+                  onPress={() => {
+                    hide();
+                    setQuantitySelected({ volume: value, name: 'Autre', icon: 'CocktailGlass' });
+                  }}>
+                  <CheckDefisValidated size={30} fill={'#DE285E'} />
+                </TouchableOpacity>
+              </View>
+
               {alcoolQuantityCatalog.map((quantity) => {
                 const Icon = getIcon(quantity.icon);
                 return (
@@ -34,6 +57,7 @@ const AddAlcoolQuantity = ({ visible, hide, setQuantitySelected }) => {
                         volume: quantity.volume?.split(' ')[0],
                         icon: quantity.icon,
                       });
+                      setValue('');
                       hide();
                     }}>
                     <Icon size={30} />
