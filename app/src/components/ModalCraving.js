edac1ca1e350ce6470afa838660e66000e5d10b7
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-04-29 12:51:04 +0200

    fix: modal craving

--- app/src/components/ModalCraving.js
+++ app/src/components/ModalCraving.js
@@ -9,8 +9,8 @@ import TipIcon from './illustrations/TipIcon';
 
 const ModalCraving = ({ firstTimeOnCraving, onClose }) => {
   return (
-    <Modal visible={firstTimeOnCraving} animationType="fade" withBackground hideOnTouch>
-      <View className="bg-white rounded-xl px-2 h-5/6">
+    <Modal visible={firstTimeOnCraving} animationType="fade" withBackground hideOnTouch safeAreaView>
+      <View className="h-full justify-between p-2">
         <View className="h-5 flex flex-row  justify-end">
           <TouchableOpacity onPress={onClose} hitSlop={hitSlop(15)}>
             <Svg fill="none" viewBox="0 0 24 24" className="h-5 w-5">
@@ -24,33 +24,33 @@ const ModalCraving = ({ firstTimeOnCraving, onClose }) => {
             </Svg>
           </TouchableOpacity>
         </View>
-        <View className="flex flex-col justify-between h-full">
-          <View className="flex flex-col items-center">
-            <CravingIcon size={70} className="" />
-          </View>
-          <Text color="#000" className="text-start text-xl font-extrabold text-[#DE285E] ">
-            M'aider avec mon craving
-          </Text>
-          <View className="flex flex-row ">
-            <TipIcon size={20} className="mt-1 -mr-4 " />
-            <Text className="text-start text-black text-lg">
-              {'     '}
-              Vous êtes dans un moment de craving, et nous sommes là pour vous aider !
-            </Text>
-          </View>
-          <Text className="text-start text-[#4030A5] font-bold text-lg">
-            Sachez qu’un moment de craving n’est pas eternel, et dure généralement entre 5 et 20 minutes.
-          </Text>
+
+        <View className="items-center">
+          <CravingIcon size={70} className="-mt-6" />
+        </View>
+        <Text color="#000" className="text-start text-xl font-extrabold text-[#DE285E] ">
+          M'aider avec mon craving
+        </Text>
+        <View className="flex flex-row ">
+          <TipIcon size={20} className="mt-1 -mr-4 " />
           <Text className="text-start text-black text-lg">
-            Le craving peut être intense et difficile à gérer, mais il existe des moyens de le surmonter.
-          </Text>
-          <Text className="text-start text-black text-lg">
-            Oz vous propose divers conseils et activités afin de vous accompagner dans ces moments délicats.
-          </Text>
-          <Text className="text-start text-black text-lg">
-            Cliquez sur “Commencer” pour accéder dès maintenant aux activités !
+            {'     '}
+            Vous êtes dans un moment de craving, et nous sommes là pour vous aider !
           </Text>
         </View>
+        <Text className="text-start text-[#4030A5] font-bold text-lg">
+          Sachez qu’un moment de craving n’est pas éternel, et dure généralement entre 5 et 20 minutes.
+        </Text>
+        <Text className="text-start text-black text-lg">
+          Le craving peut être intense et difficile à gérer, mais il existe des moyens de le surmonter.
+        </Text>
+        <Text className="text-start text-black text-lg">
+          Oz vous propose divers conseils et activités afin de vous accompagner dans ces moments délicats.
+        </Text>
+        <Text className="text-start text-black text-lg">
+          Cliquez sur “Commencer” pour accéder dès maintenant aux activités !
+        </Text>
+
         <View className="flex flex-row justify-center">
           <ButtonPrimary
             content="Commencer"
