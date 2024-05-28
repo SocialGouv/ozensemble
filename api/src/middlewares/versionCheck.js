732aa9bf5a1f8efb71befc5ab2d78cd38a48a28d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-05-28 14:01:57 +0200

    feat: force latest version

--- api/src/middlewares/versionCheck.js
+++ api/src/middlewares/versionCheck.js
@@ -1,18 +1,20 @@
-const { he } = require("date-fns/locale");
-
-const MINIMUM_MOBILE_APP_VERSION = 75;
+const MINIMUM_MOBILE_APP_VERSION = 273;
 
 module.exports = ({ headers: { appversion, appdevice } }, res, next) => {
   if (appdevice && !appversion) return res.status(403).send({ ok: false, sendInApp: ["Veuillez mettre à jour votre application!"] });
-  if (appdevice && Number(appversion) < MINIMUM_MOBILE_APP_VERSION)
+  if (appdevice && Number(appversion) < MINIMUM_MOBILE_APP_VERSION) {
+    const iosLink = "https://apps.apple.com/fr/app/oz-ensemble/id1498190343";
+    const androidLink = "https://play.google.com/store/apps/details?id=fr.gouv.ozensemble";
+
     return res.status(403).send({
       ok: false,
       sendInApp: [
         "Votre application n'est pas à jour !",
         "Vous pouvez la mettre à jour en cliquant sur le lien ci-dessous",
-        [{ text: "Mettre à jour", link: "https://ozensemble.fabrique.social.gouv.fr" }],
+        [{ text: "Mettre à jour", link: appdevice === "ios" ? iosLink : androidLink }],
         { cancelable: true },
       ],
     });
+  }
   return next();
 };
