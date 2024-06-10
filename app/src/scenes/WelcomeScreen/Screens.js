b162512421153a337aa968ff514265cbfe324c6a
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-06-10 23:09:36 +0200

    fix: splash screens lack waves

--- app/src/scenes/WelcomeScreen/Screens.js
+++ app/src/scenes/WelcomeScreen/Screens.js
@@ -7,20 +7,28 @@ import Wave from '../../components/illustrations/onboarding/Wave';
 import ButtonPrimary from '../../components/ButtonPrimary';
 import NotificationService from '../../services/notifications';
 
-export const Screen1 = ({ onPressNext }) => (
+export const ScreenRate = ({ onPressNext }) => (
   <View className="h-full">
-    <View className="h-2/3 justify-center">
-      <View className="px-5">
-        <TextStyled className="text-center text-white text-2xl font-bold -bottom-4">
-          Fixez votre objectif et{'\n'}ajoutez vos consommations
-        </TextStyled>
+    <View className="h-2/3 justify-center space-y-8 px-5 mt-5">
+      <TextStyled className="text-center text-white text-2xl font-semibold ">
+        “ Permet de faire le point et de se fixer des objectifs simplement et sans culpabilité. ”
+      </TextStyled>
+      <TextStyled className="text-center text-white text-2xl font-semibold ">
+        “ Application très bien pensée et indispensable si vous voulez suivre votre consommation. ”{' '}
+      </TextStyled>
+      <View className="flex flex-row space-x-4 justify-center">
+        <Image source={require('../../assets/illustrations/laurier-gauche.png')} resizeMode="contain" className={``} />
+        <View className="flex flex-col">
+          <TextStyled className="text-center text-white text-3xl font-bold ">+ 50 000</TextStyled>
+          <TextStyled className="text-center text-white text-2xl font-semibold ">utilisateurs satisfaits</TextStyled>
+        </View>
+        <Image source={require('../../assets/illustrations/laurier-droit.png')} resizeMode="contain" className={``} />
       </View>
-      <View className="flex-1 items-center">
-        <Image
-          source={require('../../assets/illustrations/screen1.png')}
-          resizeMode="contain"
-          className={`h-full w-full`}
-        />
+      <View className="flex flex-col items-center space-y-2">
+        <TextStyled className="text-center text-white text-3xl font-bold ">Noté 4.8</TextStyled>
+        <Image source={require('../../assets/illustrations/stars-rate.png')} resizeMode="contain" className={``} />
+        <TextStyled className="text-center text-white text-2xl font-bold ">sur les stores</TextStyled>
+        <TextStyled className="text-center text-white text-xl font-semibold ">avec + de 1000 avis</TextStyled>
       </View>
     </View>
     <View className="h-1/3 pb-[48px] justify-end items-center">
@@ -38,8 +46,32 @@ export const Screen1 = ({ onPressNext }) => (
     </View>
   </View>
 );
+export const ScreenCalendar = ({ onPressNext }) => (
+  <View className="h-full">
+    <View className="h-2/3 justify-center">
+      <View className="px-5">
+        <TextStyled className="text-center text-white text-2xl font-bold -bottom-4">
+          Fixez votre objectif et{'\n'}ajoutez vos consommations
+        </TextStyled>
+      </View>
+      <View className="flex-1 items-center">
+        <Image
+          source={require('../../assets/illustrations/screen_calendar.png')}
+          resizeMode="contain"
+          className={`h-full w-full`}
+        />
+      </View>
+    </View>
+    <View className="h-1/3 pb-[48px] justify-end items-center">
+      <View className={`absolute -bottom-0`}>
+        <Wave currentIndex={1} size={screenWidth + 4} />
+      </View>
+      <ButtonPrimary content={'Suivant'} AnimationEffect onPress={onPressNext} />
+    </View>
+  </View>
+);
 
-export const Screen2 = ({ onPressNext }) => (
+export const ScreenStats = ({ onPressNext }) => (
   <View className="h-full">
     <View className="h-2/3 justify-center">
       <View className="px-5">
@@ -49,32 +81,7 @@ export const Screen2 = ({ onPressNext }) => (
       </View>
       <View className="flex-1 items-center">
         <Image
-          source={require('../../assets/illustrations/screen2.png')}
-          resizeMode="contain"
-          className={`h-full w-full`}
-        />
-      </View>
-    </View>
-    <View className="h-1/3 pb-[48px] justify-end items-center">
-      <View className={`absolute -bottom-0`}>
-        <Wave currentIndex={1} size={screenWidth} />
-      </View>
-      <ButtonPrimary content={'Suivant'} AnimationEffect onPress={onPressNext} />
-    </View>
-  </View>
-);
-
-export const Screen3 = ({ onPressNext }) => (
-  <View className="h-full">
-    <View className="h-2/3 justify-center">
-      <View className="px-5">
-        <TextStyled className="text-center text-white text-2xl font-bold -bottom-4">
-          Explorez les activités et{'\n'}les articles santé
-        </TextStyled>
-      </View>
-      <View className="flex-1 items-center">
-        <Image
-          source={require('../../assets/illustrations/screen3.png')}
+          source={require('../../assets/illustrations/screen_stats.png')}
           resizeMode="contain"
           className={`h-full w-full`}
         />
@@ -89,7 +96,56 @@ export const Screen3 = ({ onPressNext }) => (
   </View>
 );
 
-export const Screen4 = ({ onStartPress, agreed, setAgreed }) => (
+export const ScreenDefi = ({ onPressNext }) => (
+  <View className="h-full">
+    <View className="h-2/3 justify-center">
+      <View className="px-5">
+        <TextStyled className="text-center text-white text-2xl font-bold -bottom-4">
+          Explorez les activités et{'\n'}les articles santé
+        </TextStyled>
+      </View>
+      <View className="flex-1 items-center">
+        <Image
+          source={require('../../assets/illustrations/screen_defi.png')}
+          resizeMode="contain"
+          className={`h-full w-full`}
+        />
+      </View>
+    </View>
+    <View className="h-1/3 pb-[48px] justify-end items-center">
+      <View className={`absolute -bottom-0`}>
+        <Wave currentIndex={3} size={screenWidth} />
+      </View>
+      <ButtonPrimary content={'Suivant'} AnimationEffect onPress={onPressNext} />
+    </View>
+  </View>
+);
+export const ScreenCraving = ({ onPressNext }) => (
+  <View className="h-full">
+    <View className="h-2/3 justify-center">
+      <View className="px-5">
+        <TextStyled className="text-center text-white text-2xl font-bold -bottom-4">
+          Surmontez un craving avec{'\n'}des activités et conseils
+        </TextStyled>
+      </View>
+      <View className="flex-1 items-center">
+        <Image
+          source={require('../../assets/illustrations/screen_craving.png')}
+          resizeMode="contain"
+          className={`h-full w-full`}
+        />
+      </View>
+    </View>
+    <View className="h-1/3 pb-[48px] justify-end items-center">
+      <View className={`absolute -bottom-0`}>
+        <Wave currentIndex={4} size={screenWidth} />
+      </View>
+      <ButtonPrimary content={'Suivant'} AnimationEffect onPress={onPressNext} />
+    </View>
+  </View>
+);
+
+export const ScreenAdvice = ({ onStartPress, agreed, setAgreed }) => (
   <View className="h-full">
     <View className="h-2/3 justify-center">
       <View className="px-5">
@@ -99,7 +155,7 @@ export const Screen4 = ({ onStartPress, agreed, setAgreed }) => (
       </View>
       <View className="flex-1 items-center">
         <Image
-          source={require('../../assets/illustrations/screen4.png')}
+          source={require('../../assets/illustrations/screen_advice.png')}
           resizeMode="contain"
           className={`h-full w-[80%]`}
         />
@@ -107,7 +163,7 @@ export const Screen4 = ({ onStartPress, agreed, setAgreed }) => (
     </View>
     <View className="h-1/3 pb-[48px] justify-end items-center">
       <View className={`absolute -bottom-0`}>
-        <Wave currentIndex={3} size={screenWidth} />
+        <Wave currentIndex={5} size={screenWidth} />
       </View>
       <Agreement onAgree={() => setAgreed(!agreed)} agreed={agreed} className="" />
       <ButtonPrimary content={"C'est parti"} AnimationEffect onPress={onStartPress} disabled={!agreed} />
