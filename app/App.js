04350cc10ef4a3228e2b7629a75c5fdfe975869d
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-07-10 15:11:52 +0200

    chore: import react-native-get-random-values

--- app/App.js
+++ app/App.js
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 import React, { useEffect, useState } from 'react';
 import * as Sentry from '@sentry/react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
