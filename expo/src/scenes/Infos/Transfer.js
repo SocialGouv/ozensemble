06b8b09a4ee67e8c44cea5f955e09d513a71f37d
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-07-26 11:52:28 +0200

    fix: useless import

--- expo/src/scenes/Infos/Transfer.js
+++ expo/src/scenes/Infos/Transfer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Alert, TouchableOpacity, View, Text } from "react-native";
 import WrapperContainer from "../../components/WrapperContainer.js";
 import { storage } from "../../services/storage.js";
